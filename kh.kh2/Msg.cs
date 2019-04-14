2564e0c2e5e22552c9d16b60d412950211aa2950
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-04-14 17:33:55 +0100

    Add Msg reading

--- kh.kh2/Msg.cs
+++ kh.kh2/Msg.cs
@@ -1,13 +1,59 @@
 ﻿using kh.common;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace kh.kh2
 {
     public class Msg
     {
         private static uint MagicCode = 1;
+        private static byte Terminator = 0;
+
+        public class Entry
+        {
+            public int Id { get; set; }
+            public byte[] Data { get; set; }
+        }
+
+        public static List<Entry> Open(Stream stream)
+        {
+            if (!stream.CanRead || !stream.CanSeek)
+                throw new InvalidDataException($"Read or seek must be supported.");
+
+            var reader = new BinaryReader(stream);
+            if (stream.Length < 8L || reader.ReadUInt32() != MagicCode)
+                throw new InvalidDataException("Invalid header");
+
+            int entriesCount = reader.ReadInt32();
+            return Enumerable.Range(0, entriesCount)
+                .Select(x => new
+                {
+                    Id = reader.ReadInt32(),
+                    Offset = reader.ReadInt32(),
+                })
+                .ToList()
+                .Select(x =>
+                {
+                    var data = new List<byte>();
+                    reader.BaseStream.Position = x.Offset;
+
+                    byte r;
+                    while ((r = reader.ReadByte()) != Terminator)
+                        data.Add(r);
+                    data.Add(0);
+
+                    return new Entry
+                    {
+                        Id = x.Id,
+                        Data = data.ToArray()
+                    };
+                })
+                .ToList();
+        }
 
         public static bool IsValid(Stream stream) =>
             new BinaryReader(stream).PeekUInt32() == MagicCode;
+
     }
 }
--- kh.tests/kh2/MsgTests.cs
+++ kh.tests/kh2/MsgTests.cs
@@ -6,7 +6,7 @@ namespace kh.tests.kh2
 {
     public class MsgTests : Common
     {
-        private readonly string FileName = "kh2/res/titl.bin";
+        private readonly string FilePath = "kh2/res/titl.bin";
 
         [Fact]
         public void IsValidTest()
@@ -21,5 +21,59 @@ namespace kh.tests.kh2
                 Assert.True(Msg.IsValid(stream));
             }
         }
+
+        [Fact]
+        public void IsNotValidTest()
+        {
+            using (var stream = new MemoryStream())
+            {
+                stream.WriteByte(0x02);
+                stream.WriteByte(0x00);
+                stream.WriteByte(0x00);
+                stream.WriteByte(0x00);
+                stream.Position = 0;
+                Assert.False(Msg.IsValid(stream));
+            }
+        }
+
+        [Fact]
+        public void OpenThrowsAnExceptionIfTheStreamIsNotValid()
+        {
+            using (var stream = new MemoryStream())
+            {
+                stream.WriteByte(0x02);
+                stream.WriteByte(0x00);
+                stream.WriteByte(0x00);
+                stream.WriteByte(0x00);
+                stream.Position = 0;
+                Assert.Throws<InvalidDataException>(() => Msg.Open(stream));
+            }
+        }
+
+        [Fact]
+        public void ReadsRightAmountOfEntries() =>
+            FileOpenRead(FilePath, stream =>
+            {
+                var entries = Msg.Open(stream);
+                Assert.Equal(694, entries.Count);
+            });
+
+        [Fact]
+        public void ReadsIdCorrectly() =>
+            FileOpenRead(FilePath, stream =>
+            {
+                var entries = Msg.Open(stream);
+                Assert.Equal(20233, entries[0].Id);
+                Assert.Equal(20234, entries[1].Id);
+            });
+
+        [Fact]
+        public void ReadsTextDataCorrectly() =>
+            FileOpenRead(FilePath, stream =>
+            {
+                var entries = Msg.Open(stream);
+                Assert.Equal(19, entries[0].Data.Length);
+                Assert.Equal(7, entries[1].Data.Length);
+            });
     }
 }
