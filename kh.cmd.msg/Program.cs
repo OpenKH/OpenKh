8d99846a6c31760a97711a3fc3dd7885d2d5c4c1
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-04-14 20:55:33 +0100

    Add kh.cmd.msg tool

---
+++ kh.cmd.msg/Program.cs
@@ -0,0 +1,63 @@
+﻿using kh.kh2;
+using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace kh.cmd.msg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CommandLineApplication.Execute<Program>(args);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file {e.FileName} cannot be found. The program will now exit.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"FATAL ERROR: {e.Message}\n{e.StackTrace}");
+            }
+        }
+
+        [Option(ShortName = "i", LongName = "input", Description = "IDX input")]
+        public string Input { get; }
+
+        private void OnExecute()
+        {
+            var outputFileName = Input.Replace(".bar", ".xml", StringComparison.InvariantCultureIgnoreCase);
+
+            using (var stream = File.OpenRead(Input))
+            {
+                var barEntries = Bar.Open(stream);
+                foreach (Bar.Entry barEntry in barEntries)
+                {
+                    if (barEntry.Type == Bar.EntryType.Msg)
+                    {
+                        barEntry.Stream.Position = 0;
+                        ConvertMsgToXml(barEntry.Stream, outputFileName);
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void ConvertMsgToXml(Stream inStream, string fileName)
+        {
+            using (var outStream = File.Create(fileName))
+                ConvertMsgToXml(inStream, outStream);
+        }
+
+        private void ConvertMsgToXml(Stream inStream, Stream outStream)
+        {
+            var root = MsgSerializer.SerializeXEntries(Msg.Open(inStream));
+            var document = new XDocument(root);
+            new StreamWriter(outStream).Write(document.ToString());
+        }
+    }
+}
