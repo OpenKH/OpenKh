e1f8ad4075a1af0de84b0d9ffbc59daa15e5edaa
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-04-14 19:58:04 +0100

    Add a XML serializer for Msg

---
+++ kh.kh2/MsgSerializer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace kh.kh2
+{
+    public class MsgSerializer
+    {
+        private static Dictionary<MsgParser.Command, Func<MsgParser.Entry, XElement>> _serializer =
+            new Dictionary<MsgParser.Command, Func<MsgParser.Entry, XElement>>()
+            {
+                [MsgParser.Command.PrintIcon] = x => SerializePrintIcon(x),
+                [MsgParser.Command.PrintText] = x => new XElement("text", x.Text),
+            };
+
+        private static Dictionary<byte, string> _icons =
+            new Dictionary<byte, string>()
+            {
+                [0] = "consumable",
+                [1] = "tent",
+                [2] = "material",
+                [3] = "ability-off",
+                [4] = "weapon-keyblade",
+                [5] = "weapon-staff",
+                [6] = "weapon-shield"
+            };
+
+        public static XElement SerializeXEntries(IEnumerable<MsgParser.Entry> entries)
+        {
+            var element = new XElement("message");
+            foreach (var entry in entries)
+            {
+                element.Add(SerializeXEntry(entry));
+            }
+            return element;
+        }
+
+        public static XElement SerializeXEntry(MsgParser.Entry entry)
+        {
+            if (!_serializer.TryGetValue(entry.Command, out var funcSerializer))
+                throw new NotImplementedException($"The command {entry.Command} serialization is not implemented yet.");
+
+            return funcSerializer(entry);
+        }
+
+        private static XElement SerializePrintIcon(MsgParser.Entry entry)
+        {
+            byte value = entry.Data[0];
+            if (!_icons.TryGetValue(value, out var content))
+                throw new NotImplementedException($"The icon {value} is not implemented yet.");
+
+            return new XElement("icon", content);
+        }
+    }
+}
---
+++ kh.tests/kh2/MsgSerializerTests.cs
@@ -0,0 +1,53 @@
+﻿using kh.kh2;
+using Xunit;
+
+namespace kh.tests.kh2
+{
+    public class MsgSerializerTests
+    {
+        private const System.Xml.Linq.SaveOptions xmlFormatting = System.Xml.Linq.SaveOptions.DisableFormatting;
+
+        [Fact]
+        public void SerializeEntries()
+        {
+            var entry = new MsgParser.Entry
+            {
+                Command = MsgParser.Command.PrintText,
+                Text = "Hello world!"
+            };
+
+            var element = MsgSerializer.SerializeXEntries(new[] { entry });
+            Assert.Equal("<message><text>Hello world!</text></message>", element.ToString(xmlFormatting));
+        }
+
+        [Fact]
+        public void SerializeSimpleText()
+        {
+            var entry = new MsgParser.Entry
+            {
+                Command = MsgParser.Command.PrintText,
+                Text = "Hello world!"
+            };
+
+            var element = MsgSerializer.SerializeXEntry(entry);
+            Assert.Equal("<text>Hello world!</text>", element.ToString());
+        }
+
+        [Theory]
+        [InlineData(0, "consumable")]
+        [InlineData(1, "tent")]
+        [InlineData(2, "material")]
+        [InlineData(3, "ability-off")]
+        public void SerializeIcon(byte id, string expected)
+        {
+            var entry = new MsgParser.Entry
+            {
+                Command = MsgParser.Command.PrintIcon,
+                Data = new[] { id }
+            };
+
+            var element = MsgSerializer.SerializeXEntry(entry);
+            Assert.Equal($"<icon>{expected}</icon>", element.ToString());
+        }
+    }
+}
