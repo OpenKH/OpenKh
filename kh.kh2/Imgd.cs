5d015c399a75d4781ba6202d1814306f5345bfe2
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-04-22 18:17:40 +0100

    Fix Imgd.GetClut to return the correct swizzled palette

--- kh.kh2/Imgd.cs
+++ kh.kh2/Imgd.cs
@@ -126,10 +126,48 @@ namespace kh.kh2
 				default:
 					throw new NotSupportedException($"The format {format} is not supported.");
 			}
-		}
+        }
 
-        public byte[] GetClut() => Clut
-            .Select(x => (byte)Math.Min(byte.MaxValue, x * 2))
-            .ToArray();
-	}
+        public byte[] GetClut()
+        {
+            switch (format)
+            {
+                case 0x13: return GetClut8();
+                case 0x14: return GetClut4();
+                default:
+                    throw new NotSupportedException($"The format {format} is not supported.");
+            }
+        }
+
+        public byte[] GetClut4()
+        {
+            var data = new byte[16 * 4];
+            for (var i = 0; i < 16; i++)
+            {
+                data[i * 4 + 0] = Clut[(i & 15) * 4 + 0];
+                data[i * 4 + 1] = Clut[(i & 15) * 4 + 1];
+                data[i * 4 + 2] = Clut[(i & 15) * 4 + 2];
+                data[i * 4 + 3] = FromPs2Alpha(Clut[(i & 15) * 4 + 3]);
+            }
+
+            return data;
+        }
+
+        public byte[] GetClut8()
+        {
+            var data = new byte[256 * 4];
+            for (var i = 0; i < 256; i++)
+            {
+                var srcIndex = Ps2.Repl(i);
+                data[i * 4 + 0] = Clut[srcIndex * 4 + 0];
+                data[i * 4 + 1] = Clut[srcIndex * 4 + 1];
+                data[i * 4 + 2] = Clut[srcIndex * 4 + 2];
+                data[i * 4 + 3] = FromPs2Alpha(Clut[srcIndex * 4 + 3]);
+            }
+
+            return data;
+        }
+
+        public byte FromPs2Alpha(byte alpha) => (byte)Math.Min(alpha * 2, byte.MaxValue);
+    }
 }
