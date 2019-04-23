95d8316c10deb3c083f200f2edaf00034df03545
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-04-23 23:23:42 +0100

    Add Sequence.IsValid

---
+++ kh.kh2/Sequence.cs
@@ -0,0 +1,27 @@
+﻿using kh.common;
+using System;
+using System.IO;
+
+namespace kh.kh2
+{
+    public class Sequence
+    {
+        private static readonly uint MagicCodeValidator = 0x44514553U;
+        private static readonly long MinimumLength = 48L;
+
+        private Sequence(Stream stream)
+        {
+            if (!stream.CanRead || !stream.CanSeek)
+                throw new InvalidDataException($"Read or seek must be supported.");
+
+            if (stream.Length < MinimumLength)
+                throw new InvalidDataException("Invalid header length");
+        }
+
+        public static Sequence Open(Stream stream) =>
+            new Sequence(stream);
+
+        public static bool IsValid(Stream stream) =>
+            stream.Length >= MinimumLength && new BinaryReader(stream).PeekInt32() == MagicCodeValidator;
+    }
+}
---
+++ kh.tests/kh2/SequenceTests.cs
@@ -0,0 +1,50 @@
+﻿using kh.kh2;
+using System.IO;
+using Xunit;
+
+namespace kh.tests.kh2
+{
+    public class SequenceTests
+    {
+        [Fact]
+        public void IsValidTest() => new MemoryStream()
+            .Using(stream =>
+            {
+                stream.WriteByte(0x53);
+                stream.WriteByte(0x45);
+                stream.WriteByte(0x51);
+                stream.WriteByte(0x44);
+                stream.Write(new byte[44]);
+                stream.Position = 0;
+                Assert.True(Sequence.IsValid(stream));
+                Assert.Equal(0, stream.Position);
+            });
+
+        [Fact]
+        public void IsInvalidHeaderTest() => new MemoryStream()
+            .Using(stream =>
+            {
+                stream.WriteByte(0xCC);
+                stream.WriteByte(0xCC);
+                stream.WriteByte(0xCC);
+                stream.WriteByte(0xCC);
+                stream.Write(new byte[44]);
+                stream.Position = 0;
+                Assert.False(Sequence.IsValid(stream));
+                Assert.Equal(0, stream.Position);
+            });
+
+        [Fact]
+        public void IsInvalidStreamLengthTest() => new MemoryStream()
+            .Using(stream =>
+            {
+                stream.WriteByte(0x53);
+                stream.WriteByte(0x45);
+                stream.WriteByte(0x51);
+                stream.WriteByte(0x44);
+                stream.Position = 0;
+                Assert.False(Sequence.IsValid(stream));
+                Assert.Equal(0, stream.Position);
+            });
+    }
+}
