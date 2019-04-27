3e844e9578dd49f42a2691ab2e1834ca6777e390
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-04-27 20:04:32 +0100

    Title from the assembly's ProductName

--- kh.tools.layout/ViewModels/MainViewModel.cs
+++ kh.tools.layout/ViewModels/MainViewModel.cs
@@ -1,11 +1,16 @@
-﻿namespace kh.tools.layout.ViewModels
+﻿using kh.tools.common;
+
+namespace kh.tools.layout.ViewModels
 {
     public class MainViewModel
     {
+        public string Title { get; }
+
         public RendererViewModel Renderer { get; }
 
         public MainViewModel()
         {
+            Title = Utilities.GetApplicationName();
             Renderer = new RendererViewModel();
         }
     }
