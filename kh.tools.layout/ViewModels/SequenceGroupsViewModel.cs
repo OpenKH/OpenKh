22d906fa75d8e1dae578632d0ecee48587cce52a
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-05-01 18:22:06 +0100

    Add SequenceGroupsViewModel with a ComboBox to select the sequence group

---
+++ kh.tools.layout/ViewModels/SequenceGroupsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using kh.kh2;
+using kh.tools.layout.Models;
+using System.Linq;
+using Xe.Tools.Wpf.Models;
+
+namespace kh.tools.layout.ViewModels
+{
+    public class SequenceGroupsViewModel : GenericListModel<SequenceGroupModel>
+    {
+        public SequenceGroupsViewModel(Layout layout) :
+            base(layout.SequenceGroups.Select((_, i) => new SequenceGroupModel(layout, i)))
+        {
+
+        }
+
+        public int SelectedSequenceGroupIndex
+        {
+            get => SelectedIndex;
+            set => SelectedIndex = value;
+        }
+
+        protected override void OnSelectedItem(SequenceGroupModel item)
+        {
+            base.OnSelectedItem(item);
+            OnPropertyChanged(nameof(SelectedSequenceGroupIndex));
+        }
+
+        protected override SequenceGroupModel OnNewItem()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
