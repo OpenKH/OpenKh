37aed1632762c726917f0691d0dffeb7883468b9
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-05-20 22:10:33 +0100

    Draw single characters

--- OpenKh.Tools.Common/Controls/KingdomTextArea.cs
+++ OpenKh.Tools.Common/Controls/KingdomTextArea.cs
@@ -1,16 +1,22 @@
 ﻿using OpenKh.Tools.Common.Models;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace OpenKh.Tools.Common.Controls
 {
     public class KingdomTextArea : Control
     {
+        private const int FontWidth = 18;
+        private const int FontHeight = 24;
+
         public static DependencyProperty ContextProperty =>
             DependencyPropertyUtils.GetDependencyProperty<KingdomTextArea, KingdomTextContext>(nameof(Context), (o, x) => o.SetContext(x));
 
-        private ImageSource _imageFont;
+        private BitmapSource _imageFont;
+        private int _charPerRow;
 
         public KingdomTextContext Context
         {
@@ -37,17 +43,33 @@ namespace OpenKh.Tools.Common.Controls
             drawingContext.DrawRectangle(background, null, new Rect(0.0, 0.0, renderSize.Width, renderSize.Height));
         }
 
-        protected void DrawText(DrawingContext drawingContext, string text)
+        protected void DrawText(DrawingContext dc, string text)
         {
-            drawingContext.DrawImage(_imageFont, new Rect
-            (
-                0, 0, _imageFont.Width, _imageFont.Height
-            ));
+            double x = 0.0;
+            foreach (var ch in text)
+            {
+                DrawChar(dc, x, 0.0, 0);
+                x += FontWidth;
+            }
+        }
+
+        protected void DrawChar(DrawingContext dc, double x, double y, int index)
+        {
+            DrawChar(dc, x, y, index % _charPerRow, index / _charPerRow);
+        }
+
+        protected void DrawChar(DrawingContext dc, double x, double y, int sourceX, int sourceY)
+        {
+            var croppedBitmap = new CroppedBitmap(_imageFont,
+                new Int32Rect(sourceX * FontWidth, sourceY * FontHeight, FontWidth, FontHeight));
+
+            dc.DrawImage(croppedBitmap, new Rect(x, y, FontWidth, FontHeight));
         }
 
         private void SetContext(KingdomTextContext context)
         {
             _imageFont = context.Font.GetWindowsMediaImage();
+            _charPerRow = context.Font.Size.Width / FontWidth;
 
             InvalidateVisual();
         }
