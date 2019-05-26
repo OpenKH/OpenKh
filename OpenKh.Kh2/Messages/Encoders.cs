e16c8fe63d91a1c33f0e23d489952052cd405862
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-05-26 07:17:22 +0100

    Inline InternationalSystemEncoder.cs to Encoders.cs

--- OpenKh.Kh2/Messages/Encoders.cs
+++ OpenKh.Kh2/Messages/Encoders.cs
@@ -1,9 +1,19 @@
 ﻿using OpenKh.Kh2.Messages.Internals;
+using System.Collections.Generic;
 
 namespace OpenKh.Kh2.Messages
 {
     public static class Encoders
     {
+        internal class InternationalSystemEncoder : IMessageEncoder
+        {
+            private readonly IMessageDecode _decode = new InternationalSystemDecode();
+            private readonly IMessageEncode _encode = new InternationalSystemEncode();
+
+            public List<MessageCommandModel> Decode(byte[] data) => _decode.Decode(data);
+            public byte[] Encode(List<MessageCommandModel> messageCommands) => _encode.Encode(messageCommands);
+        }
+
         public static IMessageEncoder InternationalSystem { get; } =
             new InternationalSystemEncoder();
     }
