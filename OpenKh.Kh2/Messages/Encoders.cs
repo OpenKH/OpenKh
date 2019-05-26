8433ef018563c7651349ae8badbdd6325a97d3de
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-05-26 07:20:36 +0100

    Add incomplete JapaneseSystem encoder

--- OpenKh.Kh2/Messages/Encoders.cs
+++ OpenKh.Kh2/Messages/Encoders.cs
@@ -1,4 +1,5 @@
 ﻿using OpenKh.Kh2.Messages.Internals;
+using System;
 using System.Collections.Generic;
 
 namespace OpenKh.Kh2.Messages
@@ -14,7 +15,18 @@ namespace OpenKh.Kh2.Messages
             public byte[] Encode(List<MessageCommandModel> messageCommands) => _encode.Encode(messageCommands);
         }
 
+        internal class JapaneseSystemEncoder : IMessageEncoder
+        {
+            private readonly IMessageDecode _decode = new JapaneseSystemDecode();
+            private readonly IMessageEncode _encode = null;
+
+            public List<MessageCommandModel> Decode(byte[] data) => _decode.Decode(data);
+            public byte[] Encode(List<MessageCommandModel> messageCommands) => throw new NotImplementedException();
+        }
+
         public static IMessageEncoder InternationalSystem { get; } =
             new InternationalSystemEncoder();
+        public static IMessageEncoder JapaneseSystem { get; } =
+            new JapaneseSystemEncoder();
     }
 }
