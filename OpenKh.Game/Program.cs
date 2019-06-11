4d4b959e26a92c702719ff7ec584a9087248c9eb
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-06-12 01:49:20 +0300

    Add OpenKh.Game

---
+++ OpenKh.Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OpenKh.Game
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new OpenKhGame())
+                game.Run();
+        }
+    }
+}
