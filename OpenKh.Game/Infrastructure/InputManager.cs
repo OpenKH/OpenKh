02601b9e34696ddcf04fa27da9fd56b244f0d3a1
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-06-12 04:41:37 +0300

    Add Theater sub-menu

--- OpenKh.Game/Infrastructure/InputManager.cs
+++ OpenKh.Game/Infrastructure/InputManager.cs
@@ -12,6 +12,8 @@ namespace OpenKh.Game.Infrastructure
         public bool IsExit => pad.Buttons.Back == ButtonState.Pressed || keyboard.IsKeyDown(Keys.Escape);
         public bool IsDown => keyboard.IsKeyDown(Keys.Down) && !prevKeyboard.IsKeyDown(Keys.Down);
         public bool IsUp => keyboard.IsKeyDown(Keys.Up) && !prevKeyboard.IsKeyDown(Keys.Up);
+        public bool IsCircle => keyboard.IsKeyDown(Keys.K) && !prevKeyboard.IsKeyDown(Keys.K);
+        public bool IsCross => keyboard.IsKeyDown(Keys.L) && !prevKeyboard.IsKeyDown(Keys.L);
 
         public void Update()
         {
--- OpenKh.Game/States/TitleState.cs
+++ OpenKh.Game/States/TitleState.cs
@@ -18,31 +18,36 @@ namespace OpenKh.Game.States
         private const int Menu1OptionsCount = 2;
         private const int Menu2OptionsCount = 3;
         private const bool TheaterModeUnlocked = true;
-
+        private const int MainMenuNewGameOption = 0;
+        private const int MainMenuLoadOption = 1;
+        private const int MainMenuTheaterOption = 2;
         private ArchiveManager archiveManager;
         private InputManager inputManager;
         private MonoDrawing drawing;
-        private Layout titleLayout;
-        private IEnumerable<ISurface> titleImages;
         private LayoutRenderer layoutRendererFg;
         private LayoutRenderer layoutRendererBg;
+        private LayoutRenderer layoutRendererTheater;
+        private Dictionary<string, IEnumerable<ISurface>> cachedSurfaces;
         private int optionSelected;
+        private bool isInTheaterMenu;
 
         public void Initialize(StateInitDesc initDesc)
         {
             archiveManager = initDesc.ArchiveManager;
             inputManager = initDesc.InputManager;
             drawing = new MonoDrawing(initDesc.GraphicsDevice.GraphicsDevice);
+            cachedSurfaces = new Dictionary<string, IEnumerable<ISurface>>();
 
             archiveManager.LoadArchive("menu/fm/title.2ld");
-            titleLayout = archiveManager.Get<Layout>("titl");
-            titleImages = archiveManager.Get<Imgz>("titl")?.Images?.Select(x => drawing.CreateSurface(x));
+            archiveManager.LoadArchive("menu/fm/save.2ld");
 
-            layoutRendererBg = new LayoutRenderer(titleLayout, drawing, titleImages);
+            layoutRendererBg = CreateLayoutRenderer("titl");
             layoutRendererBg.SelectedSequenceGroupIndex = BackgroundScreen;
 
-            layoutRendererFg = new LayoutRenderer(titleLayout, drawing, titleImages);
-            layoutRendererFg.SelectedSequenceGroupIndex = TheaterModeUnlocked ? Menu2 : Menu1;
+            layoutRendererFg = CreateLayoutRenderer("titl");
+            SetOption(0);
+
+            layoutRendererTheater = CreateLayoutRenderer("even");
         }
 
         public void Destroy()
@@ -52,12 +57,48 @@ namespace OpenKh.Game.States
 
         public void Update(DeltaTimes deltaTimes)
         {
-            CheckLoop(layoutRendererBg);
-            CheckLoop(layoutRendererFg);
+            CheckTitlLoop(layoutRendererBg);
+            CheckTitlLoop(layoutRendererFg);
             
-            var pad = GamePad.GetState(PlayerIndex.One);
-            var keyboard = Keyboard.GetState();
+            if (isInTheaterMenu)
+            {
+                switch (layoutRendererTheater.SelectedSequenceGroupIndex)
+                {
+                    case 0:
+                        layoutRendererTheater.FrameIndex++;
+                        if (layoutRendererTheater.FrameIndex > 32)
+                        {
+                            layoutRendererTheater.FrameIndex = 0;
+                            layoutRendererTheater.SelectedSequenceGroupIndex = 1;
+                        }
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        layoutRendererTheater.FrameIndex++;
+                        if (layoutRendererTheater.FrameIndex > 32)
+                            isInTheaterMenu = false;
+                        break;
+                }
+            }
 
+            if (isInTheaterMenu == false)
+                ProcessInputMainMenu();
+            else
+                ProcessInputTheaterMenu();
+        }
+
+        public void Draw(DeltaTimes deltaTimes)
+        {
+            layoutRendererBg.Draw();
+            layoutRendererFg.Draw();
+
+            if (isInTheaterMenu)
+                layoutRendererTheater.Draw();
+        }
+
+        private void ProcessInputMainMenu()
+        {
             var currentOption = optionSelected;
             if (inputManager.IsUp)
             {
@@ -71,15 +112,33 @@ namespace OpenKh.Game.States
                 if (currentOption >= GetMaxOptionsCount())
                     currentOption = 0;
             }
+            else if (inputManager.IsCircle)
+            {
+                switch (currentOption)
+                {
+                    case MainMenuNewGameOption:
+                        break;
+                    case MainMenuLoadOption:
+                        break;
+                    case MainMenuTheaterOption:
+                        isInTheaterMenu = true;
+                        layoutRendererTheater.FrameIndex = 0;
+                        layoutRendererTheater.SelectedSequenceGroupIndex = 0;
+                        break;
+                }
+            }
 
             if (currentOption != optionSelected)
                 SetOption(currentOption);
         }
 
-        public void Draw(DeltaTimes deltaTimes)
+        private void ProcessInputTheaterMenu()
         {
-            layoutRendererBg.Draw();
-            layoutRendererFg.Draw();
+            if (inputManager.IsCross)
+            {
+                layoutRendererTheater.FrameIndex = 0;
+                layoutRendererTheater.SelectedSequenceGroupIndex = 2;
+            }
         }
 
         private int GetMaxOptionsCount() => TheaterModeUnlocked ? Menu2OptionsCount : Menu1OptionsCount;
@@ -90,7 +149,7 @@ namespace OpenKh.Game.States
             layoutRendererFg.SelectedSequenceGroupIndex = optionSelected + (TheaterModeUnlocked ? Menu2 : Menu1);
         }
 
-        private void CheckLoop(LayoutRenderer layout)
+        private void CheckTitlLoop(LayoutRenderer layout)
         {
             switch (layout.SelectedSequenceGroupIndex)
             {
@@ -108,6 +167,18 @@ namespace OpenKh.Game.States
                         layout.FrameIndex++;
                     break;
             }
+        }
+
+        private LayoutRenderer CreateLayoutRenderer(string resourceName) => CreateLayoutRenderer(resourceName, resourceName);
+
+        private LayoutRenderer CreateLayoutRenderer(string layoutResourceName, string imagesResourceName)
+        {
+            var layout = archiveManager.Get<Layout>(layoutResourceName);
+            if (!cachedSurfaces.TryGetValue(imagesResourceName, out var images))
+                images = cachedSurfaces[imagesResourceName] = archiveManager.Get<Imgz>(imagesResourceName)
+                    ?.Images?.Select(x => drawing.CreateSurface(x));
+
+            return new LayoutRenderer(layout, drawing, images);
         }
     }
 }
