57aa6b3dc6a5e77d58aa837c82b7e807274d48fd
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-06-16 14:37:42 +0300

    Add layout and sequence extensions to calculate the rectangle visibility

---
+++ OpenKh.Kh2/Extensions/LayoutExtensions.cs
@@ -0,0 +1,39 @@
+﻿using OpenKh.Common;
+using System;
+using System.Drawing;
+
+namespace OpenKh.Kh2.Extensions
+{
+    public static class LayoutExtensions
+    {
+        public static T AggregateSequenceGroup<T>(
+            this Layout layout,
+            int sequenceGroupIndex,
+            Func<T, int, T> aggregator,
+            T initialValue = default)
+        {
+            var value = initialValue;
+            var animGroup = layout.SequenceGroups[sequenceGroupIndex];
+
+            for (var i = 0; i < animGroup.L1Count; i++)
+                value = aggregator(value, animGroup.L1Index + i);
+
+            return value;
+        }
+
+        public static Rectangle GetVisibilityRectangleFromSequenceGroup(
+            this Layout layout, int sequenceGroupIndex) =>
+            layout.AggregateSequenceGroup<Rectangle>(sequenceGroupIndex, (x, i) =>
+                x.Union(layout.GetVisibilityRectangleFromSequenceProperty(i)));
+
+        public static Rectangle GetVisibilityRectangleFromSequenceProperty(
+            this Layout layout, int sequencePropertyIndex)
+        {
+            var sequenceProperty = layout.SequenceProperties[sequencePropertyIndex];
+            var sequence = layout.SequenceItems[sequenceProperty.SequenceIndex];
+
+            return sequence.GetVisibilityRectangleFromAnimationGroup(sequenceProperty.AnimationGroup)
+                .Traslate(sequenceProperty.PositionX, sequenceProperty.PositionY);
+        }
+    }
+}
---
+++ OpenKh.Tests/kh2/SequenceExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using OpenKh.Kh2;
+using OpenKh.Kh2.Extensions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OpenKh.Tests.kh2
+{
+    public class SequenceExtensionsTests
+    {
+        [Fact]
+        public void GetVisibilityRectangleForFrameExTest()
+        {
+            const int ExpectedX = 50;
+            const int ExpectedY = 70;
+            const int ExpectedWidth = 100;
+            const int ExpectedHeight = 200;
+
+            var rect = new Sequence.FrameEx
+            {
+                Left = ExpectedX,
+                Top = ExpectedY,
+                Right = ExpectedX + ExpectedWidth,
+                Bottom = ExpectedY + ExpectedHeight
+            }.GetVisibilityRectangle();
+
+            Assert.Equal(ExpectedX, rect.X);
+            Assert.Equal(ExpectedY, rect.Y);
+            Assert.Equal(ExpectedWidth, rect.Width);
+            Assert.Equal(ExpectedHeight, rect.Height);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0, 0, 0, 0)]
+        [InlineData(1, 0, 0, 0, 0, 0)]
+        [InlineData(0, 1, 10, 20, 20, 5)]
+        [InlineData(1, 1, 20, 20, 5, 5)]
+        [InlineData(3, 1, -20, 0, 20, 10)]
+        [InlineData(0, 2, 10, 20, 20, 5)]
+        [InlineData(2, 1, 10, 10, 20, 40)]
+        [InlineData(2, 2, -20, 0, 50, 50)]
+        [InlineData(2, 3, -20, 0, 50, 50)]
+        public void GetVisibilityRectangleForFrameGroupTest(
+            short frameExIndex, short frameExCount,
+            int expectedX, int expectedY,
+            int expectedWidth, int expectedHeight)
+        {
+            var sequence = new Sequence()
+            {
+                FrameGroups = new List<Sequence.FrameGroup>
+                {
+                    new Sequence.FrameGroup
+                    {
+                        Start = frameExIndex,
+                        Count = frameExCount
+                    }
+                },
+                FramesEx = new List<Sequence.FrameEx>
+                {
+                    new Sequence.FrameEx() { Left = 10, Top = 20, Right = 30, Bottom = 25 },
+                    new Sequence.FrameEx() { Left = 20, Top = 20, Right = 25, Bottom = 25 },
+                    new Sequence.FrameEx() { Left = 10, Top = 50, Right = 30, Bottom = 10 },
+                    new Sequence.FrameEx() { Left = -20, Top = 0, Right = 0, Bottom = 10 },
+                    new Sequence.FrameEx() { Left = 0, Top = 0, Right = 0, Bottom = 100 },
+                }
+            };
+
+            var rect = sequence.GetVisibilityRectangleForFrameGroup(0);
+
+            Assert.Equal(expectedX, rect.X);
+            Assert.Equal(expectedY, rect.Y);
+            Assert.Equal(expectedWidth, rect.Width);
+            Assert.Equal(expectedHeight, rect.Height);
+        }
+    }
+}
