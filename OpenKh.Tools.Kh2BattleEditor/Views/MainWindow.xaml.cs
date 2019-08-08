87e5f95d8875b86810a4047f989d360fc80cfee7
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-08 11:59:52 +0100

    Add foundation for Kh2 Battle Editor

---
+++ OpenKh.Tools.Kh2BattleEditor/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using OpenKh.Tools.Kh2BattleEditor.ViewModels;
+
+namespace OpenKh.Tools.Kh2BattleEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
