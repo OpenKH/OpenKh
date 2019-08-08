96e1b88d497ce412f62437b43bb826190d2f2002
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-08 11:59:52 +0100

    Add foundation for Kh2 Battle Editor

---
+++ OpenKh.Tools.Kh2BattleEditor/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Windows;
+using OpenKh.Tools.Common;
+using Xe.Tools;
+
+namespace OpenKh.Tools.Kh2BattleEditor.ViewModels
+{
+    public class MainViewModel : BaseNotifyPropertyChanged
+    {
+        private static string ApplicationName = Utilities.GetApplicationName();
+        private Window Window => Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive);
+        private string _fileName;
+
+        public string Title => $"{FileName ?? "untitled"} | {ApplicationName}";
+
+        private string FileName
+        {
+            get => _fileName;
+            set
+            {
+                _fileName = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+    }
+}
