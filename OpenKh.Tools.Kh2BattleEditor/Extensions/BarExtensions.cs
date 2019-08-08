0d106734fbba4d582ff7a1c73f68da42fbcc8b85
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-08 13:44:22 +0100

    Add skeleton for 00battle editing support

---
+++ OpenKh.Tools.Kh2BattleEditor/Extensions/BarExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using OpenKh.Kh2;
+
+namespace OpenKh.Tools.Kh2BattleEditor.Extensions
+{
+    public static class BarExtensions
+    {
+        public static Stream GetBattleStream(this IEnumerable<Bar.Entry> entries, string name) =>
+            // TODO throws exception if that entry is not found
+            entries.First(x => x.Name == name && x.Index == 0 && x.Type == Bar.EntryType.Binary).Stream;
+    }
+}
---
+++ OpenKh.Tools.Kh2BattleEditor/Views/EnmpView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OpenKh.Tools.Kh2BattleEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for EnmpView.xaml
+    /// </summary>
+    public partial class EnmpView : UserControl
+    {
+        public EnmpView()
+        {
+            InitializeComponent();
+        }
+    }
+}
