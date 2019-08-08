9c791739967d6536f48490eb55b78db8d5f896c1
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-08 12:45:21 +0100

    Add open and save editor functionalities for 00battle.bin

--- OpenKh.Tools.Kh2BattleEditor/ViewModels/MainViewModel.cs
+++ OpenKh.Tools.Kh2BattleEditor/ViewModels/MainViewModel.cs
@@ -1,7 +1,14 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
+using OpenKh.Common;
+using OpenKh.Kh2;
 using OpenKh.Tools.Common;
 using Xe.Tools;
+using Xe.Tools.Wpf.Commands;
+using Xe.Tools.Wpf.Dialogs;
 
 namespace OpenKh.Tools.Kh2BattleEditor.ViewModels
 {
@@ -10,6 +17,7 @@ namespace OpenKh.Tools.Kh2BattleEditor.ViewModels
         private static string ApplicationName = Utilities.GetApplicationName();
         private Window Window => Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive);
         private string _fileName;
+        private IEnumerable<Bar.Entry> _battleItems;
 
         public string Title => $"{FileName ?? "untitled"} | {ApplicationName}";
 
@@ -21,6 +29,108 @@ namespace OpenKh.Tools.Kh2BattleEditor.ViewModels
                 _fileName = value;
                 OnPropertyChanged(nameof(Title));
             }
+        }
+
+        public RelayCommand OpenCommand { get; }
+        public RelayCommand SaveCommand { get; }
+        public RelayCommand SaveAsCommand { get; }
+        public RelayCommand ExitCommand { get; }
+        public RelayCommand AboutCommand { get; }
+
+        public MainViewModel()
+        {
+            OpenCommand = new RelayCommand(x =>
+            {
+                var fd = FileDialog.Factory(Window, FileDialog.Behavior.Open, new[]
+                {
+                    ("00battle.bin", "bin"),
+                    ("BAR file", "bar"),
+                    ("All files", "*")
+                });
+
+                if (fd.ShowDialog() == true)
+                {
+                    OpenFile(fd.FileName);
+                }
+            }, x => true);
+
+            SaveCommand = new RelayCommand(x =>
+            {
+                if (!string.IsNullOrEmpty(FileName))
+                {
+                    SaveFile(FileName, FileName);
+                }
+                else
+                {
+                    SaveAsCommand.Execute(x);
+                }
+            }, x => true);
+
+            SaveAsCommand = new RelayCommand(x =>
+            {
+                var fd = FileDialog.Factory(Window, FileDialog.Behavior.Save);
+                if (fd.ShowDialog() == true)
+                {
+                    SaveFile(FileName, fd.FileName);
+                    FileName = fd.FileName;
+                }
+            }, x => true);
+
+            ExitCommand = new RelayCommand(x =>
+            {
+                Window.Close();
+            }, x => true);
+
+            AboutCommand = new RelayCommand(x =>
+            {
+                new AboutDialog(Assembly.GetExecutingAssembly()).ShowDialog();
+            }, x => true);
+        }
+
+        public bool OpenFile(string fileName) => File.OpenRead(fileName).Using(stream =>
+        {
+            if (!Bar.IsValid(stream))
+            {
+                MessageBox.Show(Window, $"{Path.GetFileName(fileName)} is not a valid BAR file.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            var items = Bar.Read(stream);
+
+            if (!Is00battle(items))
+            {
+                MessageBox.Show(Window, $"{Path.GetFileName(fileName)} does not appear to be a valid 00battle.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            LoadBattleItems(items);
+
+            FileName = fileName;
+            return true;
+        });
+
+        public void SaveFile(string previousFileName, string fileName)
+        {
+            File.Create(fileName).Using(stream =>
+            {
+                SaveBattleItems();
+                Bar.Write(stream, _battleItems);
+            });
+        }
+
+        private bool Is00battle(IEnumerable<Bar.Entry> entries)
+        {
+            return true;
+        }
+
+        private void LoadBattleItems(IEnumerable<Bar.Entry> entries)
+        {
+            _battleItems = entries;
+        }
+
+        private void SaveBattleItems()
+        {
+
         }
     }
 }
