1e3ca5a269523165b837dfb67130b9880e184541
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-15 23:27:31 +0100

    Add useful world names while editing

--- OpenKh.Tools.BbsEventTableEditor/ViewModels/EventViewModel.cs
+++ OpenKh.Tools.BbsEventTableEditor/ViewModels/EventViewModel.cs
@@ -1,9 +1,16 @@
 ﻿using OpenKh.Bbs;
+using System.Collections.Generic;
+using System.Linq;
+using Xe.Tools;
 
 namespace OpenKh.Tools.BbsEventTableEditor.ViewModels
 {
-    public class EventViewModel
+    public class EventViewModel : BaseNotifyPropertyChanged
     {
+        private static readonly Dictionary<byte, string> _worlds = Constants.WorldNames
+            .Select((x, i) => new { Name = x, Id = i })
+            .ToDictionary(x => (byte)x.Id, x => x.Name);
+
         public EventViewModel(Event @event)
         {
             Event = @event;
@@ -11,12 +18,63 @@ namespace OpenKh.Tools.BbsEventTableEditor.ViewModels
 
         public Event Event { get; private set; }
 
-        public short Id { get => Event.Id; set => Event.Id = value; }
-        public short EventIndex { get => Event.EventIndex; set => Event.EventIndex = value; }
-        public byte World { get => Event.World; set => Event.World = value; }
-        public byte Room { get => Event.Room; set => Event.Room = value; }
-        public short Unknown06 { get => Event.Unknown06; set => Event.Unknown06 = value; }
+        public IEnumerable<KeyValuePair<byte, string>> Worlds => _worlds;
 
+        public short Id
+        {
+            get => Event.Id;
+            set
+            {
+                Event.Id = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
 
+        public short EventIndex
+        {
+            get => Event.EventIndex;
+            set
+            {
+                Event.EventIndex = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public byte World
+        {
+            get => Event.World;
+            set
+            {
+                Event.World = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public byte Room
+        {
+            get => Event.Room;
+            set
+            {
+                Event.Room = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public short Unknown06
+        {
+            get => Event.Unknown06;
+            set
+            {
+                Event.Unknown06 = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string WorldName => World >= 0 && World < Constants.WorldNames.Length ?
+            Constants.WorldNames[World] : $"{World:X02}";
+
+        public string Name => $"{Id} {WorldName} {Room} {EventIndex}";
+
+        public override string ToString() => Name;
     }
 }
