0f13413712a9a353a38ed42932e1b2c78e8c17c8
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-18 12:58:55 +0100

    Basic text editing functionalities

--- OpenKh.Tools.CtdEditor/ViewModels/CtdViewModel.cs
+++ OpenKh.Tools.CtdEditor/ViewModels/CtdViewModel.cs
@@ -1,20 +1,75 @@
 ﻿using OpenKh.Bbs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xe.Tools.Wpf.Models;
 
 namespace OpenKh.Tools.CtdEditor.ViewModels
 {
-    public class CtdViewModel
+    public class CtdViewModel : GenericListModel<MessageViewModel>
     {
-        public Ctd Ctd { get; private set; }
+        private string _searchTerm;
+        private readonly Ctd _ctd;
+
+        public Ctd Ctd
+        {
+            get
+            {
+                _ctd.Entries1.Clear();
+                _ctd.Entries1.AddRange(Items.Select(x => x.Message));
+                return _ctd;
+            }
+        }
+
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set
+            {
+                _searchTerm = value;
+                PerformFiltering();
+            }
+        }
 
         public CtdViewModel() :
             this(new Ctd())
-        {
+        { }
 
+        public CtdViewModel(Ctd ctd) :
+            this(ctd, ctd.Entries1)
+        { }
+
+        private CtdViewModel(Ctd ctd, IEnumerable<Ctd.FakeEntry> messages) :
+            base(messages.Select(x => new MessageViewModel(ctd, x)))
+        {
+            _ctd = ctd;
         }
 
-        public CtdViewModel(Ctd ctd)
+        protected override void OnSelectedItem(MessageViewModel item)
         {
-            Ctd = ctd;
+            base.OnSelectedItem(item);
         }
+
+        protected override MessageViewModel OnNewItem() =>
+            new MessageViewModel(_ctd, new Ctd.FakeEntry
+            {
+                Id = (short)(Items.Max(x => x.Message.Id) + 1),
+                Data = new byte[0],
+                Entry2Index = 0,
+                Unknown02 = _ctd.Unknown
+            });
+
+        private void PerformFiltering()
+        {
+            if (string.IsNullOrWhiteSpace(_searchTerm))
+                Filter(FilterNone);
+            else
+                Filter(FilterTextAndId);
+        }
+
+        private bool FilterNone(MessageViewModel arg) => true;
+
+        private bool FilterTextAndId(MessageViewModel arg) =>
+            $"{arg.Id.ToString()} {arg.Text}".ToLower().Contains(SearchTerm.ToLower());
     }
 }
---
+++ OpenKh.Tools.CtdEditor/ViewModels/MessageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using OpenKh.Bbs;
+using System.Linq;
+using Xe.Tools;
+
+namespace OpenKh.Tools.CtdEditor.ViewModels
+{
+    public class MessageViewModel : BaseNotifyPropertyChanged
+    {
+        private readonly Ctd _ctd;
+
+        public MessageViewModel(Ctd ctd, Ctd.FakeEntry message)
+        {
+            _ctd = ctd;
+            Message = message;
+        }
+
+        public Ctd.FakeEntry Message { get; }
+
+        public short Id
+        {
+            get => Message.Id;
+            set
+            {
+                Message.Id = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Text
+        {
+            get => Message.Text;
+            set
+            {
+                Message.Text = value;
+                OnPropertyChanged(nameof(Title));
+                OnPropertyChanged(nameof(TextDump));
+            }
+        }
+
+        public string TextDump =>
+            string.Join(" ", Message.Data.Select(x => $"{x:X02}"));
+
+        public string Title => $"{Id}: {Text}";
+
+        public override string ToString() => Title;
+    }
+}
