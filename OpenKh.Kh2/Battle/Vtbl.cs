76f340a3f0014d40486b920e6a2ac99f7e51abbe
Rikux3 <barack030@web.de>
2019-08-18 20:35:45 +0200

    Bons, Przt, Vtbl now use BaseBattle

--- OpenKh.Kh2/Battle/Vtbl.cs
+++ OpenKh.Kh2/Battle/Vtbl.cs
@@ -6,41 +6,18 @@ namespace OpenKh.Kh2.Battle
 {
     public class Vtbl
     {
-        private class Header
+        [Data] public byte CharacterId { get; set; }
+        [Data] public byte ActionId { get; set; }
+        [Data] public byte Priority { get; set; }
+        [Data] public byte Unknown03 { get; set; } //Padding?
+        [Data(offset: 4, count: 5)] public List<Voice> Voices { get; set; }
+
+        public class Voice
         {
-            [Data] public int MagicCode { get; set; }
-            [Data] public int Count { get => Items.TryGetCount(); set => Items = Items.CreateOrResize(value); }
-            [Data] public List<RandomizationTable> Items { get; set; }
+            [Data] public sbyte VsbIndex { get; set; }
+            [Data] public sbyte Weight { get; set; } //(0 = normal random; 100 = guaranteed run)
         }
 
-        private readonly Header _header;
-
-        public List<RandomizationTable> RandomizationTables => _header.Items;
-
-        public class RandomizationTable
-        {
-            [Data] public byte CharacterId { get; set; }
-            [Data] public byte ActionId { get; set; }
-            [Data] public byte Priority { get; set; }
-            [Data] public byte Unknown03 { get; set; } //Padding?
-            [Data(offset: 4, count: 5)] public List<Voice> Voices { get; set; }
-
-            public class Voice
-            {
-                [Data] public sbyte VsbIndex { get; set; }
-                [Data] public sbyte Weight { get; set; } //(0 = normal random; 100 = guaranteed run)
-            }
-        }
-
-        public Vtbl(Stream stream)
-        {
-            BinaryMapping.SetMemberLengthMapping<Header>(nameof(Header.Items), (o, m) => o.Count);
-            _header = BinaryMapping.ReadObject<Header>(stream);
-        }
-
-        public void Write(Stream stream)
-        {
-            BinaryMapping.WriteObject(stream, _header);
-        }
+        public static BaseBattle<Vtbl> Read(Stream stream) => BaseBattle<Vtbl>.Read(stream);
     }
 }
