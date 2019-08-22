0ba3b6bfd8c418efcb33cfa735026ffd03137397
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-22 21:47:23 +0100

    Add tests for TM2 read and write

--- OpenKh.Tests/Imaging/Tm2Tests.cs
+++ OpenKh.Tests/Imaging/Tm2Tests.cs
@@ -1,6 +1,7 @@
 ﻿using OpenKh.Common;
 using OpenKh.Imaging;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace OpenKh.Tests.Imaging
@@ -19,5 +20,37 @@ namespace OpenKh.Tests.Imaging
 
                 Assert.Equal(expected, Tm2.IsValid(stream));
             });
+
+        [Theory]
+        [InlineData("image-8bit-128-128", 128, 128, PixelFormat.Indexed8)]
+        [InlineData("image-8bit-512-272", 512, 272, PixelFormat.Indexed8)]
+        [InlineData("image-32bit-480-279", 480, 279, PixelFormat.Rgba8888)]
+        public void ReadImagePropertiesTest(
+            string fileName,
+            int width,
+            int height,
+            PixelFormat pixelFormat) => File.OpenRead($"Imaging/res/{fileName}.tm2").Using(stream =>
+            {
+                var image = Tm2.Read(stream).Single();
+
+                Assert.Equal(width, image.Size.Width);
+                Assert.Equal(height, image.Size.Height);
+                Assert.Equal(pixelFormat, image.PixelFormat);
+            });
+
+        [Theory]
+        [InlineData("image-8bit-128-128")]
+        [InlineData("image-8bit-512-272")]
+        [InlineData("image-32bit-480-279")]
+        public void IsWritingBackCorrectly(string fileName) => File.OpenRead($"Imaging/res/{fileName}.tm2").Using(x =>
+            Helpers.AssertStream(x, stream =>
+            {
+                var images = Tm2.Read(stream);
+
+                var newStream = new MemoryStream();
+                Tm2.Write(newStream, images);
+
+                return newStream;
+            }));
     }
 }
