64cb8d8fbece5d61ce0e67e4c452779b9828d5a7
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-08-26 12:31:00 +0100

    Split ImageFormatService into multiple files

---
+++ OpenKh.Tools.ImageViewer/Services/ImageFormatService.ImageContainer.cs
@@ -0,0 +1,23 @@
+﻿using OpenKh.Imaging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenKh.Tools.ImageViewer.Services
+{
+    public partial class ImageFormatService
+    {
+        private class ImageContainer : IImageContainer
+        {
+            private readonly IImageRead[] _images;
+
+            public ImageContainer(IEnumerable<IImageRead> images)
+            {
+                _images = images.ToArray();
+            }
+
+            public int Count => _images.Length;
+            public IEnumerable<IImageRead> Images => _images;
+            public IImageRead GetImage(int index) => _images[index];
+        }
+    }
+}
---
+++ OpenKh.Tools.ImageViewer/Services/ImageFormatService.MultipleImageFormat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace OpenKh.Tools.ImageViewer.Services
+{
+    public partial class ImageFormatService
+    {
+        private class MultipleImageFormat : GenericImageFormat, IImageMultiple
+        {
+            private readonly Func<Stream, IImageContainer> read;
+            private readonly Action<Stream, IImageContainer> write;
+
+            public MultipleImageFormat(
+                string name,
+                string ext,
+                bool isCreationSupported,
+                Func<Stream, bool> isValid,
+                Func<Stream, IImageContainer> read,
+                Action<Stream, IImageContainer> write) :
+                base(name, ext, isCreationSupported, true, isValid)
+            {
+                this.read = read;
+                this.write = write;
+            }
+
+            public IImageContainer Read(Stream stream) => read(stream);
+            public void Write(Stream stream, IImageContainer image) => write(stream, image);
+        }
+    }
+}
