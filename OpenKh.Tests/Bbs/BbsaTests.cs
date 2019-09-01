22b137cfa0e2dd9ba4b57f745a466d06ec942673
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-09-01 13:19:45 +0200

    Get directory name from the directory hash

--- OpenKh.Tests/Bbs/BbsaTests.cs
+++ OpenKh.Tests/Bbs/BbsaTests.cs
@@ -29,5 +29,47 @@ namespace OpenKh.Tests.Bbs
             Assert.Equal(0x333CE - 100, offset);
             Assert.Equal(0x38C, size);
         }
+
+        [Theory]
+        [InlineData(0x0050414D, "arc/map")]
+        [InlineData(0x4E455645, "arc/event")]
+        [InlineData(0x30004350, "arc/pc_terra")]
+        [InlineData(0x00000000, "arc_")]
+        [InlineData(0x80000000, "sound/bgm")]
+        [InlineData(0xc0000000, "lua")]
+        [InlineData(0x90000000, "sound/se/common")]
+        [InlineData(0x91000000, "sound/se/event/ex")]
+        [InlineData(0x91010000, "sound/se/event/dp")]
+        [InlineData(0x920a0000, "sound/se/footstep/di")]
+        [InlineData(0xd0000000, "message/jp/system")]
+        [InlineData(0xd1200000, "message/en/map")]
+        [InlineData(0xa1570000, "sound/voice/fr/event/jf")]
+        public void CalculateDirectoryFromHash(uint hash, string directory)
+        {
+            var actual = Bbsa.GetDirectoryName(hash);
+
+            Assert.Equal(directory, actual);
+        }
+
+        [Theory]
+        [InlineData(0x0050414D, "arc/map")]
+        [InlineData(0x4E455645, "arc/event")]
+        [InlineData(0x30004350, "arc/pc_terra")]
+        [InlineData(0x00000000, "arc_")]
+        [InlineData(0x80000000, "sound/bgm")]
+        [InlineData(0xc0000000, "lua")]
+        [InlineData(0x90000000, "sound/se/common")]
+        [InlineData(0x91000000, "sound/se/event/ex")]
+        [InlineData(0x91010000, "sound/se/event/dp")]
+        [InlineData(0x920a0000, "sound/se/footstep/di")]
+        [InlineData(0xd0000000, "message/jp/system")]
+        [InlineData(0xd1200000, "message/en/map")]
+        [InlineData(0xa1570000, "sound/voice/fr/event/jf")]
+        public void CalculateHashFromDirectory(uint hash, string directory)
+        {
+            var actual = Bbsa.GetDirectoryHash(directory);
+
+            Assert.Equal(hash, actual);
+        }
     }
 }
