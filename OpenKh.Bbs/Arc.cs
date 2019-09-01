1dee4f9590d59c7ba0d7c839ed86622a28b69af5
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-09-01 14:19:37 +0200

    Add Arc parsing and writing

---
+++ OpenKh.Bbs/Arc.cs
@@ -0,0 +1,102 @@
+﻿using OpenKh.Common;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xe.BinaryMapper;
+
+namespace OpenKh.Bbs
+{
+    public static class Arc
+    {
+        private const int MagicCode = 0x435241;
+        private const int Version = 1;
+        private const int MetaEntrySize = 0x20;
+        private const int Alignment = 0x10;
+
+        private class Header
+        {
+            [Data] public int MagicCode { get; set; }
+            [Data] public short Version { get; set; }
+            [Data] public short EntryCount { get; set; }
+            [Data] public int Unused08 { get; set; }
+            [Data] public int Unused0c { get; set; }
+        }
+
+        private class MetaEntry
+        {
+            [Data] public int Unknown00 { get; set; }
+            [Data] public int Offset { get; set; }
+            [Data] public int Length { get; set; }
+            [Data] public int Unknown0c { get; set; }
+            [Data(Count = 16)] public string Name { get; set; }
+        }
+
+        public class Entry
+        {
+            public string Name { get; set; }
+            public byte[] Data { get; set; }
+        }
+
+        public static IEnumerable<Entry> Read(Stream stream)
+        {
+            var header = BinaryMapping.ReadObject<Header>(stream);
+
+            return Enumerable.Range(0, header.EntryCount)
+                .Select(x => BinaryMapping.ReadObject<MetaEntry>(stream))
+                .ToArray()
+                .Select(x =>
+                {
+                    System.Diagnostics.Debug.Assert(x.Unknown00 == 0,
+                        $"{nameof(x.Unknown00)} was expected to be 0 but was {x.Unknown00:X}");
+                    System.Diagnostics.Debug.Assert(x.Unknown00 == 0,
+                        $"{nameof(x.Unknown0c)} was expected to be 0 but was {x.Unknown0c:X}");
+
+                    return new Entry
+                    {
+                        Name = x.Name,
+                        Data = stream.SetPosition(x.Offset).ReadBytes(x.Length)
+                    };
+                })
+                .ToArray();
+        }
+
+        public static void Write(this IEnumerable<Entry> entries, Stream stream)
+        {
+            var myEntries = entries.ToArray();
+
+            stream.Position = 0;
+            BinaryMapping.WriteObject(stream, new Header
+            {
+                MagicCode = MagicCode,
+                Version = Version,
+                EntryCount = (short)myEntries.Length,
+                Unused08 = 0,
+                Unused0c = 0
+            });
+
+            var dataStartOffset = (int)stream.Position + myEntries.Length * MetaEntrySize;
+            foreach (var entry in myEntries)
+            {
+                BinaryMapping.WriteObject(stream, new MetaEntry
+                {
+                    Unknown00 = 0,
+                    Offset = dataStartOffset,
+                    Length = entry.Data.Length,
+                    Unknown0c = 0,
+                    Name = entry.Name
+                });
+
+                dataStartOffset += Helpers.Align(entry.Data.Length, Alignment);
+            }
+
+            foreach (var entry in myEntries)
+            {
+                stream.Write(entry.Data, 0, entry.Data.Length);
+                stream.AlignPosition(Alignment);
+            }
+        }
+
+        public static bool IsValid(Stream stream) =>
+            new BinaryReader(stream.SetPosition(0)).ReadInt32() == MagicCode;
+    }
+}
---
+++ OpenKh.Tests/Bbs/ArcTests.cs
@@ -0,0 +1,81 @@
+﻿using OpenKh.Bbs;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace OpenKh.Tests.Bbs
+{
+    public class ArcTests : Common
+    {
+        private static readonly string FileName = "Bbs/res/arctest.arc";
+
+        [Fact]
+        public void IsValidTest()
+        {
+            using (var stream = new MemoryStream())
+            {
+                stream.WriteByte(0x41);
+                stream.WriteByte(0x52);
+                stream.WriteByte(0x43);
+                stream.WriteByte(0x00);
+                Assert.True(Arc.IsValid(stream));
+            }
+        }
+
+        [Fact]
+        public void IsNotValidTest()
+        {
+            using (var stream = new MemoryStream())
+            {
+                stream.WriteByte(1);
+                stream.WriteByte(2);
+                stream.WriteByte(3);
+                stream.WriteByte(4);
+                Assert.False(Arc.IsValid(stream));
+            }
+        }
+
+        [Fact]
+        public void ReadCorrectAmountOfEntries() => FileOpenRead(FileName, stream =>
+        {
+            var entries = Arc.Read(stream);
+            Assert.Equal(2, entries.Count());
+        });
+
+        [Fact]
+        public void ReadEntryNamesCorrectly() => FileOpenRead(FileName, stream =>
+        {
+            var entries = Arc.Read(stream).ToArray();
+            Assert.Equal("TBoxDtTe.itb", entries[0].Name);
+            Assert.Equal("ColeDtTe.itc", entries[1].Name);
+        });
+
+        [Fact]
+        public void ReadEntryLengthCorrectly() => FileOpenRead(FileName, stream =>
+        {
+            var entries = Arc.Read(stream).ToArray();
+            Assert.Equal(0x3ec, entries[0].Data.Length);
+            Assert.Equal(0xbc, entries[1].Data.Length);
+        });
+
+        [Fact]
+        public void ReadEntryContentCorrectly() => FileOpenRead(FileName, stream =>
+        {
+            var entries = Arc.Read(stream).ToArray();
+            Assert.Equal(0x42, entries[0].Data[2]);
+            Assert.Equal(0x43, entries[1].Data[2]);
+        });
+
+        [Fact]
+        public void WritesBackCorrectly() => FileOpenRead(FileName, stream =>
+            Helpers.AssertStream(stream, x =>
+            {
+                var entries = Arc.Read(stream);
+
+                var outStream = new MemoryStream();
+                entries.Write(outStream);
+
+                return outStream;
+            }));
+    }
+}
