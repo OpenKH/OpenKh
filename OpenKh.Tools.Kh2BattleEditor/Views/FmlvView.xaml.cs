e92beaa4c9d7ec6f23d9db5cf2a41de439432e9c
Rikux3 <d.drankov@gmail.com>
2019-10-27 01:01:40 +0200

    Fmlv view + view model

---
+++ OpenKh.Tools.Kh2BattleEditor/Views/FmlvView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OpenKh.Tools.Kh2BattleEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for FmlvView.xaml
+    /// </summary>
+    public partial class FmlvView : UserControl
+    {
+        public FmlvView()
+        {
+            InitializeComponent();
+        }
+    }
+}
