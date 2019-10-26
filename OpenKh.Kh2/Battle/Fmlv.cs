e293a2e935c5d29a6486b24fb51467aa5e1472ef
Rikux3 <d.drankov@gmail.com>
2019-10-27 01:16:24 +0200

    remove old fmlv implementation in favor of new

--- OpenKh.Kh2/Battle/Fmlv.cs
+++ OpenKh.Kh2/Battle/Fmlv.cs
@@ -1,86 +1,27 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using Xe.BinaryMapper;
 
 namespace OpenKh.Kh2.Battle
 {
     public class Fmlv
     {
-        private class Header
+        [Data] public byte FormIdFormLevel { get; set; }
+        [Data] public byte LevelOfMovementAbility { get; set; }
+        [Data] public short AbilityId { get; set; }
+        [Data] public int Exp { get; set; }
+
+        public int FormId
         {
-            [Data] public int MagicCode { get; set; }
-            [Data] public int Count { get => Items.TryGetCount(); set => Items = Items.CreateOrResize(value); }
-            [Data] public List<Level> Items { get; set; }
+            get => FormIdFormLevel >> 4;
+            set => FormIdFormLevel = (byte)((FormIdFormLevel & 0x0F) | (value << 4));
         }
 
-        private readonly Header _header;
-
-        public List<Level> Levels => _header.Items;
-        public int Count => _header.Count;
-
-        public enum FormVanilla
+        public int FormLevel
         {
-            Summon,
-            Valor,
-            Wisdom,
-            Master,
-            Final,
-            AntiForm
-        }
-        public enum FormFm
-        {
-            Summon,
-            Valor,
-            Wisdom,
-            Limit,
-            Master,
-            Final,
-            AntiForm
-        }
-        public class Level
-        {
-            [Data] public byte Unk0 { get; set; }
-            [Data] public byte LevelMovementAbility { get; set; }
-            [Data] public short Ability { get; set; }
-            [Data] public int Exp { get; set; }
-
-            public int FormId
-            {
-                get => Unk0 >> 4;
-                set => Unk0 = (byte)((Unk0 & 0x0F) | (value << 4));
-            }
-
-            public int FormLevel
-            {
-                get => Unk0 & 0xF;
-                set => Unk0 = (byte)((Unk0 & 0xF0) | (value & 0xF));
-            }
-
-            public FormVanilla FormVanilla
-            {
-                get => (FormVanilla)FormId;
-                set => FormId = (int)value;
-            }
-
-            public FormFm FormFm
-            {
-                get => (FormFm)FormId;
-                set => FormId = (int)value;
-            }
-
-            public override string ToString() =>
-                $"{FormFm} {FormLevel}: EXP {Exp}, Ability {Ability:X04} Lv. {LevelMovementAbility}";
+            get => FormIdFormLevel & 0xF;
+            set => FormIdFormLevel = (byte)((FormIdFormLevel & 0xF0) | (value & 0xF));
         }
 
-        public Fmlv(Stream stream)
-        {
-            BinaryMapping.SetMemberLengthMapping<Header>(nameof(Header.Items), (o, m) => o.Count);
-            _header = BinaryMapping.ReadObject<Header>(stream);
-        }
-
-        public void Write(Stream stream)
-        {
-            BinaryMapping.WriteObject(stream, _header);
-        }
+        public static BaseBattle<Fmlv> Read(Stream stream) => BaseBattle<Fmlv>.Read(stream);
     }
 }
