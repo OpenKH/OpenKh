00d4c6096cee0bc8f9a728ccb1cb6a19a479a980
Rikux3 <d.drankov@gmail.com>
2019-10-26 23:57:23 +0200

    Fmlv in BinaryMapper format

---
+++ OpenKh.Kh2/Battle/Fmlv2.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Xe.BinaryMapper;
+
+namespace OpenKh.Kh2.Battle
+{
+    public class Fmlv2
+    {
+        [Data] public byte FormIdFormLevel { get; set; }
+        [Data] public byte LevelOfMovementAbility { get; set; }
+        [Data] public short AbilityId { get; set; }
+        [Data] public int Exp { get; set; }
+
+        public int FormId
+        {
+            get => FormIdFormLevel >> 4;
+            set => FormIdFormLevel = (byte)((FormIdFormLevel & 0x0F) | (value << 4));
+        }
+
+        public int FormLevel
+        {
+            get => FormIdFormLevel & 0xF;
+            set => FormIdFormLevel = (byte)((FormIdFormLevel & 0xF0) | (value & 0xF));
+        }
+
+        public static BaseBattle<Fmlv2> Read(Stream stream) => BaseBattle<Fmlv2>.Read(stream);
+    }
+}
