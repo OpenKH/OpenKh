47eec496013faf8814302d87eb854c671580e31a
Rikux3 <d.drankov@gmail.com>
2019-10-29 18:51:44 +0100

    BonsView (mostly c&p from EnmpView)

---
+++ OpenKh.Tools.Kh2BattleEditor/Views/BonsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OpenKh.Tools.Kh2BattleEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for BonsView.xaml
+    /// </summary>
+    public partial class BonsView : UserControl
+    {
+        public BonsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
