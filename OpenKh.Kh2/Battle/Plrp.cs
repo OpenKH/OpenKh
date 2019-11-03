7cd91da6d118566a80cf3310225cc54243a77ac5
Rikux3 <d.drankov@gmail.com>
2019-11-03 16:22:59 +0100

    first draft of plrp

---
+++ OpenKh.Kh2/Battle/Plrp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Xe.BinaryMapper;
+
+namespace OpenKh.Kh2.Battle
+{
+    public class Plrp
+    {
+        [Data] public short Difficulty { get; set; } //???
+        [Data] public byte Character { get; set; } //???
+        [Data] public byte Hp { get; set; }
+        [Data] public byte Mp { get; set; }
+        [Data] public byte Ap { get; set; }
+        [Data] public short Unknown06 { get; set; } //Padding?
+        [Data] public short Unknown08 { get; set; }
+        [Data] public short Unknown0a { get; set; }
+        [Data(Count = 58)] public List<short> Objects { get; set; }
+
+        public static BaseBattle<Plrp> Read(Stream stream) => BaseBattle<Plrp>.Read(stream);
+    }
+}
