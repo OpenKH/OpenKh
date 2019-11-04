30780b4cfd9c0ff857468e0cc7c14b6495664c22
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-11-04 21:14:24 +0000

    Simplified Kh2.System.Item

--- OpenKh.Kh2/System/Item.cs
+++ OpenKh.Kh2/System/Item.cs
@@ -1,48 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
+using Xe.BinaryMapper;
 
 namespace OpenKh.Kh2.System
 {
     public class Item
     {
-        /*/ FORMAT ItemTable:
-         * ushort Identifier;
-         * ushort TypeShort;
-         * byte[2] Flags;
-         * ushort StatEntry;
-         * ushort[2] ShopValues;
-         * ushort[2] Strings;
-         * ushort Command;
-         * ushort Slot;
-         * short Picture;
-         * byte[2] Icon;
-        /*/
-
-        /*/ FORMAT StatTable:
-         * ushort Identifier;
-         * ushort Ability;
-         * byte[4] GeneralValues; => Attack, Magic, Defense, AP
-         * byte UnknownByte;
-         * byte[4] ResistancePercentages; => Fire, Ice, Lightning, Dark
-         * byte UnknownByte;
-         * byte GeneralResistance;
-        /*/
-
-        /// <summary>
-        /// The item's specific type. 
-        /// </summary>
-        public enum ItemType
+        public enum Type : byte
         {
-            Normal = 0,
-            Special = 256,
-            Mega = 512
         }
 
-        /// <summary>
-        /// The item's category. Specifies if it is a Keyblade, Rapier, Potion, etc.
-        /// </summary>
-        public enum ItemCategory
+        public enum Category : byte
         {
             Item,
             MenuItem,
@@ -69,174 +37,83 @@ namespace OpenKh.Kh2.System
             Report
         }
 
-        /// <summary>
-        /// The item entry itself. Used by the game to know the item's technical properties.
-        /// </summary>
-        public struct ItemEntry
+        public class Entry
         {
-            public ushort Identifier;
-            internal ushort TypeShort;
-            public List<byte> Flags;
-            public ushort StatEntry;
-            public List<ushort> ShopValues;
-            public List<ushort> Strings;
-            public ushort Command;
-            public ushort Slot;
-            public short Picture;
-            public List<byte> Icon;
-            public ItemType Type;
-            public ItemCategory Category;
+            [Data] public ushort Id { get; set; }
+            [Data] public ushort Type { get; set; }
+            [Data] public byte Flag1 { get; set; }
+            [Data] public byte Flag2 { get; set; }
+            [Data] public ushort StatEntry { get; set; }
+            [Data] public ushort String1 { get; set; }
+            [Data] public ushort String2 { get; set; }
+            [Data] public ushort ShopValue1 { get; set; }
+            [Data] public ushort ShopValue2 { get; set; }
+            [Data] public ushort Command { get; set; }
+            [Data] public ushort Slot { get; set; }
+            [Data] public short Picture { get; set; }
+            [Data] public byte Icon1 { get; set; }
+            [Data] public byte Icon2 { get; set; }
         }
 
-        /// <summary>
-        /// The item's stat entry. Used by the game to know the item's stats.
-        /// </summary>
-        public struct StatEntry
+        public class Stat
         {
-            public ushort Identifier;
-            public ushort Ability;
-            public List<byte> GeneralValues;
-            internal byte Unknown01;
-            public List<byte> ResistanceValues;
-            internal byte Unknown02;
+            [Data] public ushort Id { get; set; }
+            [Data] public ushort Ability { get; set; }
+            [Data] public byte Attack { get; set; }
+            [Data] public byte Magic { get; set; }
+            [Data] public byte Defense { get; set; }
+            [Data] public byte AbilityPoints { get; set; }
+            [Data] public byte Unknown08 { get; set; }
+            [Data] public byte FireResistance { get; set; }
+            [Data] public byte IceResistance { get; set; }
+            [Data] public byte LightningResistance { get; set; }
+            [Data] public byte DarkResistance { get; set; }
+            [Data] public byte Unknown0d { get; set; }
+            [Data] public byte GeneralResistance { get; set; }
+            [Data] public byte Unknown { get; set; }
         }
 
-        /// <summary>
-        /// Entries from the first sub-table of the parsed ItemTable.
-        /// </summary>
-        public List<ItemEntry> ItemEntries = new List<ItemEntry>();
-
-        /// <summary>
-        /// Entries from the second sub-table of the parsed ItemTable.
-        /// </summary>
-        public List<StatEntry> StatEntries = new List<StatEntry>();
-
-        internal int MagicHeader = 2;
-        internal int ItemCount;
-        internal int StatsCount;
-
-        /// <summary>
-        /// Initializes an empty item table. Ought to be interesting.
-        /// </summary>
-        public Item()
+        private class SubItemReader<T>
         {
+            [Data] public int Id { get; set; }
+            [Data] public int Count { get => Items.TryGetCount(); set => Items = Items.CreateOrResize(value); }
+            [Data] public List<T> Items { get; set; }
 
+            static SubItemReader() => BinaryMapping.SetMemberLengthMapping<SubItemReader<T>>(nameof(Items), (o, m) => o.Count);
+
+            public static SubItemReader<T> Read(Stream stream) => BinaryMapping.ReadObject<SubItemReader<T>>(stream);
+
+            public void Write(Stream stream) => BinaryMapping.WriteObject(stream, this);
         }
 
-        /// <summary>
-        /// Reads item table data. Either as a file or as raw data.
-        /// </summary>
-        /// <param name="FileName">The location of the file. If null, the function will read the raw data instead.</param>
-        /// <param name="FileData">The raw data array of the file. If null, the function will read the file instead.</param>
-        public Item(string FileName = null, byte[] FileData = null)
+        [Data] public List<Entry> Items1 { get; set; }
+        [Data] public List<Stat> Items2 { get; set; }
+
+        public static Item Read(Stream stream)
         {
-            BinaryReader GeneralReader = null;
+            var one = SubItemReader<Entry>.Read(stream);
+            var two = SubItemReader<Stat>.Read(stream);
 
-            if (FileName != null && FileData != null)
-                throw new IOException("Only one parameter should be used, other should remain null.");
-
-            else if (FileName != null)
-                GeneralReader = new BinaryReader(new FileStream(FileName, FileMode.Open));
-
-            else if (FileData != null)
-                GeneralReader = new BinaryReader(new MemoryStream(FileData));
-
-            else
-                throw new IOException("Both parameters are null.");
-
-            #region File Check-Up
-            if (GeneralReader.ReadUInt32() != MagicHeader)
-                throw new InvalidDataException("Invalid Magic Header.");
-
-            ItemCount = GeneralReader.ReadInt32();
-            GeneralReader.BaseStream.Position = 8 + 24 * ItemCount;
-
-            if (GeneralReader.ReadUInt32() != 0)
-                throw new IOException("Start of the Property Table cannot be located. The file is invalid.");
-
-            StatsCount = GeneralReader.ReadInt32();
-            GeneralReader.BaseStream.Position += 16 * StatsCount;
-
-            if (GeneralReader.BaseStream.Position != GeneralReader.BaseStream.Length)
-                Console.WriteLine("The file length is incorrect. Not necessarily an invalid file.");
-            #endregion
-
-            #region Parsing Region
-            for (int i = 0; i < ItemCount; i++)
+            return new Item
             {
-                List<ushort> TemporaryList = null;
+                Items1 = one.Items,
+                Items2 = two.Items
+            };
+        }
 
-                GeneralReader.BaseStream.Position = 8 + 24 * i;
-
-                ItemEntry NewEntry = new ItemEntry();
-
-                NewEntry.Identifier = GeneralReader.ReadUInt16();
-                NewEntry.TypeShort = GeneralReader.ReadUInt16();
-                NewEntry.Flags = new List<byte>(GeneralReader.ReadBytes(2));
-                NewEntry.StatEntry = GeneralReader.ReadUInt16();
-
-                TemporaryList = new List<ushort>();
-                TemporaryList.Add(BitConverter.ToUInt16(GeneralReader.ReadBytes(2), 0));
-                TemporaryList.Add(BitConverter.ToUInt16(GeneralReader.ReadBytes(2), 0));
-                NewEntry.Strings = TemporaryList;
-
-                TemporaryList = new List<ushort>();
-                TemporaryList.Add(BitConverter.ToUInt16(GeneralReader.ReadBytes(2), 0));
-                TemporaryList.Add(BitConverter.ToUInt16(GeneralReader.ReadBytes(2), 0));
-                NewEntry.ShopValues = TemporaryList;
-
-                NewEntry.Command = GeneralReader.ReadUInt16();
-                NewEntry.Slot = GeneralReader.ReadUInt16();
-                NewEntry.Picture = GeneralReader.ReadInt16();
-                NewEntry.Icon = new List<byte>(GeneralReader.ReadBytes(2));
-
-                for (int z = 256; z < 1024; z = z ^ 2)
-                {
-                    if ((NewEntry.TypeShort & z) == z)
-                        NewEntry.Type = (ItemType)z;
-                    
-                    else if (z == 512)
-                        NewEntry.Type = ItemType.Normal;
-                }
-
-                for (int z = 0; z < 23; z++)
-                {
-                    if ((NewEntry.TypeShort & z) == z)
-                        NewEntry.Category = (ItemCategory)z;
-
-                    else if (z == 22)
-                        throw new InvalidDataException("Invalid Item Type.");
-                }
-
-                ItemEntries.Add(NewEntry);
-            }
-
-            for (int i = 0; i < StatsCount; i++)
+        public void Write(Stream stream)
+        {
+            new SubItemReader<Entry>
             {
-                GeneralReader.BaseStream.Position = (8 + 24 * ItemCount) + 8 + 16 * i;
+                Id = 6,
+                Items = Items1
+            }.Write(stream);
 
-                StatEntry NewEntry = new StatEntry();
-
-                NewEntry.Identifier = GeneralReader.ReadUInt16();
-                NewEntry.Ability = GeneralReader.ReadUInt16();
-                NewEntry.GeneralValues = new List<byte>(GeneralReader.ReadBytes(4));
-                NewEntry.Unknown01 = GeneralReader.ReadByte();
-                NewEntry.ResistanceValues = new List<byte>(GeneralReader.ReadBytes(4));
-                NewEntry.Unknown02 = GeneralReader.ReadByte();
-                NewEntry.ResistanceValues.Add(GeneralReader.ReadByte());
-
-                for (int z = 0; z < 5; z++)
-                {
-                    if (NewEntry.ResistanceValues[z] == 0)
-                        NewEntry.ResistanceValues[z] = 100;
-
-                    else
-                        NewEntry.ResistanceValues[z] = (byte)(NewEntry.ResistanceValues[z] % 100);
-                }
-
-                StatEntries.Add(NewEntry);
-            }
-            #endregion
+            new SubItemReader<Stat>
+            {
+                Id = 0,
+                Items = Items2
+            }.Write(stream);
         }
     }
 }
