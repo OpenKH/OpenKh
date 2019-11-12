f1cd99129061f606715564f63870ba6b69581752
Rikux3 <d.drankov@gmail.com>
2019-11-12 21:16:56 +0100

    Adjusted structure of Przt (Drop table)

--- OpenKh.Kh2/Battle/Przt.cs
+++ OpenKh.Kh2/Battle/Przt.cs
@@ -5,23 +5,22 @@ namespace OpenKh.Kh2.Battle
 {
     public class Przt
     {
-        [Data] public byte Index { get; set; }
-        [Data] public byte Unknown01 { get; set; }
-        [Data] public byte Unknown02 { get; set; }
-        [Data] public byte Unknown03 { get; set; }
-        [Data] public byte Unknown04 { get; set; }
-        [Data] public byte Unknown05 { get; set; }
-        [Data] public byte Unknown06 { get; set; }
-        [Data] public byte Unknown07 { get; set; }
-        [Data] public byte Unknown08 { get; set; }
-        [Data] public byte Unknown09 { get; set; }
-        [Data] public byte Unknown0a { get; set; }
-        [Data] public byte Unknown0b { get; set; }
-        [Data] public short DropItem1 { get; set; }
+        [Data] public ushort Id { get; set; }
+        [Data] public byte SmallHpOrbs { get; set; }
+        [Data] public byte BigHpOrbs { get; set; }
+        [Data] public byte BigMoneyOrbs { get; set; }
+        [Data] public byte MediumMoneyOrbs { get; set; }
+        [Data] public byte SmallMoneyOrbs { get; set; }
+        [Data] public byte SmallMpOrbs { get; set; }
+        [Data] public byte BigMpOrbs { get; set; }
+        [Data] public byte SmallDriveOrbs { get; set; }
+        [Data] public byte BigDriveOrbs { get; set; }
+        [Data] public byte Unknown0a { get; set; } // Padding?
+        [Data] public ushort DropItem1 { get; set; }
         [Data] public short DropItem1Percentage { get; set; }
-        [Data] public short DropItem2 { get; set; }
+        [Data] public ushort DropItem2 { get; set; }
         [Data] public short DropItem2Percentage { get; set; }
-        [Data] public short DropItem3 { get; set; }
+        [Data] public ushort DropItem3 { get; set; }
         [Data] public short DropItem3Percentage { get; set; }
 
         public static BaseBattle<Przt> Read(Stream stream) => BaseBattle<Przt>.Read(stream);
