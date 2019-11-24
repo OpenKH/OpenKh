4c22914116f3a45c91682e583d8be51858a481b3
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-11-24 22:44:59 +0000

    Update InputManager with new keys

--- OpenKh.Game/Infrastructure/InputManager.cs
+++ OpenKh.Game/Infrastructure/InputManager.cs
@@ -10,10 +10,21 @@ namespace OpenKh.Game.Infrastructure
         private KeyboardState prevKeyboard;
 
         public bool IsExit => pad.Buttons.Back == ButtonState.Pressed || keyboard.IsKeyDown(Keys.Escape);
-        public bool IsDown => keyboard.IsKeyDown(Keys.Down) && !prevKeyboard.IsKeyDown(Keys.Down);
-        public bool IsUp => keyboard.IsKeyDown(Keys.Up) && !prevKeyboard.IsKeyDown(Keys.Up);
+        public bool IsUp => Up && !prevKeyboard.IsKeyDown(Keys.Up);
+        public bool IsDown => Down && !prevKeyboard.IsKeyDown(Keys.Down);
+        public bool IsLeft => Left && !prevKeyboard.IsKeyDown(Keys.Left);
+        public bool IsRight => Right && !prevKeyboard.IsKeyDown(Keys.Right);
         public bool IsCircle => keyboard.IsKeyDown(Keys.K) && !prevKeyboard.IsKeyDown(Keys.K);
         public bool IsCross => keyboard.IsKeyDown(Keys.L) && !prevKeyboard.IsKeyDown(Keys.L);
+
+        public bool Up => keyboard.IsKeyDown(Keys.Up);
+        public bool Down => keyboard.IsKeyDown(Keys.Down);
+        public bool Left => keyboard.IsKeyDown(Keys.Left);
+        public bool Right => keyboard.IsKeyDown(Keys.Right);
+        public bool A => keyboard.IsKeyDown(Keys.A);
+        public bool D => keyboard.IsKeyDown(Keys.D);
+        public bool S => keyboard.IsKeyDown(Keys.S);
+        public bool W => keyboard.IsKeyDown(Keys.W);
 
         public void Update()
         {
