b1a1030d981bff4ed3228db986e93a3854144e66
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-11-27 22:00:55 +0000

    Add model parser (not complete!)

---
+++ OpenKh.Engine/Parsers/MdlxParser.cs
@@ -0,0 +1,19 @@
+﻿using khkh_xldMii.Mc;
+using khkh_xldMii.Mx;
+using OpenKh.Engine.Parsers.Kddf2;
+
+namespace OpenKh.Engine.Parsers
+{
+    public class MdlxParser
+    {
+        private readonly Mdlxfst _mdlx;
+        
+        public MdlxParser(Mdlxfst mdlx)
+        {
+            _mdlx = mdlx;
+            Models = System.Array.Empty<Model>();
+        }
+
+        public Model[] Models { get; }
+    }
+}
---
+++ OpenKh.Engine/Parsers/Model.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OpenKh.Engine.Parsers
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PositionColoredTextured
+    {
+        public float X, Y, Z;
+        public float U, V;
+        public int Color;
+    }
+
+    public class Model
+    {
+        public PositionColoredTextured[] Vertices { get; set; }
+    }
+}
