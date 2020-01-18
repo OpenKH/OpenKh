a7b425f4dce45441624a5fb2c3e63df01ef78ad4
Rikux3 <d.drankov@gmail.com>
2020-01-18 21:01:30 +0100

    Add JapaneseEventEncoder

--- OpenKh.Kh2/Messages/Encoders.cs
+++ OpenKh.Kh2/Messages/Encoders.cs
@@ -24,9 +24,20 @@ namespace OpenKh.Kh2.Messages
             public byte[] Encode(List<MessageCommandModel> messageCommands) => _encode.Encode(messageCommands);
         }
 
+        internal class JapaneseEventEncoder : IMessageEncoder
+        {
+            private readonly IMessageDecode _decode = new JapaneseEventDecode();
+            private readonly IMessageEncode _encode = new JapaneseEventEncode();
+
+            public List<MessageCommandModel> Decode(byte[] data) => _decode.Decode(data);
+            public byte[] Encode(List<MessageCommandModel> messageCommands) => _encode.Encode(messageCommands);
+        }
+
         public static IMessageEncoder InternationalSystem { get; } =
             new InternationalSystemEncoder();
         public static IMessageEncoder JapaneseSystem { get; } =
             new JapaneseSystemEncoder();
+        public static IMessageEncoder JapaneseEvent { get; } =
+            new JapaneseEventEncoder();
     }
 }
