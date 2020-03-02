476bd2e4d22ee34a43be589bf619f87a5be944a3
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-03-02 18:42:20 +0000

    Update collision editor

--- OpenKh.Tools.Kh2MapCollisionEditor/ViewModels/CollisionEditorViewModel.cs
+++ OpenKh.Tools.Kh2MapCollisionEditor/ViewModels/CollisionEditorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using OpenKh.Kh2;
 using System.Collections.Generic;
+using System.Linq;
 using Xe.Tools;
 
 namespace OpenKh.Tools.Kh2MapCollisionEditor.ViewModels
@@ -14,6 +15,8 @@ namespace OpenKh.Tools.Kh2MapCollisionEditor.ViewModels
         private Coct.Co5 c5Item;
         private Coct.Co6 c6Item;
         private Coct.Co7 c7Item;
+        private bool _allC2;
+        private bool _allC3;
 
         public Coct Coct
         {
@@ -26,19 +29,30 @@ namespace OpenKh.Tools.Kh2MapCollisionEditor.ViewModels
         }
 
         public List<Coct.Co1> C1 => _coct?.Collision1;
-        //public List<Coct.CollisionMesh> C2 => _coct?.Collision2;
-        //public List<Coct.Co3> C3 => _coct?.Collision3;
-        public List<Coct.Vector4> C4 => _coct?.CollisionVertices;
+        public IEnumerable<Coct.CollisionMesh> C2 => !AllC2 ? C1Item?.Meshes : C1.SelectMany(c1 => c1.Meshes);
+        public IEnumerable<Coct.Co3> C3 => !AllC3 ? C2Item?.Items : C1.SelectMany(c1 => c1.Meshes.SelectMany(c2 => c2.Items));
+        public IEnumerable<Coct.Vector4> C4 { get
+            {
+                if (C3Item == null) yield break;
+                yield return _coct?.CollisionVertices[C3Item.Vertex1];
+                yield return _coct?.CollisionVertices[C3Item.Vertex2];
+                yield return _coct?.CollisionVertices[C3Item.Vertex3];
+                if (C3Item.Vertex4 >= 0)
+                    yield return _coct?.CollisionVertices[C3Item.Vertex4];
+            } }
         public List<Coct.Co5> C5 => _coct?.Collision5;
         public List<Coct.Co6> C6 => _coct?.Collision6;
         public List<Coct.Co7> C7 => _coct?.Collision7;
 
-        public Coct.Co1 C1Item { get => c1Item; set { c1Item = value; OnPropertyChanged(); } }
-        public Coct.CollisionMesh C2Item { get => c2Item; set { c2Item = value; OnPropertyChanged(); } }
-        public Coct.Co3 C3Item { get => c3Item; set { c3Item = value; OnPropertyChanged(); } }
+        public Coct.Co1 C1Item { get => c1Item; set { c1Item = value; OnPropertyChanged(); OnPropertyChanged(nameof(C2)); } }
+        public Coct.CollisionMesh C2Item { get => c2Item; set { c2Item = value; OnPropertyChanged(); OnPropertyChanged(nameof(C3)); } }
+        public Coct.Co3 C3Item { get => c3Item; set { c3Item = value; OnPropertyChanged(); OnPropertyChanged(nameof(C4)); } }
         public Coct.Vector4 C4Item { get => c4Item; set { c4Item = value; OnPropertyChanged(); } }
         public Coct.Co5 C5Item { get => c5Item; set { c5Item = value; OnPropertyChanged(); } }
         public Coct.Co6 C6Item { get => c6Item; set { c6Item = value; OnPropertyChanged(); } }
         public Coct.Co7 C7Item { get => c7Item; set { c7Item = value; OnPropertyChanged(); } }
+
+        public bool AllC2 { get => _allC2; set { _allC2 = value; OnPropertyChanged(nameof(C2)); } }
+        public bool AllC3 { get => _allC3; set { _allC3 = value; OnPropertyChanged(nameof(C3)); } }
     }
 }
