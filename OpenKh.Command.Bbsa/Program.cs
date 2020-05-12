d5b03a2849ac898f84a7ecb3ef63dddbad12857d
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-05-13 00:00:22 +0100

    Proper CLI tool with arguments for OpenKh.Commands.Bbsa

--- OpenKh.Command.Bbsa/Program.cs
+++ OpenKh.Command.Bbsa/Program.cs
@@ -1,78 +1,136 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace OpenKh.Command.Bbsa
 {
+    [Command("OpenKh.Command.Bbsa")]
+    [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+    [Subcommand(typeof(ExtractCommand), typeof(ListCommand))]
     public class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var argument = args.FirstOrDefault() ?? Directory.GetCurrentDirectory();
             try
             {
-                ProcessArgument(argument);
+                return CommandLineApplication.Execute<Program>(args);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file {e.FileName} cannot be found. The program will now exit.");
+                return 2;
+            }
+            catch (ArchiveNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return 3;
             }
             catch (Exception e)
             {
-                Console.WriteLine($"ERROR: {e.Message}");
+                Console.WriteLine($"FATAL ERROR: {e.Message}\n{e.StackTrace}");
+                return -1;
             }
         }
 
-        private static void ProcessArgument(string argument)
+        protected int OnExecute(CommandLineApplication app)
         {
-            if (Directory.Exists(argument))
-            {
-                if (!DoesContainBbsa(argument))
-                    throw new ArchiveNotFoundException(argument, 0);
-                ExtractByFolder(argument);
-            }
-            else
-            {
-                ExtractByPattern(argument);
-            }
+            app.ShowHelp();
+            return 1;
         }
 
-        private static bool DoesContainBbsa(string path) =>
-            File.Exists(Path.Combine(path, "BBS0.DAT"));
+        private static string GetVersion()
+            => typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
 
-        private static void ExtractByFolder(string path) =>
-            ExtractByPattern(Path.Combine(path, "BBS"));
-
-        private static void ExtractByPattern(string prefix)
+        private class ExtractCommand
         {
-            var bbsaFileNames = Enumerable.Range(0, 5)
-                .Select(x => $"{prefix}{x}.DAT");
-            var outputDir = Path.Combine(Path.GetDirectoryName(prefix), Path.GetFileName(prefix));
+            [Required]
+            [DirectoryExists]
+            [Option(CommandOptionType.SingleValue, Description = "Required. Path where the various BBSAx.DAT files are located", ShortName = "i", LongName = "input")]
+            public string InputPath { get; set; }
 
-            ExtractArchives(bbsaFileNames, outputDir);
-        }
+            [Option(CommandOptionType.SingleValue, Description = "Path where the content will be extracted", ShortName = "o", LongName = "output")]
+            public string OutputDir { get; set; }
 
-        private static void ExtractArchives(IEnumerable<string> bbsaFileNames, string outputDir)
-        {
-            var streams = bbsaFileNames
-                .Select(x => File.OpenRead(x))
-                .ToArray();
+            [Option(CommandOptionType.SingleValue, Description = "Archive file name prefix. By default it is 'BBS'.", ShortName = "p", LongName = "prefix")]
+            public string ArchivePrefix { get; set; }
 
-            var bbsa = Bbs.Bbsa.Read(streams[0]);
-            foreach (var file in bbsa.Files)
+            protected int OnExecute(CommandLineApplication app)
             {
-                var name = file.CalculateNameWithExtension(i => streams[i]);
-                var bbsaFileStream = file.OpenStream(i => streams[i]);
-                if (bbsaFileStream == null)
-                    continue;
+                var prefix = ArchivePrefix ?? "BBS";
 
-                var destinationFileName = Path.Combine(outputDir, name);
-                var destinationFolder = Path.GetDirectoryName(destinationFileName);
-                if (!Directory.Exists(destinationFolder))
-                    Directory.CreateDirectory(destinationFolder);
+                if (!DoesContainBbsa(InputPath, prefix))
+                    throw new ArchiveNotFoundException(InputPath, 0);
 
-                Console.WriteLine(name);
+                var bbsaFileNames = Enumerable.Range(0, 5)
+                    .Select(x => Path.Combine(InputPath, $"{prefix}{x}.DAT"));
 
-                using (var outStream = File.Create(destinationFileName))
-                    bbsaFileStream.CopyTo(outStream);
+                var outputDir = OutputDir ?? Path.Combine(Path.GetDirectoryName(InputPath), prefix);
+
+                ExtractArchives(bbsaFileNames, outputDir);
+                return 0;
+            }
+
+            private static void ExtractArchives(IEnumerable<string> bbsaFileNames, string outputDir)
+            {
+                var streams = bbsaFileNames
+                    .Select(x => File.OpenRead(x))
+                    .ToArray();
+
+                var bbsa = Bbs.Bbsa.Read(streams[0]);
+                foreach (var file in bbsa.Files)
+                {
+                    var name = file.CalculateNameWithExtension(i => streams[i]);
+                    var bbsaFileStream = file.OpenStream(i => streams[i]);
+                    if (bbsaFileStream == null)
+                        continue;
+
+                    var destinationFileName = Path.Combine(outputDir, name);
+                    var destinationFolder = Path.GetDirectoryName(destinationFileName);
+                    if (!Directory.Exists(destinationFolder))
+                        Directory.CreateDirectory(destinationFolder);
+
+                    Console.WriteLine(name);
+
+                    using (var outStream = File.Create(destinationFileName))
+                        bbsaFileStream.CopyTo(outStream);
+                }
             }
         }
+        
+        private class ListCommand
+        {
+            [Required]
+            [DirectoryExists]
+            [Option(CommandOptionType.SingleValue, Description = "Required. Path where the various BBSAx.DAT files are located", ShortName = "i", LongName = "input")]
+            public string InputPath { get; set; }
+
+            [Option(CommandOptionType.SingleValue, Description = "Archive file name prefix. By default it is 'BBS'.", ShortName = "p", LongName = "prefix")]
+            public string ArchivePrefix { get; set; }
+
+            protected int OnExecute(CommandLineApplication app)
+            {
+                var prefix = ArchivePrefix ?? "BBS";
+
+                if (!DoesContainBbsa(InputPath, prefix))
+                    throw new ArchiveNotFoundException(InputPath, 0);
+
+                var bbsaFileName = Path.Combine(InputPath, $"{prefix}{0}.DAT");
+                using var stream = File.OpenRead(bbsaFileName);
+                var bbsa = Bbs.Bbsa.Read(stream);
+                foreach (var file in bbsa.Files)
+                {
+                    Console.WriteLine(file.Name);
+                }
+
+                return 0;
+            }
+        }
+
+        private static bool DoesContainBbsa(string path, string prefix) =>
+            File.Exists(Path.Combine(path, $"{prefix}{0}.DAT"));
     }
 }
