b6e73b0a7be2b3bcfa2e70a0afb2c39d384e2151
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-05-13 15:46:14 +0100

    Parse and write 00place.bin and place.bin correctly

--- OpenKh.Kh2/Places.cs
+++ OpenKh.Kh2/Places.cs
@@ -1,9 +1,7 @@
 ﻿using OpenKh.Common;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace OpenKh.Kh2
 {
@@ -11,37 +9,79 @@ namespace OpenKh.Kh2
     {
         public ushort MessageId { get; set; }
 
+        public List<ushort> SubPlaces { get; set; }
+
+        private static IEnumerable<ushort> ReadPlaceSubEntries(Stream stream)
+        {
+            while (true)
+            {
+                var ch = stream.ReadByte();
+                if (ch <= 0)
+                    yield break;
+
+                yield return (ushort)(ch | (stream.ReadByte() << 8));
+            }
+        }
+
         private static IEnumerable<Place> ReadPlaceEntry(Stream stream)
         {
             if (stream.Length < 4)
                 yield break;
 
-            var messageId = stream.ReadUInt16();
-            var startOffset = stream.ReadUInt16();
-            yield return new Place
-            {
-                MessageId = messageId
-            };
+            var placeCount = stream.SetPosition(2).ReadUInt16() / 4;
+            stream.SetPosition(0);
 
-            while (stream.Position < startOffset)
+            var placeIds = new List<ushort>(placeCount);
+            var offsetIds = new List<int>(placeCount);
+            for (var i = 0; i < placeCount; i++)
             {
+                placeIds.Add(stream.ReadUInt16());
+                offsetIds.Add(stream.ReadUInt16() + i * 4);
+            }
+
+            for (var i = 0; i < placeCount - 1; i++)
+            {
+                var hasSubPlaces = offsetIds[i] != offsetIds[i + 1];
+                var subPlaces = hasSubPlaces ?
+                    ReadPlaceSubEntries(stream.SetPosition(offsetIds[i])).ToList() :
+                    new List<ushort>();
+
                 yield return new Place
                 {
-                    MessageId = stream.ReadUInt16()
+                    MessageId = placeIds[i],
+                    SubPlaces = subPlaces
                 };
-
-                var offset = stream.ReadUInt16(); // skip
             }
+
+            yield return new Place
+            {
+                MessageId = placeIds[placeCount - 1],
+                SubPlaces = ReadPlaceSubEntries(stream.SetPosition(offsetIds[placeCount - 1])).ToList()
+            };
         }
 
         private static void Write(Stream stream, List<Place> places)
         {
-            var endOfFile = (ushort)(places.Count * 4);
+            var endOfFile = places.Count * 4;
             foreach (var place in places)
             {
                 stream.Write(place.MessageId);
-                stream.Write(endOfFile);
+                stream.Write((ushort)endOfFile);
+
                 endOfFile -= 4;
+                if (place.SubPlaces.Count > 0)
+                    endOfFile += place.SubPlaces.Count * 2 + 1;
+            }
+
+            foreach (var place in places)
+            {
+                // If no subplaces are found, then it's a place.bin and not a 00place.bin
+                if (place.SubPlaces.Count > 0)
+                {
+                    foreach (var subplace in place.SubPlaces)
+                        stream.Write(subplace);
+                    stream.Write((byte)0);
+                }
             }
         }
 
@@ -52,12 +92,14 @@ namespace OpenKh.Kh2
 
         public static void Write(Stream stream, Dictionary<string, List<Place>> entries)
         {
-            var barEntries = new List<Bar.Entry>();
-            barEntries.Add(new Bar.Entry
+            var barEntries = new List<Bar.Entry>
             {
-                Name = "tmp",
-                Stream = new MemoryStream()
-            });
+                new Bar.Entry
+                {
+                    Name = "tmp",
+                    Stream = new MemoryStream()
+                }
+            };
 
             foreach (var placeGroup in entries)
             {
--- OpenKh.Tests/kh2/PlaceTests.cs
+++ OpenKh.Tests/kh2/PlaceTests.cs
@@ -14,7 +14,7 @@ namespace OpenKh.Tests.kh2
         [InlineData(OldPlaceFileName)]
         public void HasTheRightAmountOfWorlds(string fileName) => Common.FileOpenRead(fileName, stream =>
         {
-            Assert.Equal(20, Place.Read(stream).Count);
+            Assert.Equal(19, Place.Read(stream).Count);
         });
 
         [Theory]
@@ -23,7 +23,6 @@ namespace OpenKh.Tests.kh2
         public void HasTheRightWorldNames(string fileName) => Common.FileOpenRead(fileName, stream =>
         {
             var places = Place.Read(stream);
-            Assert.Contains(places, x => x.Key == "tmp");
             Assert.Contains(places, x => x.Key == "al");
             Assert.Contains(places, x => x.Key == "bb");
             Assert.Contains(places, x => x.Key == "wm");
@@ -36,6 +35,18 @@ namespace OpenKh.Tests.kh2
         public void HasWorldTheRightAmountOfPlaces(string fileName) => Common.FileOpenRead(fileName, stream =>
         {
             Assert.Equal(16, Place.Read(stream)["bb"].Count);
+        });
+
+        [Fact]
+        public void ReadSubPlaces() => Common.FileOpenRead(OldPlaceFileName, stream =>
+        {
+            var places = Place.Read(stream)["bb"];
+            Assert.Equal(9, places[0].SubPlaces.Count);
+            Assert.Equal(0x4783, places[0].SubPlaces[0]);
+            Assert.Equal(0x8B83, places[0].SubPlaces[8]);
+
+            Assert.Single(places[15].SubPlaces);
+            Assert.Equal(0xB48B, places[15].SubPlaces[0]);
         });
 
         [Theory]
