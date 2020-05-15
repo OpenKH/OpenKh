2e7341904f1899161b361f58b10bfdf39b11bad0
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-05-15 20:01:41 +0100

    Add title screen support for EU and JP builds

--- OpenKh.Game/States/TitleState.cs
+++ OpenKh.Game/States/TitleState.cs
@@ -11,14 +11,98 @@ namespace OpenKh.Game.States
 {
     public class TitleState : IState
     {
-        private const int BackgroundScreen = 14;
-        private const int Menu1 = 0;
-        private const int Menu2 = 2;
-        private const int Menu1OptionsCount = 2;
-        private const int Menu2OptionsCount = 3;
         private const int MainMenuNewGameOption = 0;
         private const int MainMenuLoadOption = 1;
         private const int MainMenuTheaterOption = 2;
+        private const int MainMenuBackOption = 3;
+        private const int MainMenuMaxOptionCount = 4;
+
+        private class TitleLayout
+        {
+            public int FadeIn { get; set; }
+            public int Copyright { get; set; }
+            public int Intro { get; set; }
+            public int IntroSkip { get; set; }
+            public int NewGame { get; set; }
+            public int MenuOptionNewGame { get; set; }
+            public int MenuOptionLoad { get; set; }
+            public int MenuOptionTheater { get; set; }
+            public int MenuOptionBack { get; set; }
+            public bool HasTheater { get; set; }
+            public bool HasBack { get; set; }
+        }
+
+        private static readonly TitleLayout VanillaTitleLayout = new TitleLayout
+        {
+            Copyright = 8,
+            Intro = 11,
+            IntroSkip = 12,
+            NewGame = 13,
+            MenuOptionNewGame = 0,
+            MenuOptionLoad = 1,
+            MenuOptionTheater = 2,
+            MenuOptionBack = 3,
+            HasTheater = false,
+            HasBack = false,
+        };
+
+        private static readonly TitleLayout FinalMixTitleLayout = new TitleLayout
+        {
+            FadeIn = 17,
+            Copyright = 8,
+            Intro = 12,
+            IntroSkip = 14,
+            NewGame = 10, // but also 15?
+            MenuOptionNewGame = 0,
+            MenuOptionLoad = 1,
+            MenuOptionTheater = 4,
+            HasTheater = false,
+            HasBack = false,
+        };
+
+        private static readonly TitleLayout FinalMixTheaterTitleLayout = new TitleLayout
+        {
+            FadeIn = 17,
+            Copyright = 8,
+            Intro = 13,
+            IntroSkip = 14,
+            NewGame = 11, // but also 16?
+            MenuOptionNewGame = 2,
+            MenuOptionLoad = 3,
+            MenuOptionTheater = 4,
+            HasTheater = true,
+            HasBack = false,
+        };
+
+        private static readonly TitleLayout ReMixTitleLayout = new TitleLayout
+        {
+            FadeIn = 17,
+            Copyright = 8,
+            Intro = 12,
+            IntroSkip = 14,
+            NewGame = 10, // but also 15?
+            MenuOptionNewGame = 0,
+            MenuOptionLoad = 1,
+            MenuOptionTheater = 4,
+            MenuOptionBack = 18,
+            HasTheater = false,
+            HasBack = true,
+        };
+
+        private static readonly TitleLayout ReMixTheaterTitleLayout = new TitleLayout
+        {
+            FadeIn = 17,
+            Copyright = 8,
+            Intro = 13,
+            IntroSkip = 14,
+            NewGame = 11, // but also 15?
+            MenuOptionNewGame = 2,
+            MenuOptionLoad = 3,
+            MenuOptionTheater = 4,
+            MenuOptionBack = 19,
+            HasTheater = false,
+            HasBack = true,
+        };
 
         private Kernel _kernel;
         private ArchiveManager _archiveManager;
@@ -29,7 +113,8 @@ namespace OpenKh.Game.States
         private LayoutRenderer layoutRendererBg;
         private LayoutRenderer layoutRendererTheater;
         private Dictionary<string, IEnumerable<ISurface>> cachedSurfaces;
-        
+
+        private TitleLayout _titleLayout;
         private bool _isTheaterModeUnlocked;
         private int _optionSelected;
         private bool _isInTheaterMenu;
@@ -47,21 +132,23 @@ namespace OpenKh.Game.States
             _archiveManager.LoadArchive($"menu/{_kernel.Region}/title.2ld");
             _archiveManager.LoadArchive($"menu/{_kernel.Region}/save.2ld");
 
-            layoutRendererBg = CreateLayoutRenderer("titl");
-            layoutRendererBg.SelectedSequenceGroupIndex = BackgroundScreen;
-
-            layoutRendererFg = CreateLayoutRenderer("titl");
-
-            // it will not work for non-FM versions
+            _isTheaterModeUnlocked = true;
             if (_kernel.RegionId == Constants.RegionFinalMix)
             {
-                _isTheaterModeUnlocked = true;
-                layoutRendererTheater = CreateLayoutRenderer("even");
+                if (_isTheaterModeUnlocked)
+                    _titleLayout = FinalMixTheaterTitleLayout;
+                else
+                    _titleLayout = FinalMixTitleLayout;
             }
             else
-            {
-                _isTheaterModeUnlocked = false;
-            }
+                _titleLayout = VanillaTitleLayout;
+
+            layoutRendererBg = CreateLayoutRenderer("titl");
+            layoutRendererFg = CreateLayoutRenderer("titl");
+            layoutRendererBg.SelectedSequenceGroupIndex = _titleLayout.IntroSkip;
+
+            if (_titleLayout.HasTheater)
+                layoutRendererTheater = CreateLayoutRenderer("even");
 
             SetOption(0);
         }
@@ -117,17 +204,9 @@ namespace OpenKh.Game.States
         {
             var currentOption = _optionSelected;
             if (_inputManager.IsUp)
-            {
                 currentOption--;
-                if (currentOption < 0)
-                    currentOption = GetMaxOptionsCount() - 1;
-            }
             else if (_inputManager.IsDown)
-            {
                 currentOption++;
-                if (currentOption >= GetMaxOptionsCount())
-                    currentOption = 0;
-            }
             else if (_inputManager.IsCircle || _inputManager.IsCross)
             {
                 switch (currentOption)
@@ -146,6 +225,22 @@ namespace OpenKh.Game.States
                 }
             }
 
+            if (currentOption == MainMenuTheaterOption && !_titleLayout.HasTheater)
+                currentOption++;
+            if (currentOption == MainMenuBackOption && !_titleLayout.HasBack)
+                currentOption++;
+            if (currentOption >= MainMenuMaxOptionCount)
+                currentOption = 0;
+            if (currentOption < 0)
+            {
+                if (_titleLayout.HasBack)
+                    currentOption = MainMenuBackOption;
+                else if (_titleLayout.HasTheater)
+                    currentOption = MainMenuTheaterOption;
+                else
+                    currentOption = MainMenuLoadOption;
+            }
+
             if (currentOption != _optionSelected)
                 SetOption(currentOption);
         }
@@ -159,31 +254,44 @@ namespace OpenKh.Game.States
             }
         }
 
-        private int GetMaxOptionsCount() => _isTheaterModeUnlocked ? Menu2OptionsCount : Menu1OptionsCount;
-
         private void SetOption(int option)
         {
             _optionSelected = option;
-            layoutRendererFg.SelectedSequenceGroupIndex = _optionSelected + (_isTheaterModeUnlocked ? Menu2 : Menu1);
+
+            switch (_optionSelected)
+            {
+                case MainMenuNewGameOption:
+                    layoutRendererFg.SelectedSequenceGroupIndex = _titleLayout.MenuOptionNewGame;
+                    break;
+                case MainMenuLoadOption:
+                    layoutRendererFg.SelectedSequenceGroupIndex = _titleLayout.MenuOptionLoad;
+                    break;
+                case MainMenuTheaterOption:
+                    layoutRendererFg.SelectedSequenceGroupIndex = _titleLayout.MenuOptionTheater;
+                    break;
+                case MainMenuBackOption:
+                    layoutRendererFg.SelectedSequenceGroupIndex = _titleLayout.MenuOptionBack;
+                    break;
+            }
         }
 
         private void CheckTitlLoop(LayoutRenderer layout)
         {
-            switch (layout.SelectedSequenceGroupIndex)
+            var currentSequenceGroupIndex = layout.SelectedSequenceGroupIndex;
+
+            if (currentSequenceGroupIndex == _titleLayout.MenuOptionNewGame ||
+                currentSequenceGroupIndex == _titleLayout.MenuOptionLoad ||
+                currentSequenceGroupIndex == _titleLayout.MenuOptionTheater ||
+                currentSequenceGroupIndex == _titleLayout.MenuOptionBack)
             {
-                case Menu1:
-                case Menu1 + 1:
-                case Menu2:
-                case Menu2 + 1:
-                case Menu2 + 2:
-                    if (layout.FrameIndex > 178)
-                        layout.FrameIndex = 70;
+                if (layout.FrameIndex > 178)
+                    layout.FrameIndex = 70;
+                layout.FrameIndex++;
+            }
+            else if (currentSequenceGroupIndex == _titleLayout.IntroSkip)
+            {
+                if (layout.FrameIndex < 119)
                     layout.FrameIndex++;
-                    break;
-                case BackgroundScreen:
-                    if (layout.FrameIndex < 119)
-                        layout.FrameIndex++;
-                    break;
             }
         }
 
