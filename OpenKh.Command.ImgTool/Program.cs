339256e31da10a002e0bbd0419dcd61937cd9715
kenjiuno <h8mastre@gmail.com>
2020-05-29 20:31:45 +0900

    Making OpenKh.Command.ImgTool.

---
+++ OpenKh.Command.ImgTool/Program.cs
@@ -0,0 +1,157 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using OpenKh.Command.ImgTool.Utils;
+using OpenKh.Common;
+using OpenKh.Kh2;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace OpenKh.Command.ImgTool
+{
+    [Command("OpenKh.Command.MsgTool")]
+    [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+    [Subcommand(typeof(UnimdCommand), typeof(UnimzCommand), typeof(PackimdCommand))]
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                return CommandLineApplication.Execute<Program>(args);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file {e.FileName} cannot be found. The program will now exit.");
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"FATAL ERROR: {e.Message}\n{e.StackTrace}");
+                return 1;
+            }
+        }
+
+        protected int OnExecute(CommandLineApplication app)
+        {
+            app.ShowHelp();
+            return 1;
+        }
+
+
+        private static string GetVersion()
+            => typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+        [HelpOption]
+        [Command(Description = "Convert an imd file to a png file")]
+        private class UnimdCommand
+        {
+            [Required]
+            [FileExists]
+            [Argument(0, Description = "Input an imd file path")]
+            public string ImdFile { get; set; }
+
+            [Option(CommandOptionType.SingleValue, Description = "Save png to file path", ShortName = "o", LongName = "output")]
+            public string OutputPng { get; set; }
+
+            protected int OnExecute(CommandLineApplication app)
+            {
+                var inputFile = ImdFile;
+                var outputFile = OutputPng ?? Path.Combine(
+                    Path.GetDirectoryName(inputFile),
+                    "extracted",
+                    Path.GetFileName(
+                        Path.ChangeExtension(inputFile, ".png")
+                    )
+                );
+
+                Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
+
+                using (var stream = File.OpenRead(inputFile))
+                {
+                    var imgd = Imgd.Read(stream);
+                    var bitmap = ImgdBitmapUtil.ToBitmap(imgd);
+                    bitmap.Save(outputFile);
+                }
+                return 0;
+            }
+        }
+
+        [HelpOption]
+        [Command(Description = "Convert an imz file to png files")]
+        private class UnimzCommand
+        {
+            [Required]
+            [FileExists]
+            [Argument(0, Description = "Input an imz file path")]
+            public string ImzFile { get; set; }
+
+            [Option(CommandOptionType.SingleValue, Description = "Save png files to dir", ShortName = "o", LongName = "output")]
+            public string OutputDir { get; set; }
+
+            protected int OnExecute(CommandLineApplication app)
+            {
+                var inputFile = ImzFile;
+                var outputDir = OutputDir ?? Path.Combine(Path.GetDirectoryName(inputFile), "extracted");
+
+                using (var stream = File.OpenRead(inputFile))
+                {
+                    var pairs = Imgz.Read(stream).Select((imgd, index) => (imgd, index));
+
+                    Directory.CreateDirectory(outputDir);
+
+                    foreach (var (imgd, index) in pairs)
+                    {
+                        var outputFile = Path.Combine(outputDir, $"{Path.GetFileNameWithoutExtension(inputFile)}-{1 + index}.png");
+
+                        var bitmap = ImgdBitmapUtil.ToBitmap(imgd);
+                        bitmap.Save(outputFile);
+                    }
+                }
+                return 0;
+            }
+        }
+
+        [HelpOption]
+        [Command(Description = "Convert a png file to an imd file")]
+        private class PackimdCommand
+        {
+            [Required]
+            [FileExists]
+            [Argument(0, Description = "Input an png file path")]
+            public string PngFile { get; set; }
+
+            [Option(CommandOptionType.SingleValue, Description = "Save imd to file path", ShortName = "o", LongName = "output")]
+            public string OutputImd { get; set; }
+
+            [Option(CommandOptionType.SingleValue, Description = "Force bits per pixel: 4, 8, 32", ShortName = "b", LongName = "bpp")]
+            public int BitsPerPixel { get; set; }
+
+            protected int OnExecute(CommandLineApplication app)
+            {
+                var inputFile = PngFile;
+                var outputFile = OutputImd ?? Path.Combine(
+                    Path.GetDirectoryName(inputFile),
+                    "converted",
+                    Path.GetFileName(
+                        Path.ChangeExtension(inputFile, ".imd")
+                    )
+                );
+
+                Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
+
+                // Alpha enabled png → always 32 bpp
+                using (var bitmap = new Bitmap(inputFile))
+                {
+                    var imgd = ImgdBitmapUtil.ToImgd(bitmap, BitsPerPixel);
+                    File.Create(outputFile).Using(stream => imgd.Write(stream));
+                }
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ OpenKh.Command.ImgTool/Utils/ImgdBitmapUtil.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using OpenKh.Imaging;
+using OpenKh.Kh2;
+
+namespace OpenKh.Command.ImgTool.Utils
+{
+    class ImgdBitmapUtil
+    {
+        public static Bitmap ToBitmap(Imgd imgd)
+        {
+            switch (imgd.PixelFormat)
+            {
+                case Imaging.PixelFormat.Indexed4:
+                    {
+                        var bitmap = new Bitmap(imgd.Size.Width, imgd.Size.Height, System.Drawing.Imaging.PixelFormat.Format4bppIndexed);
+                        var dest = bitmap.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                        try
+                        {
+                            var sourceBits = imgd.GetData();
+                            var sourceWidth = imgd.Size.Width;
+                            var sourceStride = ((sourceWidth + 1) / 2) & (~1);
+                            for (int y = 0; y < bitmap.Height; y++)
+                            {
+                                Marshal.Copy(sourceBits, sourceStride * y, dest.Scan0 + dest.Stride * y, sourceStride);
+                            }
+                        }
+                        finally
+                        {
+                            bitmap.UnlockBits(dest);
+                        }
+
+                        {
+                            var clut = imgd.GetClut();
+                            var palette = bitmap.Palette;
+                            for (int index = 0; index < 16; index++)
+                            {
+                                palette.Entries[index] = Color.FromArgb(
+                                    clut[4 * index + 3],
+                                    clut[4 * index + 0],
+                                    clut[4 * index + 1],
+                                    clut[4 * index + 2]
+                                );
+                            }
+                            bitmap.Palette = palette;
+                        }
+
+                        return bitmap;
+                    }
+                case Imaging.PixelFormat.Indexed8:
+                    {
+                        var bitmap = new Bitmap(imgd.Size.Width, imgd.Size.Height, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+                        var dest = bitmap.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                        try
+                        {
+                            var sourceBits = imgd.GetData();
+                            var sourceWidth = imgd.Size.Width;
+                            for (int y = 0; y < bitmap.Height; y++)
+                            {
+                                Marshal.Copy(sourceBits, sourceWidth * y, dest.Scan0 + dest.Stride * y, sourceWidth);
+                            }
+                        }
+                        finally
+                        {
+                            bitmap.UnlockBits(dest);
+                        }
+
+                        {
+                            var clut = imgd.GetClut();
+                            var palette = bitmap.Palette;
+                            for (int index = 0; index < 256; index++)
+                            {
+                                palette.Entries[index] = Color.FromArgb(
+                                    clut[4 * index + 3],
+                                    clut[4 * index + 0],
+                                    clut[4 * index + 1],
+                                    clut[4 * index + 2]
+                                );
+                            }
+                            bitmap.Palette = palette;
+                        }
+
+                        return bitmap;
+                    }
+            }
+            throw new NotSupportedException($"{imgd.PixelFormat} not recognized!");
+        }
+
+        class ReadAs32bppPixels
+        {
+            public ReadAs32bppPixels(Bitmap bitmap)
+            {
+                Width = bitmap.Width;
+                Height = bitmap.Height;
+
+                var src = bitmap.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                var srcBits = new byte[src.Stride * src.Height];
+                try
+                {
+                    Marshal.Copy(src.Scan0, srcBits, 0, srcBits.Length);
+                }
+                finally
+                {
+                    bitmap.UnlockBits(src);
+                }
+
+                Pixels = new List<uint>(Width * Height);
+
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < Width; x++)
+                    {
+                        Pixels.Add(BitConverter.ToUInt32(srcBits, src.Stride * y + 4 * x));
+                    }
+                }
+
+            }
+
+            public List<uint> Pixels { get; }
+            public int Width { get; }
+            public int Height { get; }
+        }
+
+        class PaletteGenerator
+        {
+            public PaletteGenerator(List<uint> pixels, int maxColors)
+            {
+                MostUsedPixels = pixels
+                    .GroupBy(pixel => pixel)
+                    .OrderByDescending(group => group.Count())
+                    .Select(group => group.Key)
+                    .Take(maxColors)
+                    .ToArray();
+
+            }
+
+            public uint[] MostUsedPixels { get; }
+
+            public int FindNearest(uint pixel)
+            {
+                int found = Array.IndexOf(MostUsedPixels, pixel);
+                if (found == -1)
+                {
+                    var a = (byte)(pixel >> 0);
+                    var b = (byte)(pixel >> 8);
+                    var c = (byte)(pixel >> 16);
+                    var d = (byte)(pixel >> 24);
+
+                    int minDistance = int.MaxValue;
+                    for (int index = 0; index < MostUsedPixels.Length; index++)
+                    {
+                        var target = MostUsedPixels[index];
+                        var A = (byte)(target >> 0);
+                        var B = (byte)(target >> 0);
+                        var C = (byte)(target >> 0);
+                        var D = (byte)(target >> 0);
+                        var distance = Math.Abs((int)a - A) + Math.Abs((int)b - B) + Math.Abs((int)c - C) + Math.Abs((int)d - D);
+                        if (distance < minDistance)
+                        {
+                            found = index;
+                            minDistance = distance;
+                        }
+                    }
+                }
+                return found;
+            }
+        }
+
+        public static Imgd ToImgd(Bitmap bitmap, int bpp)
+        {
+            switch (bpp)
+            {
+                case 4:
+                    {
+                        var src = new ReadAs32bppPixels(bitmap);
+
+                        var newPalette = new PaletteGenerator(src.Pixels, 16);
+
+                        var destBits = new byte[src.Width * src.Height];
+                        var clut = new byte[4 * 16];
+
+                        for (int index = 0; index < newPalette.MostUsedPixels.Length; index++)
+                        {
+                            var pixel = newPalette.MostUsedPixels[index];
+
+                            clut[4 * index + 0] = (byte)(pixel >> 16);
+                            clut[4 * index + 1] = (byte)(pixel >> 8);
+                            clut[4 * index + 2] = (byte)(pixel >> 0);
+                            clut[4 * index + 3] = (byte)(pixel >> 24);
+                        }
+
+                        var srcPointer = 0;
+                        var destPointer = 0;
+
+                        for (int y = 0; y < src.Height; y++)
+                        {
+                            for (int x = 0; x < src.Width; x++)
+                            {
+                                var newPixel = newPalette.FindNearest(src.Pixels[srcPointer++]) & 15;
+                                if (0 == (x & 1))
+                                {
+                                    // hi byte
+                                    destBits[destPointer] = (byte)(newPixel << 4);
+                                }
+                                else
+                                {
+                                    // lo byte
+                                    destBits[destPointer++] |= (byte)(newPixel);
+                                }
+                            }
+                        }
+
+                        return Imgd.Create(bitmap.Size, Imaging.PixelFormat.Indexed4, destBits, clut, false);
+                    }
+                case 8:
+                    {
+                        var src = new ReadAs32bppPixels(bitmap);
+
+                        var newPalette = new PaletteGenerator(src.Pixels, 256);
+
+                        var destBits = new byte[src.Width * src.Height];
+                        var clut = new byte[4 * 256];
+
+                        for (int index = 0; index < newPalette.MostUsedPixels.Length; index++)
+                        {
+                            var pixel = newPalette.MostUsedPixels[index];
+
+                            clut[4 * index + 0] = (byte)(pixel >> 16);
+                            clut[4 * index + 1] = (byte)(pixel >> 8);
+                            clut[4 * index + 2] = (byte)(pixel >> 0);
+                            clut[4 * index + 3] = (byte)(pixel >> 24);
+                        }
+
+                        var srcPointer = 0;
+                        var destPointer = 0;
+
+                        for (int y = 0; y < src.Height; y++)
+                        {
+                            for (int x = 0; x < src.Width; x++)
+                            {
+                                destBits[destPointer++] = (byte)newPalette.FindNearest(src.Pixels[srcPointer++]);
+                            }
+                        }
+
+                        return Imgd.Create(bitmap.Size, Imaging.PixelFormat.Indexed8, destBits, clut, false);
+                    }
+            }
+            throw new NotSupportedException($"BitsPerPixel {bpp} not recognized!");
+        }
+
+        public static Imgd ToImgd(Bitmap bitmap)
+        {
+            switch (bitmap.PixelFormat)
+            {
+                case System.Drawing.Imaging.PixelFormat.Format4bppIndexed:
+                    {
+                        var destHeight = bitmap.Height;
+                        var destStride = (bitmap.Width + 1) & (~1);
+                        var destBits = new byte[destStride * destHeight];
+                        var src = bitmap.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.ReadOnly, bitmap.PixelFormat);
+                        try
+                        {
+                            for (int y = 0; y < bitmap.Height; y++)
+                            {
+                                Marshal.Copy(src.Scan0 + src.Stride * y, destBits, destStride * y, destStride);
+                            }
+                        }
+                        finally
+                        {
+                            bitmap.UnlockBits(src);
+                        }
+
+                        var clut = new byte[4 * 16];
+                        {
+                            var palette = bitmap.Palette;
+                            for (int index = 0; index < 16; index++)
+                            {
+                                var color = palette.Entries[index];
+
+                                clut[4 * index + 0] = color.R;
+                                clut[4 * index + 1] = color.G;
+                                clut[4 * index + 2] = color.B;
+                                clut[4 * index + 3] = color.A;
+                            }
+                            bitmap.Palette = palette;
+                        }
+
+                        return Imgd.Create(bitmap.Size, Imaging.PixelFormat.Indexed4, destBits, clut, false);
+                    }
+                case System.Drawing.Imaging.PixelFormat.Format8bppIndexed:
+                    {
+                        var destHeight = bitmap.Height;
+                        var destStride = bitmap.Width;
+                        var destBits = new byte[destStride * destHeight];
+                        var src = bitmap.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.ReadOnly, bitmap.PixelFormat);
+                        try
+                        {
+                            for (int y = 0; y < bitmap.Height; y++)
+                            {
+                                Marshal.Copy(src.Scan0 + src.Stride * y, destBits, destStride * y, destStride);
+                            }
+                        }
+                        finally
+                        {
+                            bitmap.UnlockBits(src);
+                        }
+
+                        var clut = new byte[4 * 256];
+                        {
+                            var palette = bitmap.Palette;
+                            for (int index = 0; index < 256; index++)
+                            {
+                                var color = palette.Entries[index];
+
+                                clut[4 * index + 0] = color.R;
+                                clut[4 * index + 1] = color.G;
+                                clut[4 * index + 2] = color.B;
+                                clut[4 * index + 3] = color.A;
+                            }
+                            bitmap.Palette = palette;
+                        }
+
+                        return Imgd.Create(bitmap.Size, Imaging.PixelFormat.Indexed4, destBits, clut, false);
+                    }
+            }
+            throw new NotSupportedException($"{bitmap.PixelFormat} not recognized!");
+        }
+    }
+}
