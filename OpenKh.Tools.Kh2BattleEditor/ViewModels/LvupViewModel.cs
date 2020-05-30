05fcd975a4b98a488504262618d3529c67f3b694
Rikux3 <d.drankov@gmail.com>
2020-05-31 01:48:15 +0200

    Lvup: Add views from LevelUp Editor

---
+++ OpenKh.Tools.Kh2BattleEditor/ViewModels/LvupViewModel.cs
@@ -0,0 +1,98 @@
+﻿using OpenKh.Kh2;
+using OpenKh.Kh2.Battle;
+using OpenKh.Tools.Kh2BattleEditor.Extensions;
+using OpenKh.Tools.Kh2BattleEditor.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xe.Tools.Wpf.Models;
+
+namespace OpenKh.Tools.Kh2BattleEditor.ViewModels
+{
+    public class LvupViewModel : GenericListModel<LvupViewModel.CharacterViewModel>, IBattleGetChanges
+    {
+        private const int DefaultType = 2;
+        private const string entryName = "lvup";
+        private Lvup _lvup;
+        public string EntryName => entryName;
+
+        public LvupViewModel(IEnumerable<Bar.Entry> entries) :
+            this(Lvup.Read(entries.GetBattleStream(entryName)))
+        {
+        }
+
+        public LvupViewModel(Lvup Lvup) :
+            this(Lvup.Characters)
+        {
+            _lvup = Lvup;
+        }
+
+        public LvupViewModel(IEnumerable<Lvup.Character> characters) : 
+            base(characters.Select((x, i) => new CharacterViewModel(x, i)))
+        {
+        }
+
+        public LvupViewModel() :
+            this(new Lvup
+            {
+                MagicCode = DefaultType,
+                Count = 14,
+                Unknown08 = new byte[0x30],
+                Characters = new List<Lvup.Character>()
+            })
+        {
+
+        }
+
+        public Stream CreateStream()
+        {
+            var stream = new MemoryStream();
+            new Lvup
+            {
+                MagicCode = _lvup.MagicCode,
+                Count = _lvup.Count,
+                Unknown08 = _lvup.Unknown08,
+                Characters = Items.Select(x => x.Character).ToList()
+            }.Write(stream);
+
+            return stream;
+        }
+
+        public class CharacterViewModel : GenericListModel<CharacterViewModel.LevelViewModel>
+        {
+            private int _index;
+            public Lvup.Character Character { get; set; }
+            public string Name => ((Lvup.PlayableCharacterType)_index).ToString();
+
+            public CharacterViewModel(Lvup.Character character, int index) :
+                base(character.Levels.Select((x, i) => new LevelViewModel(x, i)))
+            {
+                _index = index;
+                Character = character;
+            }
+
+            public class LevelViewModel
+            {
+                private Lvup.Character.Level _level;
+                private int _index;
+
+                public string Name => $"Level {_index + 1}";
+
+                public LevelViewModel(Lvup.Character.Level level, int index)
+                {
+                    _level = level;
+                    _index = index;
+                }
+
+                public int Exp { get => _level.Exp; set => _level.Exp = value; }
+                public byte Strength { get => _level.Strength; set => _level.Strength = value; }
+                public byte Magic { get => _level.Magic; set => _level.Magic = value; }
+                public byte Defense { get => _level.Defense; set => _level.Defense = value; }
+                public byte Ap { get => _level.Ap; set => _level.Ap = value; }
+                public short SwordAbility { get => _level.SwordAbility; set => _level.SwordAbility = value; }
+                public short ShieldAbility { get => _level.ShieldAbility; set => _level.ShieldAbility = value; }
+                public short StaffAbility { get => _level.StaffAbility; set => _level.StaffAbility = value; }
+            }
+        }
+    }
+}
---
+++ OpenKh.Tools.Kh2BattleEditor/Views/LvupView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OpenKh.Tools.Kh2BattleEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for LvupView.xaml
+    /// </summary>
+    public partial class LvupView : UserControl
+    {
+        public LvupView()
+        {
+            InitializeComponent();
+        }
+    }
+}
