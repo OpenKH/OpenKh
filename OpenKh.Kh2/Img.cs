90bbe4600f53ea1c1ec890e9abe90e4275df47e0
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-05-30 20:19:16 +0100

    Add Img compression algorithm

--- OpenKh.Kh2/Img.cs
+++ OpenKh.Kh2/Img.cs
@@ -147,5 +147,106 @@ namespace OpenKh.Kh2
 
             return dstData;
         }
+
+        public static byte[] Compress(byte[] srcData)
+        {
+            const int MaxWindowSize = 0x100;
+
+            var decompressedLength = srcData.Length;
+            var key = GetLeastUsedByte(srcData);
+
+            // the following buffer size covers the worst compression scenario possible.
+            var buffer = new byte[decompressedLength + decompressedLength / 2 + 5];
+            var length = 0;
+            int iSrc = 0;
+
+            while (iSrc < decompressedLength)
+            {
+                var ch = srcData[iSrc];
+                if (ch == key)
+                {
+                    buffer[length++] = 0;
+                    buffer[length++] = key;
+                    iSrc++;
+                    continue;
+                }
+
+                int mi = 0;
+                int matches = 0;
+                var windowSize = iSrc + MaxWindowSize < decompressedLength ? MaxWindowSize : (decompressedLength - iSrc);
+                for (var i = 1; i < windowSize; i++)
+                {
+                    for (int j = i, m = 0; j < windowSize; j++)
+                    {
+                        if (srcData[iSrc + j - i] == srcData[iSrc + j])
+                        {
+                            m++;
+                            if (j + 1 == windowSize)
+                            {
+                                if (matches <= m)
+                                {
+                                    matches = m;
+                                    mi = i;
+                                }
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (matches <= m)
+                            {
+                                matches = m;
+                                mi = i;
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                if (matches > 3)
+                {
+                    buffer[length++] = (byte)(matches - 3);
+                    buffer[length++] = (byte)mi;
+                    buffer[length++] = key;
+                    iSrc += matches;
+                }
+                else
+                {
+                    buffer[length++] = ch;
+                    iSrc++;
+                }
+            }
+
+            buffer[length++] = (byte)(decompressedLength >> 24);
+            buffer[length++] = (byte)(decompressedLength >> 16);
+            buffer[length++] = (byte)(decompressedLength >> 8);
+            buffer[length++] = (byte)(decompressedLength >> 0);
+            buffer[length++] = key;
+
+            var dstData = new byte[length];
+            Array.Copy(buffer, dstData, length);
+
+            return dstData;
+        }
+
+        private static byte GetLeastUsedByte(byte[] data)
+        {
+            var dictionary = new int[0x100];
+            foreach (var ch in data)
+                dictionary[ch]++;
+
+            var elementFound = int.MaxValue;
+            var indexFound = 1; // the least used byte is never zero
+            for (var i = 1; i < 0x100; i++)
+            {
+                if (dictionary[i] < elementFound)
+                {
+                    elementFound = dictionary[i];
+                    indexFound = i;
+                }
+            }
+
+            return (byte)indexFound;
+        }
     }
 }
