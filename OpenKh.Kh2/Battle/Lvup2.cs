0c03e9c5e40cda98ca3404f87cfd570e767aec3b
Rikux3 <d.drankov@gmail.com>
2020-05-31 01:27:17 +0200

    Lvup: commit this only for traceability
    Will be moved into Lvup next commit

--- OpenKh.Kh2/Battle/Lvup2.cs
+++ OpenKh.Kh2/Battle/Lvup2.cs
@@ -6,9 +6,14 @@ namespace OpenKh.Kh2.Battle
 {
     public class Lvup2
     {
+        [Data] public int MagicCode { get; set; }
+        [Data] public int Count { get; set; }
+        [Data(Count = 0x38)] public byte[] Unknown08 { get; set; }
+        [Data(Count = 13)] public List<Character> Characters { get; set; }
+
         public class Character
         {
-            [Data] public int Unknown00 { get; set; }
+            [Data] public int NumLevels { get; set; }
             [Data(Count = 99)] public List<Level> Levels { get; set; }
 
             public class Level
@@ -25,28 +30,25 @@ namespace OpenKh.Kh2.Battle
             }
         }
 
-        private class Header
+        public enum PlayableCharacterType
         {
-            [Data] public int MagicCode { get; set; }
-            [Data] public int Count { get => Characters.TryGetCount(); set => Characters = Characters.CreateOrResize(value); }
-            [Data(Count = 0x38)] public byte[] Unknown08 { get; set; }
-            [Data(Count = 13)] public List<Character> Characters { get; set; }
+            Sora,
+            Donald,
+            Goofy,
+            Mickey,
+            Auron,
+            PingMulan,
+            Aladdin,
+            Sparrow,
+            Biest,
+            Jack,
+            Simba,
+            Tron,
+            Riku
         }
 
+        public static Lvup2 Read(Stream stream) => BinaryMapping.ReadObject<Lvup2>(stream);
 
-        static Lvup2()
-        {
-            //BinaryMapping.SetMemberLengthMapping<Header>(nameof(Header.Characters), (o, m) => o.Count);
-        }
-
-        public static List<Character> Read(Stream stream) =>
-            BinaryMapping.ReadObject<Header>(stream).Characters;
-
-        //public static void Write(Stream stream, List<Character> entries) =>
-        //    BinaryMapping.WriteObject(stream, new Header
-        //    {
-        //        MagicCode = 1,
-        //        Entries = entries
-        //    });
+        public void Write(Stream stream) => BinaryMapping.WriteObject(stream, this);
     }
 }
