5f09fa49d2916e704061a921379d988d7bdc828d
Rikux3 <d.drankov@gmail.com>
2020-05-31 19:26:24 +0200

    Resolve another rebasing error

--- OpenKh.Tools.CtdEditor/App.xaml.cs
+++ OpenKh.Tools.CtdEditor/App.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-<<<<<<< HEAD:OpenKh.Tools.CtdEditor/App.xaml.cs
 namespace OpenKh.Tools.CtdEditor
-=======
-namespace OpenKh.Tools.Kh2BattleEditor
->>>>>>> 1dafc52c7e4bcd9acdd7f44d13b9926d871a7598:OpenKh.Tools.Kh2BattleEditor/App.xaml.cs
 {
     /// <summary>
     /// Interaction logic for App.xaml
