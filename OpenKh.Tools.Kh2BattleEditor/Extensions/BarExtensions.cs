283a5deadd108362d8dd33797be773854e7e44ba
Rikux3 <d.drankov@gmail.com>
2020-05-31 14:12:08 +0200

    Bar EntryTypes have been shifted, use EntryType List

--- OpenKh.Tools.Kh2BattleEditor/Extensions/BarExtensions.cs
+++ OpenKh.Tools.Kh2BattleEditor/Extensions/BarExtensions.cs
@@ -9,6 +9,6 @@ namespace OpenKh.Tools.Kh2BattleEditor.Extensions
     {
         public static Stream GetBattleStream(this IEnumerable<Bar.Entry> entries, string name) =>
             // TODO throws exception if that entry is not found
-            entries.First(x => x.Name == name && x.Index == 0 && x.Type == Bar.EntryType.Binary).Stream;
+            entries.First(x => x.Name == name && x.Index == 0 && x.Type == Bar.EntryType.List).Stream;
     }
 }
