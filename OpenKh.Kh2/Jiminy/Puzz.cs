60a35cf16eb145335dbce5a7cec4b2e05ecdcef5
Rikux3 <d.drankov@gmail.com>
2020-06-05 23:35:12 +0200

    Update Puzz structure with Rotation and Size

--- OpenKh.Kh2/Jiminy/Puzz.cs
+++ OpenKh.Kh2/Jiminy/Puzz.cs
@@ -4,18 +4,34 @@ namespace OpenKh.Kh2.Jiminy
 {
     public class Puzz
     {
-        public enum PieceSize : byte
+        public enum Rotation : byte
+        {
+            Fixed = 0x0,
+            Rotatable = 0x1,
+        }
+
+        public enum PuzzleSize : byte
         {
             Twelve = 0x0,
             FourtyEight = 0x1,
-            Twelve2 = 0x10,
-            FourtyEight2 = 0x11
         }
 
         [Data] public byte Id { get; set; }
-        [Data] public PieceSize Pieces { get; set; }
+        [Data] public byte Unk1 { get; set; }
         [Data] public ushort Name { get; set; }
         [Data] public ushort RewardItem { get; set; }
         [Data(Count = 0xA)] public string FileName { get; set; }
+
+        public int PieceRotation
+        {
+            get => Unk1 >> 4;
+            set => Unk1 = (byte)((Unk1 & 0x0F) | (value << 4));
+        }
+
+        public int Size
+        {
+            get => Unk1 & 0xF;
+            set => Unk1 = (byte)((Unk1 & 0xF0) | (value & 0xF));
+        }
     }
 }
