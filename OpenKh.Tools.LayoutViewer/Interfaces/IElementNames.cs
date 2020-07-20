b83d7e0248c2d0570cc0ce5fa94b67f9e2daf2c2
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-07-20 10:31:17 +0100

    Remove LayoutViewer

--- OpenKh.Tools.LayoutViewer/Interfaces/IElementNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OpenKh.Tools.LayoutViewer.Interfaces
-{
-    public interface IElementNames
-    {
-        string AnimationName { get; set; }
-        string SpriteName { get; set; }
-    }
-}
