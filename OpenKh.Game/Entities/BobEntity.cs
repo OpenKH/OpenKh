eb5509d5a17b53f405579ebd73767147cf90dabc
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-07-31 16:39:39 +0200

    Load BOB in the right position

--- OpenKh.Game/Entities/BobEntity.cs
+++ OpenKh.Game/Entities/BobEntity.cs
@@ -8,7 +8,7 @@ namespace OpenKh.Game.Entities
         public BobEntity(BobDescriptor bobDesc)
         {
             BobIndex = bobDesc.BobIndex;
-            Position = new Vector3(bobDesc.PositionX, bobDesc.PositionY, bobDesc.PositionZ);
+            Position = new Vector3(-bobDesc.PositionX, -bobDesc.PositionY, -bobDesc.PositionZ);
             Rotation = new Vector3(bobDesc.RotationX, bobDesc.RotationY, bobDesc.RotationZ);
             Scaling = new Vector3(bobDesc.ScalingX, bobDesc.ScalingY, bobDesc.ScalingZ);
         }
