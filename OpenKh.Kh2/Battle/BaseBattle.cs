fa0ca81abd4b38ec43374dfe4d353b2eb29dae55
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-08-03 13:52:30 +0200

    Fix failing tests due to a missing functionality in Xe.BinaryMapper 1.5

--- OpenKh.Kh2/Battle/BaseBattle.cs
+++ OpenKh.Kh2/Battle/BaseBattle.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Xe.BinaryMapper;
 
 namespace OpenKh.Kh2.Battle
 {
     public class BaseBattle<T>
+        where T : class
     {
         [Data] public int Id { get; set; }
-        [Data] public int Count { get => Items.TryGetCount(); set => Items = Items.CreateOrResize(value); }
-        [Data] public List<T> Items { get; set; }
+        [Data] public int Count { get; set; }
+        public List<T> Items { get; set; }
 
-        static BaseBattle() => BinaryMapping.SetMemberLengthMapping<BaseBattle<T>>(nameof(Items), (o, m) => o.Count);
+        public static BaseBattle<T> Read(Stream stream)
+        {
+            var baseTable = BinaryMapping.ReadObject<BaseBattle<T>>(stream);
+            baseTable.Items = Enumerable.Range(0, baseTable.Count)
+                .Select(_ => BinaryMapping.ReadObject<T>(stream))
+                .ToList();
+            return baseTable;
+        }
 
-        public static BaseBattle<T> Read(Stream stream) => BinaryMapping.ReadObject<BaseBattle<T>>(stream);
-
-        public void Write(Stream stream) => BinaryMapping.WriteObject(stream, this);
+        public void Write(Stream stream)
+        {
+            Count = Items.Count;
+            BinaryMapping.WriteObject(stream, this);
+            foreach (var item in Items)
+                BinaryMapping.WriteObject(stream, item);
+        }
     }
 }
--- OpenKh.Kh2/System/Ftst.cs
+++ OpenKh.Kh2/System/Ftst.cs
@@ -12,26 +12,10 @@ namespace OpenKh.Kh2.System
             [Data(Count = Constants.WorldCount)] public int[] Colors { get; set; }
         }
 
-        private class Header
-        {
-            [Data] public int MagicCode { get; set; }
-            [Data] public int Count { get => Entries.TryGetCount(); set => Entries = Entries.CreateOrResize(value); }
-            [Data] public List<Entry> Entries { get; set; }
-        }
-
-        static Ftst()
-        {
-            BinaryMapping.SetMemberLengthMapping<Header>(nameof(Header.Entries), (o, m) => o.Count);
-        }
-
         public static List<Entry> Read(Stream stream) =>
-            BinaryMapping.ReadObject<Header>(stream).Entries;
+            BaseTable<Entry>.Read(stream).Items;
 
         public static void Write(Stream stream, List<Entry> entries) =>
-            BinaryMapping.WriteObject(stream, new Header
-            {
-                MagicCode = 1,
-                Entries = entries
-            });
+            BaseTable<Entry>.Write(stream, 1, entries);
     }
 }
