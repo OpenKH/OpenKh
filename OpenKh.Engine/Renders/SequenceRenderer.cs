82bd7265907cfac3e911e56f2feecfad14e86985
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-08-18 15:43:08 +0200

    Fix sequence interpolation

--- OpenKh.Engine/Renders/SequenceRenderer.cs
+++ OpenKh.Engine/Renders/SequenceRenderer.cs
@@ -158,7 +158,7 @@ namespace OpenKh.Engine.Renderers
             if ((animation.Flags & Sequence.LinearInterpolationFlag) != 0)
                 t = (float)delta;
             else
-                t = (float)(delta * delta * delta);
+                t = (float)((Math.Sin(delta * Math.PI - Math.PI / 2.0) + 1.0) / 2.0);
 
             context.PositionX += Lerp(t, animation.TranslateXStart, animation.TranslateXEnd);
             context.PositionY += Lerp(t, animation.TranslateYStart, animation.TranslateYEnd);
