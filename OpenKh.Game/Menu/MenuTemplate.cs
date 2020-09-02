5157e6c951955d4b77f1b0fd222d169662eeb3c0
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-09-02 21:52:22 +0200

    Update MenuTemplate with an example of selectable options

--- OpenKh.Game/Menu/MenuTemplate.cs
+++ OpenKh.Game/Menu/MenuTemplate.cs
@@ -1,13 +1,31 @@
+using OpenKh.Engine.Renderers;
 using OpenKh.Game.Infrastructure;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenKh.Game.Menu
 {
     public class MenuTemplate : MenuBase
     {
+        private const int OptionCount = 3;
         private readonly int _stackIndex;
         private IAnimatedSequence _menuSeq;
+        private int _selectedOption;
 
-        public int SelectedIndex { get; set; }
+        public int SelectedOption
+        {
+            get => _selectedOption;
+            set
+            {
+                value = value < 0 ? OptionCount - 1 : value % OptionCount;
+                if (_selectedOption != value)
+                {
+                    _selectedOption = value;
+                    _menuSeq = InitializeMenu(true);
+                    _menuSeq.Begin();
+                }
+            }
+        }
         protected override bool IsEnd => _menuSeq.IsEnd;
 
         public MenuTemplate(
@@ -21,23 +39,42 @@ namespace OpenKh.Game.Menu
 
         private IAnimatedSequence InitializeMenu(bool skipIntro)
         {
-            return SequenceFactory.Create(new AnimatedSequenceDesc
-            {
-                SequenceIndexStart = skipIntro ? -1 : 133,
-                SequenceIndexLoop = 134,
-                SequenceIndexEnd = 135,
-                Flags = AnimationFlags.TextTranslateX |
-                    AnimationFlags.ChildStackHorizontally,
-                MessageText = $"Template {_stackIndex}"
-            });
+            return SequenceFactory.Create(Enumerable.Range(0, OptionCount)
+                .Select(i => new AnimatedSequenceDesc
+                {
+                    SequenceIndexStart = skipIntro ? -1 : 133,
+                    SequenceIndexLoop = SelectedOption == i ? 132 : 134,
+                    SequenceIndexEnd = 135,
+                    StackIndex = i,
+                    StackWidth = 0,
+                    StackHeight = AnimatedSequenceDesc.DefaultStacking,
+                    Flags = AnimationFlags.TextTranslateX |
+                        AnimationFlags.ChildStackHorizontally,
+                    MessageText = $"Template {_stackIndex}-{i}",
+                    Children = SelectedOption == i ? new List<AnimatedSequenceDesc>
+                    {
+                        new AnimatedSequenceDesc
+                        {
+                            SequenceIndexLoop = 25,
+                            TextAnchor = TextAnchor.BottomLeft,
+                            Flags = AnimationFlags.NoChildTranslationX,
+                        },
+                        new AnimatedSequenceDesc
+                        {
+                            SequenceIndexStart = skipIntro ? -1 : 27,
+                            SequenceIndexLoop = 28,
+                            SequenceIndexEnd = 29,
+                        }
+                    } : new List<AnimatedSequenceDesc>()
+                }));
         }
 
         protected override void ProcessInput(InputManager inputManager)
         {
             if (inputManager.IsMenuUp)
-                SelectedIndex--;
+                SelectedOption--;
             else if (inputManager.IsMenuDown)
-                SelectedIndex++;
+                SelectedOption++;
             else if (inputManager.IsCircle)
             {
                 Push(new MenuTemplate(
@@ -62,7 +99,7 @@ namespace OpenKh.Game.Menu
         protected override void MyUpdate(double deltaTime) =>
             _menuSeq.Update(deltaTime);
 
-        protected override void MyDraw() => 
+        protected override void MyDraw() =>
             _menuSeq.Draw(0, 0);
     }
 }
