863cc25bbbf1d7086ae0d1c209e1ddf6a2246257
Truthkey <verdadmodificada@hotmail.com>
2020-09-07 15:56:57 +0200

    Olo info and bbsa update.

--- OpenKh.Bbs/Olo.cs
+++ OpenKh.Bbs/Olo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Xe.BinaryMapper;
 
 namespace OpenKh.Bbs
@@ -9,20 +8,49 @@ namespace OpenKh.Bbs
     {
         public Dictionary<string, string> SpawnObjectList = new Dictionary<string, string>()
         {
-            {"p01ex00", "Terra (PC)"},
-            {"p02ex00", "Ventus (PC)"},
-            {"p03ex00", "Aqua (PC)"},
-            {"p11ex00", "Terra Armor (PC)"},
-            {"p12ex00", "Ventus Armor (PC)"},
-            {"p13ex00", "Aqua Armor (PC)"},
+            {"g01ex00", "Savepoint"},
+            {"g02ex00", "Examine Actor (To place on static object)"},
+            {"g03ex00", "Crown (Puzzle)"},
+
+            {"p01ex00", "Ventus (PC)"},
+            {"p02ex00", "Aqua (PC)"},
+            {"p03ex00", "Terra (PC)"},
+            {"p11ex00", "Ventus Armor (PC)"},
+            {"p12ex00", "Aqua Armor (PC)"},
+            {"p13ex00", "Terra Armor (PC)"},
+            {"p41ex00", "Ventus Armor Helmetless (PC)"},
+            {"p42ex00", "Aqua Armor Helmetless (PC)"},
+            {"p43ex00", "Terra Armor Helmetless (PC)"},
+
+            {"n10ex00", "Moogle Shop 1"},
+            {"n11ex00", "Moogle Shop 2"},
+            {"n12ex00", "Moogle Shop 3"},
+            {"n13ex00", "Moogle Shop 4"},
         };
 
         private const int MagicCode = 0x4F4C4F40; // Always @OLO
 
         private struct UnkStruct
         {
-            [Data] public int unk1 { get; set; }
+            [Data] public int unk1 { get; set; } // 0x1 for bosses - 0x2 execute special code. SHOP-SAVE-TALK NECESSARY FOR OBJECTS TO APPEAR
             [Data] public int ExtraDataOffset { get; set; } // Indicates offset where other info such as pos/rot/scale are defined.
+        }
+
+        private struct OLOInfo
+        {
+
+        }
+
+        // Y-Axis is UP.
+        private struct SpawnStruct
+        {
+            [Data] public int UnknownVal1 { get; set; }
+            [Data] public float PositionX { get; set; }
+            [Data] public float PositionY { get; set; }
+            [Data] public float PositionZ { get; set; }
+            [Data] public float RotationX { get; set; }
+            [Data] public float RotationY { get; set; }
+            [Data] public float RotationZ { get; set; } // Yaw
         }
 
         private readonly Header _header;
