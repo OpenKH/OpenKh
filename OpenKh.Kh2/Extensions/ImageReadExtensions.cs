255ea9bbc97a096ac186d6eece3a021225d429c0
kenjiuno <h8mastre@gmail.com>
2020-10-17 19:20:18 +0900

    Imgd.AsImgd() passthru

--- OpenKh.Kh2/Extensions/ImageReadExtensions.cs
+++ OpenKh.Kh2/Extensions/ImageReadExtensions.cs
@@ -7,25 +7,32 @@ namespace OpenKh.Kh2.Extensions
     {
         public static Imgd AsImgd(this IImageRead image, bool isSwizzled = false)
         {
-            switch (image.PixelFormat)
+            if (image is Imgd imgd)
             {
-                case PixelFormat.Rgba8888:
-                case PixelFormat.Rgbx8888:
-                    return new Imgd(image.Size, image.PixelFormat, SwapRAndB(image.GetData()), image.GetClut(), isSwizzled);
-                default:
-                    return new Imgd(image.Size, image.PixelFormat, image.GetData(), image.GetClut(), isSwizzled);
+                return imgd;
+            }
+            else
+            {
+                switch (image.PixelFormat)
+                {
+                    case PixelFormat.Rgba8888:
+                    case PixelFormat.Rgbx8888:
+                        return new Imgd(image.Size, image.PixelFormat, GetAsRGBA(image.GetData()), image.GetClut(), isSwizzled);
+                    default:
+                        return new Imgd(image.Size, image.PixelFormat, image.GetData(), image.GetClut(), isSwizzled);
+                }
             }
         }
 
-        private static byte[] SwapRAndB(byte[] inData)
+        private static byte[] GetAsRGBA(byte[] inData)
         {
             var outData = new byte[inData.Length];
             for (var x = 0; x < inData.Length; x += 4)
             {
-                outData[x + 0] = inData[x + 2];
-                outData[x + 1] = inData[x + 1];
-                outData[x + 2] = inData[x + 0];
-                outData[x + 3] = inData[x + 3];
+                outData[x + 0] = inData[x + 2]; // B to R
+                outData[x + 1] = inData[x + 1]; // G to G
+                outData[x + 2] = inData[x + 0]; // R to B
+                outData[x + 3] = inData[x + 3]; // A
             }
             return outData;
         }
--- OpenKh.Tests/kh2/ImageReadExtensionsTests.cs
+++ OpenKh.Tests/kh2/ImageReadExtensionsTests.cs
@@ -1,8 +1,11 @@
-﻿using OpenKh.Imaging;
+﻿using OpenKh.Common;
+using OpenKh.Imaging;
+using OpenKh.Kh2;
 using OpenKh.Kh2.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using Xunit;
 
@@ -25,7 +28,7 @@ namespace OpenKh.Tests.kh2
         }
 
         [Fact]
-        public void Rgba8888AsImgd()
+        public void SampleRgba8888AsImgd()
         {
             var it = ImageReadExtensions.AsImgd(
                 new SampleImager
@@ -54,6 +57,40 @@ namespace OpenKh.Tests.kh2
                     0x33, 0x22, 0x11, 0x44,
                 },
                 it.Data
+            );
+        }
+
+        /// <summary>
+        /// AsImgd() shouldn't double alpha channel data.
+        /// </summary>
+        [Fact]
+        public void Rgba8888ImgdAsImgdx3()
+        {
+            var imgd = File.OpenRead($"kh2/res/2x2x32.imd").Using(Imgd.Read)
+                .AsImgd()
+                .AsImgd()
+                .AsImgd();
+            Assert.Equal(new Size(2, 2), imgd.Size);
+            Assert.Equal(PixelFormat.Rgba8888, imgd.PixelFormat);
+            Assert.Equal(
+                new byte[] {
+                    // GetData(): B, G, R, A
+                    0xFF, 0x7F, 0x27, 0xFF,
+                    0x22, 0xB1, 0x4C, 0xFF,
+                    0x00, 0xA2, 0xE8, 0xFF,
+                    0xFF, 0xF2, 0x00, 0xFF,
+                },
+                imgd.GetData()
+            );
+            Assert.Equal(
+                new byte[] {
+                    // Data: R, G, B, A
+                    0x27, 0x7F, 0xFF, 0xFF,
+                    0x4C, 0xB1, 0x22, 0xFF,
+                    0xE8, 0xA2, 0x00, 0xFF,
+                    0x00, 0xF2, 0xFF, 0xFF,
+                },
+                imgd.Data
             );
         }
     }
