6a18ca4e947e3e2c912a33bed5e8cfa721556804
Topaz-Reality <topaz.maxwilliam@gmail.com>
2020-12-19 22:18:48 +0300

    Additions to KH2TextEditor

--- OpenKh.Tools.Kh2TextEditor/ViewModels/TextEditorViewModel.cs
+++ OpenKh.Tools.Kh2TextEditor/ViewModels/TextEditorViewModel.cs
@@ -1,4 +1,4 @@
-﻿using OpenKh.Engine.Renders;
+using OpenKh.Engine.Renders;
 using OpenKh.Kh2;
 using OpenKh.Kh2.Messages;
 using OpenKh.Tools.Common.Rendering;
@@ -8,7 +8,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using Xe.Tools;
+using Xe.Tools.Wpf.Commands;
+
+using ColorPickerWPF;
+using System.Windows.Media;
 
 namespace OpenKh.Tools.Kh2TextEditor.ViewModels
 {
@@ -22,6 +27,12 @@ namespace OpenKh.Tools.Kh2TextEditor.ViewModels
         private string _searchTerm;
         private RenderingMessageContext textContext;
         private bool _showErrors;
+
+        public RelayCommand HandleReturn { get; }
+        public RelayCommand HandleAddition { get; }
+        public RelayCommand HandleRemoval { get; }
+        public RelayCommand HandleComm { get; }
+        public RelayCommand HandleIcon { get; }
 
         public List<Msg.Entry> MessageEntries
         {
@@ -56,6 +67,7 @@ namespace OpenKh.Tools.Kh2TextEditor.ViewModels
                 OnPropertyChanged(nameof(Text));
             }
         }
+
 
         public string Text
         {
@@ -127,6 +139,66 @@ namespace OpenKh.Tools.Kh2TextEditor.ViewModels
             Drawing = new SpriteDrawingDirect3D();
             CurrentMessageEncoder = Encoders.InternationalSystem;
             _messages = new MessagesModel(this, this, new Msg.Entry[] { });
+
+            HandleReturn = new RelayCommand(x =>
+            {
+                Text += "{:newline}";                
+                SelectedItem = SelectedItem;
+            });
+
+            HandleRemoval = new RelayCommand(x =>
+            {
+                MessageEntries.RemoveAll(x => x.Id == SelectedItem.Id);
+                ResetMessagesView();
+            });
+
+            HandleAddition = new RelayCommand(x =>
+            {
+                int _id = MessageEntries[MessageEntries.Count - 1].Id;
+                MessageEntries.Add(new Msg.Entry() { Id = _id + 1, Data = new byte[] { 0x33, 0x2E, 0x38, 0x32, 0x00 } });
+                ResetMessagesView();
+            });
+
+            HandleComm = new RelayCommand(x =>
+            {
+                int _o = Convert.ToInt32(x);
+
+                switch (_o)
+                {
+                    case 0:
+                        Text += "{:reset}";
+                        break;
+                    case 1:
+                    {
+                        Color _color;
+                        bool _ok = ColorPickerWindow.ShowDialog(out _color);
+
+                        if (_ok)
+                            Text += "{:color " + string.Format("#{0}{1}{2}{3}", _color.R.ToString("X2"), _color.G.ToString("X2"), _color.B.ToString("X2"), _color.A.ToString("X2")) + "}";
+                    }
+                        break;
+                    case 2:
+                        Text += "{:scale 16}";
+                        break;
+                    case 3:
+                        Text += "{:width 100}";
+                        break;
+                    case 4:
+                        Text += "{:clear}";
+                        break;
+                    case 5:
+                        Text += "{:position 0,0}";
+                        break;
+                }
+
+                SelectedItem = SelectedItem;
+            });
+
+            HandleIcon = new RelayCommand(x =>
+            {
+                Text += "{:icon " + x + "}";
+                SelectedItem = SelectedItem;
+            });
         }
 
         public void SelectMessage(int id) => SelectedItem = Messages.GetMessage(id);
