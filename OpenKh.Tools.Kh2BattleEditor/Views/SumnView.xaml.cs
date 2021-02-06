83f30299f99f3897d41ce37df3bf04ae0922e6fb
Rikux3 <d.drankov@gmail.com>
2021-02-06 19:54:16 +0100

    Add UI for Btlv, Plrp and Sumn

---
+++ OpenKh.Tools.Kh2BattleEditor/Views/SumnView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace OpenKh.Tools.Kh2BattleEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for SumnView.xaml
+    /// </summary>
+    public partial class SumnView : UserControl
+    {
+        public SumnView()
+        {
+            InitializeComponent();
+        }
+    }
+}
