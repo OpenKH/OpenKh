472f04ba82327d3b6d89c366e4dc132fff4bb411
TopazTK <topaz.maxwilliam@gmail.com>
2021-02-19 02:58:42 +0300

    Push

--- OpenKh.Tools.ImageViewer/Views/MainWindow.xaml.cs
+++ OpenKh.Tools.ImageViewer/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using OpenKh.Tools.Common;
+using OpenKh.Tools.Common;
 using OpenKh.Tools.ImageViewer.ViewModels;
 using System.Windows;
+using System.Linq;
 
 namespace OpenKh.Tools.ImageViewer.Views
 {
@@ -18,6 +19,21 @@ namespace OpenKh.Tools.ImageViewer.Views
         public MainWindow(ToolInvokeDesc toolInvokeDesc) : this()
         {
             (DataContext as ImageViewerViewModel).LoadImage(toolInvokeDesc);
+        }
+
+        private void DropAction(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = e.Data.GetData(DataFormats.FileDrop) as string[];
+                var fileName = files?.FirstOrDefault();
+                if (string.IsNullOrEmpty(fileName))
+                    return;
+
+                var vm = DataContext as ImageViewerViewModel;
+                vm.LoadImage(fileName);
+                vm.FileName = fileName;
+            }
         }
     }
 }
