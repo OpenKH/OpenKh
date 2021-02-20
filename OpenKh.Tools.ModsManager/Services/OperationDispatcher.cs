5a12b186b6c5705de423193a2e53d9c977503609
Luciano Ciccariello <xeeynamo@hotmail.com>
2021-02-20 14:57:08 +0100

    Add set-up wizard

--- OpenKh.Tools.ModsManager/Services/OperationDispatcher.cs
+++ OpenKh.Tools.ModsManager/Services/OperationDispatcher.cs
@@ -7,9 +7,9 @@ namespace OpenKh.Tools.ModsManager.Services
     {
         public bool LoadFile(Stream outStream, string fileName)
         {
-            var realFileName = Path.Combine(ConfigurationService.OutputModPath, fileName);
+            var realFileName = Path.Combine(ConfigurationService.GameModPath, fileName);
             if (!File.Exists(realFileName))
-                realFileName = Path.Combine(ConfigurationService.GameAssetPath, fileName);
+                realFileName = Path.Combine(ConfigurationService.GameDataLocation, fileName);
             if (!File.Exists(realFileName))
                 return false;
 
@@ -19,9 +19,9 @@ namespace OpenKh.Tools.ModsManager.Services
 
         public int GetFileSize(string fileName)
         {
-            var realFileName = Path.Combine(ConfigurationService.OutputModPath, fileName);
+            var realFileName = Path.Combine(ConfigurationService.GameModPath, fileName);
             if (!File.Exists(realFileName))
-                realFileName = Path.Combine(ConfigurationService.GameAssetPath, fileName);
+                realFileName = Path.Combine(ConfigurationService.GameDataLocation, fileName);
             if (!File.Exists(realFileName))
                 return 0;
 
