a2505b66bc994d3c21e1a3dd2f57dd5df02318eb
TopazTK <topaz.maxwilliam@gmail.com>
2021-02-22 06:59:35 +0300

    Add the menus with all the functions.

--- OpenKh.Tools.BarTool/ViewModels/MainWindowViewModel.cs
+++ OpenKh.Tools.BarTool/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
@@ -10,9 +10,8 @@ using OpenKh.Tools.BarTool.Views;
 using OpenKh.Tools.BarTool.Models;
 using OpenKh.Tools.BarTool.Dialogs;
 
-using Avalonia.Controls;
 using ReactiveUI;
-using System.Threading.Tasks;
+using Avalonia.Controls;
 
 namespace OpenKh.Tools.BarTool.ViewModels
 {
@@ -26,9 +25,17 @@ namespace OpenKh.Tools.BarTool.ViewModels
 
         public string Title { get; private set; }
 
-        public IReactiveCommand NewCommand { get; }
-        public IReactiveCommand OpenCommand { get; }
-        public IReactiveCommand SaveCommand { get; }
+        public EntryModel CurrentItem { get; set; }
+
+        public IReactiveCommand NewCommand => ReactiveCommand.Create(NewEvent);
+        public IReactiveCommand OpenCommand => ReactiveCommand.Create(OpenEvent);
+        public IReactiveCommand SaveCommand => ReactiveCommand.Create(SaveEvent);
+        public IReactiveCommand SubOpenCommand => ReactiveCommand.Create(SubOpenEvent);
+        public IReactiveCommand DeleteCommand => ReactiveCommand.Create(DeleteEvent);
+        public IReactiveCommand ImportCommand => ReactiveCommand.Create(ImportEvent);
+        public IReactiveCommand ExtractCommand => ReactiveCommand.Create(ExtractEvent);
+        public IReactiveCommand AddCommand => ReactiveCommand.Create(AddEvent);
+        public IReactiveCommand ExtractAllCommand => ReactiveCommand.Create(ExtractAllEvent);
 
         public MainWindowViewModel()
         {
@@ -38,12 +45,11 @@ namespace OpenKh.Tools.BarTool.ViewModels
             Items = new ObservableCollection<EntryModel>();
             Types = new EnumModel<Bar.EntryType>();
 
-            NewCommand = ReactiveCommand.Create(NewEvent);
-            OpenCommand = ReactiveCommand.Create(OpenEvent);
-            SaveCommand = ReactiveCommand.Create(SaveEvent);
-
             Title = "Untitled | BAR - OpenKH";
         }
+
+        private static string GetSuggestedFileName(Bar.Entry item) =>
+            $"{item.Name}.{Helpers.GetSuggestedExtension(item.Type)}";
 
         async Task<bool> SaveCheck()
         {
@@ -131,6 +137,95 @@ namespace OpenKh.Tools.BarTool.ViewModels
                     Bar.Write(_stream, Items.Select(item => item.Entry));
 
                 MainWindow.Instance.IsSaved = true;
+            }
+        }
+
+        void SubOpenEvent() { throw new NotImplementedException(); }
+
+        void DeleteEvent()
+        {
+            Items.Remove(CurrentItem);
+            this.RaisePropertyChanged(nameof(Items));
+        }
+
+        async void ImportEvent()
+        {
+            var _dialog = new OpenFileDialog()
+            {
+                Title = "Open a file to import...",
+                Filters = new List<FileDialogFilter>
+                {
+                     new FileDialogFilter() { Name = "All Files", Extensions = new List<string>() { "*" } },
+                }
+            };
+
+            var _files = await _dialog.ShowAsync(MainWindow.Instance);
+
+            if (_files.Length == 1)
+            {
+                using (FileStream _stream = new FileStream(_files[0], FileMode.Open))
+                {
+                    byte[] _bArray = new byte[_stream.Length];
+                    _stream.Read(_bArray, 0, (int)_stream.Length);
+
+                    CurrentItem.Entry.Stream = new MemoryStream(_bArray);
+                    CurrentItem.Entry.Stream.Position = 0;
+                }
+
+                this.RaisePropertyChanged(nameof(CurrentItem));
+            }
+        }
+
+        void AddEvent()
+        {
+            var _entry = new Bar.Entry();
+            _entry.Name = "newf";
+
+            Items.Add(new EntryModel(_entry));
+            this.RaisePropertyChanged(nameof(Items));
+        }
+
+        async void ExtractEvent()
+        {
+            var _defName = GetSuggestedFileName(CurrentItem.Entry);
+
+            var _dialog = new SaveFileDialog()
+            {
+                Title = "Save this file...",
+                Filters = new List<FileDialogFilter>
+                {
+                    new FileDialogFilter() { Name = "All Files", Extensions = new List<string>() { "*" } },
+                },
+                InitialFileName = _defName
+            };
+
+            var _file = await _dialog.ShowAsync(MainWindow.Instance);
+
+            if (!string.IsNullOrEmpty(_file))
+            {
+                using (FileStream _stream = new FileStream(_file, FileMode.OpenOrCreate))
+                    CurrentItem.Entry.Stream.CopyTo(_stream);
+            }
+        }
+
+        async void ExtractAllEvent()
+        {
+            var _dialog = new OpenFolderDialog()
+            {
+                Title = "Choose a path for export...",
+            };
+
+            var _folder = await _dialog.ShowAsync(MainWindow.Instance);
+
+            if (!string.IsNullOrEmpty(_folder))
+            {
+                foreach (var _item in Items)
+                {
+                    var _defName = GetSuggestedFileName(_item.Entry);
+
+                    using (FileStream _stream = new FileStream(Path.Combine(_folder, _defName), FileMode.OpenOrCreate))
+                        _item.Entry.Stream.CopyTo(_stream);
+                }
             }
         }
     }
