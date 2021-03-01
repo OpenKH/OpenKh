6c7c756562173b2ebfbaf1991ab09cb7d3fe3789
Truthkey <verdadmodificada@hotmail.com>
2021-03-01 21:02:33 +0100

    Fix PAM test.

--- OpenKh.Tests/Bbs/PamTests.cs
+++ OpenKh.Tests/Bbs/PamTests.cs
@@ -17,7 +17,7 @@ namespace OpenKh.Tests.Bbs
         {
             var TestPam = Pam.Read(stream);
             Assert.Equal(0x4D4150, (int)TestPam.header.MagicCode);
-            Assert.Equal("p01ex00001a", TestPam.animList[0].AnimEntry.AnimationName);
+            Assert.Equal("OpenKHAnim", TestPam.animList[0].AnimEntry.AnimationName);
             Assert.Equal(1.818989362888275E-14, TestPam.animList[0].BoneChannels[1].TranslationX.Header.MaxValue);
             Assert.Equal(-0.027958117425441742, TestPam.animList[0].BoneChannels[2].TranslationX.Header.MaxValue);
             Assert.Equal(0.17539772391319275, TestPam.animList[0].BoneChannels[52].RotationZ.Header.MaxValue);
