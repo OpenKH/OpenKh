e38cea2a40199e439337927f6e519f39a36901cb
Luciano Ciccariello <xeeynamo@hotmail.com>
2021-04-08 11:29:23 +0100

    Implement GetFileSize for KH2

--- OpenKh.Research.Panacea/KingdomApi.cpp
+++ OpenKh.Research.Panacea/KingdomApi.cpp
@@ -7,6 +7,9 @@
 
 HINSTANCE g_hInstance;
 PFN_Axa_CFileMan_LoadFile pfn_Axa_CFileMan_LoadFile;
+PFN_Axa_CFileMan_GetFileSize pfn_Axa_CFileMan_GetFileSize;
+PFN_Axa_AxaResourceMan_SetResourceItem pfn_Axa_AxaResourceMan_SetResourceItem;
+PFN_Axa_PackageMan_GetFileInfo pfn_Axa_PackageMan_GetFileInfo;
 
 template <typename T>
 void Hook(T& pfn, long address) {
@@ -20,6 +23,9 @@ void Hook() {
     printf("Executable instance at %p\n", g_hInstance);
 
     Hook(pfn_Axa_CFileMan_LoadFile, Axa_CFileMan_LoadFile);
+    Hook(pfn_Axa_CFileMan_GetFileSize, Axa_CFileMan_GetFileSize);
+    Hook(pfn_Axa_AxaResourceMan_SetResourceItem, Axa_AxaResourceMan_SetResourceItem);
+    Hook(pfn_Axa_PackageMan_GetFileInfo, Axa_PackageMan_GetFileInfo);
     Panacea::Initialize();
 
     CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Main, NULL, 0, NULL);
@@ -32,4 +38,16 @@ void WINAPI Main()
 
 long Axa::CFileMan::LoadFile(CFileMan* _this, const char* filename, void* addr, bool unk) {
     return pfn_Axa_CFileMan_LoadFile(_this, filename, addr, unk);
+}
+
+long Axa::CFileMan::GetFileSize(CFileMan* _this, const char* filename, int mode) {
+    return pfn_Axa_CFileMan_GetFileSize(_this, filename, mode);
+}
+
+long Axa::AxaResourceMan::SetResourceItem(const char* filename) {
+    return pfn_Axa_AxaResourceMan_SetResourceItem(filename);
+}
+
+Axa::PackageMan::Unk* Axa::PackageMan::GetFileInfo(const char* filename, int mode) {
+    return pfn_Axa_PackageMan_GetFileInfo(filename, mode);
 }
