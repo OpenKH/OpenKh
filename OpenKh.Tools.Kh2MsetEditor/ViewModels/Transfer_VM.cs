f2a533cde83b564c2218f2a1369b23e3b849840d
Osda <osda@test.com>
2022-04-24 13:01:34 +0200

    Mset update - Motion transfer, Frame edition and docs update

---
+++ OpenKh.Tools.Kh2MsetEditor/ViewModels/Transfer_VM.cs
@@ -0,0 +1,323 @@
+using OpenKh.Kh2;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using static OpenKh.Tools.Kh2MsetEditor.ViewModels.DataView_VM;
+
+namespace OpenKh.Tools.Kh2MsetEditor.ViewModels
+{
+    public class Transfer_VM
+    {
+        public Bar ToMset { get; set; }
+        public ObservableCollection<AnbEntryWrapper> toEntryList_View { get; set; }
+        public Bar.Entry toSelected { get; set; }
+        public Bar FromMset { get; set; }
+        public ObservableCollection<AnbEntryWrapper> fromEntryList_View { get; set; }
+        public Bar.Entry fromSelected { get; set; }
+        public ObservableCollection<boneTransferWrapper> boneTransferWrappers { get; set; }
+        public short ToBoneCount { get; set; }
+        public short FromBoneCount { get; set; }
+
+        public Transfer_VM() { }
+
+        public void loadToFile(string filePath)
+        {
+            ToMset = fileToBar(filePath);
+            loadToViewList();
+        }
+        public void loadFromFile(string filePath)
+        {
+            FromMset = fileToBar(filePath);
+            loadFromViewList();
+        }
+
+        public Bar fileToBar(string filePath)
+        {
+            if (!DataView_VM.isValidFilepath(filePath))
+                return null;
+
+            using var stream = File.Open(filePath, FileMode.Open);
+            if (!Bar.IsValid(stream))
+                return null;
+
+            return Bar.Read(stream);
+        }
+
+        public static void loadViewList(ObservableCollection<AnbEntryWrapper> anbEntries, Bar msetBar)
+        {
+            if (anbEntries == null)
+                anbEntries = new ObservableCollection<AnbEntryWrapper>();
+
+            anbEntries.Clear();
+            int barIndex = 0;
+            foreach (Bar.Entry barEntry in msetBar)
+            {
+                anbEntries.Add(new AnbEntryWrapper(barIndex, barEntry));
+                barIndex++;
+            }
+        }
+
+        public void loadToViewList()
+        {
+            if (toEntryList_View == null)
+                toEntryList_View = new ObservableCollection<AnbEntryWrapper>();
+
+            toEntryList_View.Clear();
+            int barIndex = 0;
+            foreach (Bar.Entry barEntry in ToMset)
+            {
+                toEntryList_View.Add(new AnbEntryWrapper(barIndex, barEntry));
+                barIndex++;
+            }
+            searchToBoneCount();
+        }
+        public void loadFromViewList()
+        {
+            if (fromEntryList_View == null)
+                fromEntryList_View = new ObservableCollection<AnbEntryWrapper>();
+
+            fromEntryList_View.Clear();
+            int barIndex = 0;
+            foreach (Bar.Entry barEntry in FromMset)
+            {
+                fromEntryList_View.Add(new AnbEntryWrapper(barIndex, barEntry));
+                barIndex++;
+            }
+        }
+
+        public class boneTransferWrapper
+        {
+            public short FromBone { get; set; }
+            public short? ToBone { get; set; }
+
+            public boneTransferWrapper(short FromBone, short? ToBone)
+            {
+                this.FromBone = FromBone;
+                this.ToBone = ToBone;
+            }
+        }
+        public void loadBoneTransferWrappers()
+        {
+            if (boneTransferWrappers == null) {
+                boneTransferWrappers = new ObservableCollection<boneTransferWrapper>();
+            }
+            boneTransferWrappers.Clear();
+
+            for (short i = 0; i < FromBoneCount; i++) {
+                boneTransferWrappers.Add(new boneTransferWrapper(i,null));
+            }
+        }
+        public void loadBoneTransferFile(string filePath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                if (!line.Contains(","))
+                    continue;
+
+                string[] lineData = line.Split(",");
+                if (lineData.Length != 2 || boneTransferWrappers == null)
+                    continue;
+
+                foreach (boneTransferWrapper transfer in boneTransferWrappers)
+                {
+                    if (transfer.FromBone == System.Int16.Parse(lineData[0])) {
+                        transfer.ToBone = System.Int16.Parse(lineData[1]);
+                    }
+                }
+                
+            }
+        }
+        public void searchToBoneCount()
+        {
+            foreach(AnbEntryWrapper anbWrapper in toEntryList_View)
+            {
+                if (anbWrapper.Entry.Stream.Length < 1) {
+                    continue;
+                }
+                anbWrapper.Entry.Stream.Position = 0;
+                AnimationBinary anb = new AnimationBinary(anbWrapper.Entry.Stream);
+                ToBoneCount = anb.MotionFile.InterpolatedMotionHeader.BoneCount;
+                break;
+            }
+        }
+
+        public void transferAnb()
+        {
+            if (toSelected == null || fromSelected == null || fromSelected.Stream.Length == 0)
+                return;
+
+            fromSelected.Stream.Position = 0;
+
+            AnimationBinary fromAnb = new AnimationBinary(fromSelected.Stream);
+
+            short boneCountFrom = fromAnb.MotionFile.InterpolatedMotionHeader.BoneCount;
+
+            // BONE TRANSFERS
+            Dictionary<short, short?> boneTransfers = new Dictionary<short, short?>();
+            // Bones
+            foreach (boneTransferWrapper boneTransfer in boneTransferWrappers)
+            {
+                boneTransfers.Add(boneTransfer.FromBone, boneTransfer.FromBone);
+            }
+            // IK Helpers
+            foreach (int i in Enumerable.Range(0, fromAnb.MotionFile.IKHelpers.Count))
+            {
+                boneTransfers.Add(fromAnb.MotionFile.IKHelpers[i].Index, (short)(ToBoneCount + i));
+            }
+            // None
+            boneTransfers.Add(-1, -1);
+
+            // INITIAL POSE - Reassign used bones, delete unused 
+            for (int i = fromAnb.MotionFile.InitialPoses.Count - 1; i >= 0; i--)
+            {
+                Motion.InitialPose initialPose = fromAnb.MotionFile.InitialPoses[i];
+
+                if (boneTransfers[initialPose.BoneId] != null)
+                {
+                    initialPose.BoneId = (short)boneTransfers[initialPose.BoneId];
+                }
+                else
+                {
+                    fromAnb.MotionFile.InitialPoses.RemoveAt(i);
+                }
+            }
+
+            // F-CURVE FORWARD - Reassign used bones, delete unused 
+            // F-CURVE INVERSE - Remain the same
+            int deletedFCurveCount = 0;
+            for (int i = fromAnb.MotionFile.FCurvesForward.Count - 1; i >= 0; i--)
+            {
+                Motion.FCurve fCurve = fromAnb.MotionFile.FCurvesForward[i];
+
+                if (boneTransfers[fCurve.JointId] != null)
+                {
+                    fCurve.JointId = (short)boneTransfers[fCurve.JointId];
+                }
+                else
+                {
+                    fromAnb.MotionFile.FCurvesForward.RemoveAt(i);
+                    deletedFCurveCount++;
+                }
+            }
+
+
+            // CONSTRAINTS - If it uses an unused bone delete, otherwise reassign
+            for (int i = fromAnb.MotionFile.Constraints.Count - 1; i >= 0; i--)
+            {
+                Motion.Constraint constraint = fromAnb.MotionFile.Constraints[i];
+
+                if (boneTransfers[constraint.ConstrainedJointId] == null || boneTransfers[constraint.SourceJointId] == null)
+                {
+                    fromAnb.MotionFile.Constraints.RemoveAt(i);
+                    continue;
+                }
+                else
+                {
+                    constraint.ConstrainedJointId = (short)boneTransfers[constraint.ConstrainedJointId];
+                    constraint.SourceJointId = (short)boneTransfers[constraint.SourceJointId];
+                }
+            }
+
+            // EXPRESSIONS - Should always be over IK Helpers
+            for (int i = fromAnb.MotionFile.Expressions.Count - 1; i >= 0; i--)
+            {
+                Motion.Expression expression = fromAnb.MotionFile.Expressions[i];
+
+                if (boneTransfers[expression.TargetId] != null)
+                {
+                    expression.TargetId = (short)boneTransfers[expression.TargetId];
+                }
+                else
+                {
+                    //fromAnb.MotionFile.BoneFCurves.RemoveAt(i);
+                }
+            }
+
+            // IK HELPERS
+            for (int i = fromAnb.MotionFile.IKHelpers.Count - 1; i >= 0; i--)
+            {
+                Motion.IKHelper IkHelper = fromAnb.MotionFile.IKHelpers[i];
+                IkHelper.Index = (short)boneTransfers[IkHelper.Index];
+                IkHelper.ParentId = (short)boneTransfers[IkHelper.ParentId];
+                IkHelper.SiblingId = (short)boneTransfers[IkHelper.SiblingId];
+                IkHelper.ChildId = (short)boneTransfers[IkHelper.ChildId];
+            }
+
+            // JOINTS - CHECK LATER
+            Dictionary<short, List<short>> IKRelocators = new Dictionary<short, List<short>>();
+            Dictionary<short, byte> flagSaves = new Dictionary<short, byte>();
+            IKRelocators.Add(-1, new List<short>());
+            foreach (Motion.Joint joint in fromAnb.MotionFile.Joints)
+            {
+                if (boneTransfers[joint.JointId] != null)
+                {
+                    flagSaves.Add((short)boneTransfers[joint.JointId], joint.Flags);
+                }
+
+                if (joint.JointId >= boneCountFrom)
+                {
+                    IKRelocators[-1].Add((short)boneTransfers[joint.JointId]);
+                }
+                else if (IKRelocators[-1].Count != 0)
+                {
+                    short reloc = (short)boneTransfers[joint.JointId];
+                    IKRelocators.Add(reloc, new List<short>());
+                    foreach (short relocator in IKRelocators[-1])
+                    {
+                        IKRelocators[reloc].Add(relocator);
+                    }
+                    IKRelocators[-1].Clear();
+                }
+            }
+            fromAnb.MotionFile.Joints.Clear();
+            for (short i = 0; i < ToBoneCount; i++)
+            {
+                if (IKRelocators.ContainsKey(i))
+                {
+                    foreach (short reloc in IKRelocators[i])
+                    {
+                        fromAnb.MotionFile.Joints.Add(new Motion.Joint(reloc));
+                    }
+                }
+
+                fromAnb.MotionFile.Joints.Add(new Motion.Joint(i));
+            }
+            if (IKRelocators.ContainsKey(-1))
+            {
+                foreach (short reloc in IKRelocators[-1])
+                {
+                    fromAnb.MotionFile.Joints.Add(new Motion.Joint(reloc));
+                }
+            }
+            foreach (Motion.Joint joint in fromAnb.MotionFile.Joints)
+            {
+                if (flagSaves.ContainsKey(joint.JointId))
+                {
+                    joint.Flags = flagSaves[joint.JointId];
+                }
+            }
+
+            // POSITION
+            for (int i = 0; i < fromAnb.MotionFile.RootPosition.FCurveId.Length; i++)
+            {
+                if (fromAnb.MotionFile.RootPosition.FCurveId[i] != -1)
+                {
+                    fromAnb.MotionFile.RootPosition.FCurveId[i] -= deletedFCurveCount;
+                }
+            }
+
+            fromAnb.MotionFile.InterpolatedMotionHeader.BoneCount = ToBoneCount;
+            fromSelected.Stream = fromAnb.toStream(); // load new data on origin entry
+            fromSelected.Stream.Position = 0;
+
+            toSelected.Name = fromSelected.Name;
+            toSelected.Index = fromSelected.Index;
+            toSelected.Type = fromSelected.Type;
+            toSelected.Stream = fromAnb.toStream();
+            toSelected.Stream.Position = 0;
+            toSelected.Type = fromSelected.Type;
+        }
+    }
+}
--- docs/kh2/file/anb/anb.md
+++ docs/kh2/file/anb/anb.md
@@ -3,7 +3,7 @@
 ANB files are used by the game engine to animate a 3D model and trigger events in specific frames. They are used for both gameplay and cutscenes. All the gameplay ANB files are located inside [MSET](mset.md) files in `obj/`, while for cutscenes they are located in `anm/{WORLD}/{MODEL}/`.
 Internally they are just [BAR archives](../type/bar.md) and for what it's known the only two files that can be found in are [motion data](#motion-data) and [effect data](#effect-data).
 
-## Motion data
+# Motion data
 
 This file is responsible of animating a [3D model](../type/mdlx.md). It comes in two forms, interpolated and RAW. Due to the bigger complexity of interpolated-type motion files, they will be documented in a [separate document](motion.md).
 
@@ -11,115 +11,208 @@ The game engine assumes that the maximum frame rate is `60`; this is referred as
 
 The RAW animation type just takes an array of matrices for each frame and applies them to the bones of a model. It is a very cheap technique in terms of CPU usage, but it requires a big amount of memory.
 
-### Motion header
+## Motion header
 
 Like model files, the first `0x90` are reserved an always set to `00`. Offsets and file size ignores the first `0x90`. This document refers to the motion type `1`, which is the RAW one. When the motion type is `0`, the rest of the file after the header will be very different; refer to [interpolated motion document](motion.md).
 
 | Offset | Type | Description
 |--------|------|--------------
-| 0      | int  | Motion 'type', 0=Interpolated, 1=Raw
-| 4      | int  | Unknown. It can be `0` or `1`
-| 8      | int  | Size of the file
-| 12     | int  | Always 0
+| 0      | int  | Type
+| 4      | int  | Subtype
+| 8      | int  | Extra Offset (Equal to size of motion)
+| 12     | int  | Extra Size
 
-### Raw motion header
+### Motion Types
+
+Types for the unknown extra data.
+
+| Channel | Description
+|---------|---------------
+| 0       | Interpolated
+| 1       | Raw
+
+### Motion Subtypes
+
+Types for the unknown extra data.
+
+| Channel | Description
+|---------|---------------
+| 0       | Normal
+| 1       | Ignore Scale
+
+### Extra Types
+
+Types for the unknown extra data.
+
+| Channel | Description
+|---------|---------------
+| 0       | Weapon Cns
+| 1       | Terminate
+
+## Raw motion header
 
 This is located straight after the motion header.
 
-| Offset | Type | Description
-|--------|------|--------------
-| 0x00   | int  | Bone count. Must match with the bone count of the model.
-| 0x04   | int  | Always 0
-| 0x08   | int  | Always 0
-| 0x0C   | int  | Always 0
-| 0x10   | int  | Amount of GFR in a loop (`FrameEnd - FrameLoop`)
-| 0x14   | int  | Total amount of frames, expressed in LFR
-| 0x18   | int  | Unknown
-| 0x1c   | int  | Offset to the [second matrix table](#raw-motion-more-matrices)
-| 0x20   | vec4f | Bounding Box minimum
-| 0x30   | vec4f | Bounding Box maximum
-| 0x40   | float | Frame loop
-| 0x44   | float | Frame end
-| 0x48   | float | Frames per second, defines the LFR
-| 0x4c   | float | Frame count, expressed in LFR
+| Offset | Type   | Description
+|--------|--------|--------------
+| 0x00   | int    | Bone count. Must match with the bone count of the model.
+| 0x04   | int[3] | Reserved
+| 0x10   | int    | Frame count: Amount of GFR in a loop (`FrameEnd - FrameLoop`)
+| 0x14   | int    | Total Frame count, expressed in LFR
+| 0x18   | int    | Animation Matrix Offset
+| 0x1c   | int    | [Position Matrix](#raw-motion-more-matrices) Offset
+| 0x20   | vec4f  | Bounding Box minimum
+| 0x30   | vec4f  | Bounding Box maximum
+| 0x40   | float  | Frame start
+| 0x44   | float  | Frame end
+| 0x48   | float  | Frames per second, defines the LFR
+| 0x4c   | float  | Frame return, expressed in LFR
 
-### Raw motion matrices
+## Raw motion matrices
 
 An array of array of 4x4 matrices (`Matrix4x4[][]`). The first dimension of the array refers to the total amount of frames, while the second dimension is the bone count. For each frame (LFR), the game engine takes BoneCount amount of `Matrix4x4` and multiplies them to the mesh that corresponds to a specific bone.
 
-### Raw motion more matrices
+## Raw motion more matrices
 
 This is optionally defined and its purpose is unknown. It is an array of 4x4 matrices (`Matrix4x4[]`), where the size of the array is equal to the bone count. For the few files that has been analysed, those matrices are always a Matrix Identity.
 
-## Effect data
+# Motion Triggers
 
 This file describe triggers that happens during a [motion](#motion-data) and it's believed they are located into a different file to favour decoupling.
 
-### Effect main header
+## Motion Triggers structure
 
 | Amount | Description |
 |--------|---------------|
 | Single |  [Header](#effect-entry-header)
-| Array  |  [Type A effect](#Type-A-effect) (Happens during X frames)
-| Array  |  [Type B effect](#Type-B-effect) (Triggered on a specific frame)
+| Array  |  [Range Trigger](#range-trigger-list) count (Happens during X frames)
+| Array  |  [Frame Trigger](#frame-trigger-list) count (Triggered on a specific frame)
 
-### Effect entry header
+## Motion Trigger Header
 
 | Offset | Variable Type | Description |
 |--------|---------------|-------------|
-| 0      | byte | [Type A effect](#Type-A-effect) count
-| 1      | byte | [Type B effect](#Type-B-effect) count
-| 2      | short | Start offset of the type B effects
+| 0      | byte | [Range Trigger](#range-trigger-list) count
+| 1      | byte | [Frame Trigger](#frame-trigger-list) count
+| 2      | short | Frame Triggers offset
 
-### Type A effect
+## Range Trigger
 
 | Offset | Variable Type | Description |
 |--------|---------------|-------------|
 | 0      | short | Start frame
 | 2      | short | End frame
-| 4      | byte | [Effect A ID](#effect-a-list)
+| 4      | byte | [Range Trigger ID](#range-trigger-list)
 | 5      | byte | Param size as shorts
 | 6      | short[] | Param
 
-### Type B effect
+## Frame Trigger
 
 | Offset | Variable Type | Description |
 |--------|---------------|-------------|
 | 0      | short | Trigger frame
-| 2      | byte | [Effect B ID](#effect-b-list)
+| 2      | byte | [Frame Trigger ID](#frame-trigger-list)
 | 3      | byte | Param size as shorts
 | 4      | short[] | Param
 
-### Effect A list
+## Range Trigger list
 
-| ID | Effect | Parameter | Size
+| ID | Trigger | Parameter Size | Parameter Desc
 |--------|---------------|-------------|-------------|
-| 0      | Allows controls (But brings to idle animaiton) | |
-| 1      | Allows controls (But blocks the animation) | |
-| 2      | Allows controls | |
-| 3      | Blocks the animation (But disables gravity) | |
-| 4      | Blocks controls (But allows gravity) | |
-| 10     | Activates hitbox | ? | ?
-| 20     | Performs a reaction command (On current model) | ? | ?
-| 23     | Draws an additional texture | ? | ?
-| 25     | Performs a reaction command (On another model) | ? | ?
-| 27     | Makes invincible | |
-| 30     | Blocks everything | |
-| 34     | Blocks reaction command | |
-| 41     | Allows controls (But disables model rotation) | |
+| 0 | STATE: Grounded | 0 | 
+| 1 | STATE: Falling | 0 | 
+| 2 | STATE: Grounded 2 | 0 | 
+| 3 | STATE: No gravity | 0 | 
+| 4 | Enable collision flag | 1 | 
+| 5 | Disable collision flag | 1 | 
+| 6 | Enable RECOM flag | 2 | 
+| 7 | Disable RECOM flag (UNUSED?) | ? | 
+| 8 | \<unknown\> (Prints "using NO_DAMAGE_REACTION attribute"?) (UNUSED?) | ? | 
+| 9 | STATE: \<unknown\> | 0 | 
+| 10 | Motion attack on | 2 | 
+| 11 | STATE: Allows combo attack | 0 | 
+| 12 | STATE: \<unknown\> | 0 | 
+| 13 | STATE: \<unknown\> | 0 | 
+| 14 | STATE: Allows Battle RCs | 0 | 
+| 15 | STATE: \<unknown\> (UNUSED?) | ? | 
+| 16 | \<unknown\> ("Mobility enhancement", eg: LW's dash) | 0 | 
+| 17 | \<unknown\> (AI combo 1) | 0 | 
+| 18 | \<unknown\> (AI combo 2) | 0 | 
+| 19 | \<unknown\> ("Disable forced i-frames", eg: LW's idle, ground and air) | 0 | 
+| 20 | Reaction Command (Self) | 2 | 
+| 21 | STATE: \<unknown\> | 0 | 
+| 22 | Turn to target | 1 | Turn speed
+| 23 | Texture animation | 1 | 
+| 24 | PROPERTY: \<unknown\> (Disable gravity but keeps kinetics) | 1 | 
+| 25 | PROPERTY: \<unknown\> (AnmatrCommand; Reaction command on other object) | ? | 
+| 26 | PROPERTY: \<unknown\> (AnmatrCommand) | 1 | 
+| 27 | STATE: Hitbox off (Can't be hit) | 0 | 
+| 28 | Turn to lock on | 1 | Turn speed
+| 29 | STATE: \<unknown\> (Can't fall off edges easily) | 0 | 
+| 30 | STATE: \<unknown\> (Freeze animation? Immovable?) | 0 | 
+| 31 | STATE: \<unknown\> | 0 | 
+| 32 | STATE: \<unknown\> | 0 | 
+| 33 | motion attack on (Enemy) | 3 | 
+| 34 | STATE: \<unknown\> (UNUSED?) | ? | 
+| 35 | STATE: \<unknown\> (Eg: Fire. Makes fire count as combo) | 0 | 
+| 36 | Pattern enable (LW attacks. Teleport to player?) | 0 | 
+| 37 | Pattern disable | 0 | 
+| 38 | STATE: \<unknown\> | 0 | 
+| 39 | STATE: \<unknown\> (UNUSED?) | ? | 
+| 40 | STATE: \<unknown\> | 0 | 
+| 41 | STATE: \<unknown\> | 0 | 
+| 42 | STATE: \<unknown\> (Eg: landing, using items) | 0 | 
+| 43 | STATE: \<unknown\> (UNUSED?) | ? | 
+| 44 | STATE: \<unknown\> (Eg: Explosion finisher, LW bow. Not movable by other objects?) | 0 | 
+| 45 | STATE: \<unknown\> (UNUSED?) | ? | 
+| 46 | STATE: \<unknown\> (UNUSED?) | ? | 
+| 47 | STATE: \<unknown\> | 0 | 
+| 48 | STATE: \<unknown\> (UNUSED?) | ? | 
+| 49 | STATE: \<unknown\> (Eg: Dodge roll) | 0 | 
+| 50 | STATE: \<unknown\> (Allows combo finisher next) | 0 | 
+| 51 | Play singleton SE | 3 | 
+| 52 | STATE: \<unknown\> | 0 | 
+| 53 | STATE: \<unknown\> | 0 | 
 
-### Effect B list
+## Frame Trigger list
 
-| ID | Effect | Parameter | Size
+| ID | Trigger | Parameter Size | Parameter Desc
 |----|---------------|-------------|-------------|
-| 1  | Plays PAX sprite | PAX sprite ID | ?
-| 2  | Plays footstep sound | Sound ID | ?
-| 3  | Plays animation in slot 628 | |
-| 13 | Plays an enemy vsb voice | vsb ID | ?
-| 14 | Plays an ally vsb voice | vsb ID | ?
-| 22 | Makes the keyblade appear | ? | ?
-| 23 | Makes model opacity decrease | ? | ?
-| 24 | Makes model opacity increase | ? | ?
-| 26 | Makes a mesh disappear | ? | ?
-| 27 | Makes a mesh appear | ? | ?
-| 29 | Plays a Keyblade appearance sprite | |
+| 0 | Jump State | 0 | 
+| 1 | Play PAX effect from a.fm | 1 | PAX effect Id
+| 2 | Play footstep sound | 2 | Sound Id
+| 3 | Dusk Jump State (animation in slot 628) | 0 | 
+| 4 | Texture animation start | 1 | 
+| 5 | Texture animation stop (UNUSED?) | ? | 
+| 6 | Use Item effect | 0 | 
+| 7 | \<unknown\> (LIMIT: AnmatrCallback; Limit RC?) | 2 | 
+| 8 | Play SE sound from a.fm | 4 | 
+| 9 | \<unknown\> (VariousTrigger 1) | 0 | 
+| 10 | \<unknown\> (VariousTrigger 2) | 0 | 
+| 11 | \<unknown\> (VariousTrigger 4) | 0 | 
+| 12 | \<unknown\> (VariousTrigger 8) | 0 | 
+| 13 | Plays an enemy vsb voice | 1/2 | VSB Id
+| 14 | Plays an ally vsb voice | 1 | VSB Id
+| 15 | Turn to Target | 1 | 
+| 16 | \<unknown\> (DisableCommandTime; Eg: When hit on air) | 1 | 
+| 17 | Magic cast | 1 | 
+| 18 | \<UNDEFINED\> |  | 
+| 19 | Apply footstep effect (Footprint, water splashes...) | 1 | 
+| 20 | \<UNDEFINED\> |  | 
+| 21 | Turn to lock on | 1 | 
+| 22 | Makes the keyblade appear | 0 | 
+| 23 | Fade start (Opacity decrease) | 1 | 
+| 24 | Fade start (Opacity increase) | 1 | 
+| 25 | \<unknown\> (Related to the party) | 0 | 
+| 26 | Set mesh color | 2 | 
+| 27 | Reset mesh color | 2 | 
+| 28 | Revenge check | 0 | 
+| 29 | Plays a Keyblade appearance sprite | 0 | 
+| 30 | \<unknown\> (LIMIT: PlayVoice) (UNUSED?) | ? | 
+| 31 | Trigger vibration | 1 | 
+| 32 | \<UNDEFINED\> |  | 
+| 33 | \<UNDEFINED\> |  | 
+| 34 | Check for Dodge Roll to Airslide (Quick Run) | 0 | 
+| 35 | \<unknown\> (MOTION: start; Eg: dodge roll) | 0
+
+Note: This data has been checked on normal MSET files. Those marked as (UNUSED?) are probably used in RC and Limit files.
