c2ac34fc3e7cc821af9732dc258ac01f2fbd84b0
Osda <osda@test.com>
2022-10-28 17:51:31 +0200

    Camera controls

--- OpenKh.Tools.Kh2MdlxEditor/Views/Viewport_Control.xaml.cs
+++ OpenKh.Tools.Kh2MdlxEditor/Views/Viewport_Control.xaml.cs
@@ -1,20 +1,10 @@
 using OpenKh.Tools.Kh2MdlxEditor.Utils;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OpenKh.Tools.Kh2MdlxEditor.Views
 {
@@ -28,8 +18,13 @@ namespace OpenKh.Tools.Kh2MdlxEditor.Views
         public Viewport3D Viewport { get; set; }
         public PerspectiveCamera VPCamera { get; set; }
         public List<GeometryModel3D> VPMeshes { get; set; }
-        public Point3D LookAtPoint { get; set; }
+        public Point3D AnchorPoint { get; set; }
+        public Point3D AnchorPointTemp { get; set; }
+        public Vector3D AnchorPointHorVec { get; set; }
+        public Vector3D AnchorPointVerVec { get; set; }
+        public bool AnchorPointLocked { get; set; }
 
+        // CONSTRUCTOR
         public Viewport_Control()
         {
             InitializeComponent();
@@ -45,7 +40,9 @@ namespace OpenKh.Tools.Kh2MdlxEditor.Views
                 VPCamera = Viewport3DUtils.getDefaultCamera();
 
             Viewport.Camera = VPCamera;
-            LookAtPoint = new Point3D();
+            AnchorPoint = new Point3D();
+            AnchorPointTemp = new Point3D();
+            AnchorPointLocked = false;
 
             Model3DGroup myModel3DGroup = new Model3DGroup();
 
@@ -66,17 +63,22 @@ namespace OpenKh.Tools.Kh2MdlxEditor.Views
             viewportFrame.Content = Viewport;
         }
 
+        // ACTIONS
         private void Viewport_MouseWheel(object sender, MouseWheelEventArgs e)
         {
+            double scale = 0.3;
             Point3D position = VPCamera.Position;
-            //Vector3D vector = new Vector3D(position.X, position.Y, position.Z);
-            //double length = vector.Length;
+            Vector3D lookVector = Viewport3DUtils.getVectorToTarget(VPCamera.Position, AnchorPoint);
+            double length = lookVector.Length;
+            lookVector.Normalize();
 
             if (e.Delta > 0)
-                VPCamera.Position = new Point3D(position.X, position.Y, position.Z *= 0.7);
+                lookVector *= length * scale;
 
             else if (e.Delta < 0)
-                VPCamera.Position = new Point3D(position.X, position.Y, position.Z *= 1.3);
+                lookVector *= length * (-scale);
+
+            VPCamera.Position = new Point3D(position.X + lookVector.X, position.Y + lookVector.Y, position.Z + lookVector.Z);
         }
 
         private void Viewport_MouseMove(object sender, MouseEventArgs e)
@@ -99,13 +101,35 @@ namespace OpenKh.Tools.Kh2MdlxEditor.Views
             R_previousPosition = e.GetPosition(viewportFrame);
             L_previousPosition = e.GetPosition(viewportFrame);
         }
+
+        private void Viewport_MouseRightButtonDown(object sender, MouseEventArgs e)
+        {
+            AnchorPointLocked = true;
+            AnchorPointTemp = new Point3D(AnchorPoint.X, AnchorPoint.Y, AnchorPoint.Z);
+
+            Point3D position = VPCamera.Position;
+            AnchorPointHorVec = getHorizontalPerpendicularVector(new Vector3D(position.X - AnchorPoint.X, position.Y - AnchorPoint.Y, position.Z - AnchorPoint.Z));
+            AnchorPointVerVec = getVerticalPerpendicularVector(new Vector3D(position.X - AnchorPoint.X, position.Y - AnchorPoint.Y, position.Z - AnchorPoint.Z), AnchorPointHorVec);
+        }
+        private void Viewport_MouseRightButtonUp(object sender, MouseEventArgs e)
+        {
+            AnchorPointLocked = false;
+            AnchorPoint = new Point3D(AnchorPointTemp.X, AnchorPointTemp.Y, AnchorPointTemp.Z);
+        }
+
+        // FUNCTIONS
         public void moveCamera(Point pre, Point cur)
         {
             Point3D position = VPCamera.Position;
-            double speed = position.Z / 600;
+            Vector3D positionVector = new Vector3D(VPCamera.Position.X, VPCamera.Position.Y, VPCamera.Position.Z);
+            double speed = positionVector.Length / 600;
 
-            VPCamera.Position = new Point3D(position.X - (cur.X - pre.X) * speed, position.Y + (cur.Y - pre.Y) * speed, position.Z);
-            //VPCamera.LookDirection = Viewport3DUtils.getVectorToTarget(VPCamera.Position, LookAtPoint);
+            Vector3D moveHorVec = AnchorPointHorVec * (cur.X - pre.X) * speed;
+            Vector3D moveVerVec = AnchorPointVerVec * (cur.Y - pre.Y) * speed;
+
+
+            VPCamera.Position = new Point3D(position.X - moveHorVec.X, position.Y + moveVerVec.Y, position.Z - moveHorVec.Z);
+            AnchorPointTemp = new Point3D(AnchorPointTemp.X - moveHorVec.X, AnchorPointTemp.Y + moveVerVec.Y, AnchorPointTemp.Z - moveHorVec.Z);
         }
         public void rotateCamera(Point pre, Point cur)
         {
@@ -117,24 +141,45 @@ namespace OpenKh.Tools.Kh2MdlxEditor.Views
 
         private void rotateCamera(double rX, double rY, double rZ)
         {
-            Vector3D vector = new Vector3D(VPCamera.Position.X, VPCamera.Position.Y, VPCamera.Position.Z);
+            Vector3D vector = new Vector3D(VPCamera.Position.X - AnchorPoint.X, VPCamera.Position.Y - AnchorPoint.Y, VPCamera.Position.Z - AnchorPoint.Z);
 
             double length = vector.Length;
-            double theta = Math.Acos(vector.Y / length);
-            double phi = Math.Atan2(-vector.Z, vector.X);
+            double theta = Math.Acos(vector.Y / length); // Vertical angle
+            double phi = Math.Atan2(-vector.Z, vector.X); // Horizontal angle
 
             theta -= rY * 0.01;
             phi -= rX * 0.01;
             length *= 1.0 - 0.1 * rZ;
 
-            theta = Math.Clamp(theta, 0.0001, Math.PI - 0 - 0001);
+            theta = Math.Clamp(theta, 0.0001, Math.PI - 0001);
 
             vector.X = length * Math.Sin(theta) * Math.Cos(phi);
             vector.Z = -length * Math.Sin(theta) * Math.Sin(phi);
             vector.Y = length * Math.Cos(theta);
 
-            VPCamera.Position = new Point3D(vector.X, vector.Y, vector.Z);
-            VPCamera.LookDirection = Viewport3DUtils.getVectorToTarget(VPCamera.Position);
+            VPCamera.Position = new Point3D(AnchorPoint.X + vector.X, AnchorPoint.Y + vector.Y, AnchorPoint.Z + vector.Z);
+            VPCamera.LookDirection = Viewport3DUtils.getVectorToTarget(VPCamera.Position, AnchorPoint);
+        }
+
+        // Returns the horizontal perpendicular vector of length 1
+        private Vector3D getHorizontalPerpendicularVector(Vector3D vector)
+        {
+            // Counterclockwise
+            Vector3D perpendicularVector = new Vector3D(vector.Z, 0, -vector.X);
+            if (perpendicularVector.X != 0 || perpendicularVector.Y != 0 || perpendicularVector.Z != 0)
+                perpendicularVector.Normalize();
+
+            return perpendicularVector;
+        }
+        // Returns the horizontal perpendicular vector of length 1
+        private Vector3D getVerticalPerpendicularVector(Vector3D cameraVector, Vector3D horizontalVector)
+        {
+            // Upwards
+            Vector3D perpendicularVector = Vector3D.CrossProduct(cameraVector, horizontalVector);
+            if(perpendicularVector.X != 0 || perpendicularVector.Y != 0 || perpendicularVector.Z != 0)
+                perpendicularVector.Normalize();
+
+            return perpendicularVector;
         }
     }
 }
