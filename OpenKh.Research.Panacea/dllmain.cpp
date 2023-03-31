7110fa414cf1f084ba0d61da533650f01742413a
TauAkiou <karaken@gmail.com>
2023-03-31 10:47:13 -0400

    add direct8.dll hooking support to Panacea

--- OpenKh.Research.Panacea/dllmain.cpp
+++ OpenKh.Research.Panacea/dllmain.cpp
@@ -10,6 +10,10 @@
 
 typedef BOOL(WINAPI* PFN_MiniDumpWriteDump)(HANDLE hProcess, DWORD ProcessId, HANDLE hFile, MINIDUMP_TYPE DumpType, PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam, PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam, PMINIDUMP_CALLBACK_INFORMATION CallbackParam);
 PFN_MiniDumpWriteDump MiniDumpWriteDumpPtr;
+typedef HRESULT(WINAPI* PFN_DirectInput8Create)(HINSTANCE hinst, DWORD dwVersion, LPCVOID riidltf, LPVOID* ppvOut, LPVOID punkOuter);
+PFN_DirectInput8Create DirectInput8CreatePtr;
+
+
 void HookDbgHelp()
 {
     if (PathFileExists(L"LuaBackend.dll"))
@@ -29,6 +33,25 @@ void HookDbgHelp()
     assert(hModule != nullptr);
 }
 
+void HookDInput8()
+{
+    if (PathFileExists(L"LuaBackend.dll"))
+        LoadLibrary(L"LuaBackend.dll");
+    const char OriginalDllName[] = "\\DINPUT8.dll";
+    char buffer[MAX_PATH];
+
+    auto initialLength = GetSystemDirectoryA(buffer, sizeof(buffer) - sizeof(OriginalDllName) - 1);
+    assert(initialLength > 0);
+
+    strcpy(buffer + initialLength, OriginalDllName);
+
+    auto hModule = LoadLibraryA(buffer);
+    assert(hModule != nullptr);
+
+    DirectInput8CreatePtr = (PFN_DirectInput8Create)GetProcAddress(hModule, "DirectInput8Create");
+    assert(hModule != nullptr);
+}
+
 BOOL APIENTRY DllMain(
     HMODULE hModule,
     DWORD ul_reason_for_call,
@@ -38,6 +61,7 @@ BOOL APIENTRY DllMain(
     {
     case DLL_PROCESS_ATTACH:
         HookDbgHelp();
+        HookDInput8();
         OpenKH::Initialize();
         break;
     case DLL_THREAD_ATTACH:
@@ -49,8 +73,16 @@ BOOL APIENTRY DllMain(
     return TRUE;
 }
 
+
 extern "C" __declspec(dllexport) BOOL WINAPI MiniDumpWriteDump(HANDLE hProcess, DWORD ProcessId, HANDLE hFile, MINIDUMP_TYPE DumpType, PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam, PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam, PMINIDUMP_CALLBACK_INFORMATION CallbackParam)
 {
     if (!MiniDumpWriteDumpPtr) HookDbgHelp();
     return MiniDumpWriteDumpPtr(hProcess, ProcessId, hFile, DumpType, ExceptionParam, UserStreamParam, CallbackParam);
+}
+
+
+extern "C" __declspec(dllexport) HRESULT WINAPI DirectInput8Create(HINSTANCE hinst, DWORD dwVersion, LPCVOID riidltf, LPVOID * ppvOut, LPVOID punkOuter)
+{
+    if (!DirectInput8CreatePtr) HookDInput8();
+    return DirectInput8CreatePtr(hinst, dwVersion, riidltf, ppvOut, punkOuter);
 }
