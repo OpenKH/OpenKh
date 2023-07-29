12c30f68d9603b4e69a9ebd9ffd574ed51098505
kenjiuno <h8mastre@gmail.com>
2023-07-30 07:13:46 +0900

    WIP

--- OpenKh.Tools.Kh2MsetEditorCrazyEdition/Usecases/ImGuiWindows/IKHelperManagerWindowUsecase.cs
+++ OpenKh.Tools.Kh2MsetEditorCrazyEdition/Usecases/ImGuiWindows/IKHelperManagerWindowUsecase.cs
@@ -12,12 +12,12 @@ using OpenKh.Tools.Kh2MsetEditorCrazyEdition.Helpers;
 using OpenKh.Kh2;
 using System.Numerics;
 using OpenKh.Tools.Kh2MsetEditorCrazyEdition.Helpers.HandyEditorSpec;
+using MathNet.Numerics.LinearAlgebra.Factorization;
 
 namespace OpenKh.Tools.Kh2MsetEditorCrazyEdition.Usecases.ImGuiWindows
 {
     public class IKHelperManagerWindowUsecase : IWindowRunnableProvider
     {
-        private readonly MakeHandyEditorUsecase _makeHandyEditorUsecase;
         private readonly ErrorMessages _errorMessages;
         private readonly LoadedModel _loadedModel;
         private readonly Settings _settings;
@@ -25,21 +25,12 @@ namespace OpenKh.Tools.Kh2MsetEditorCrazyEdition.Usecases.ImGuiWindows
         public IKHelperManagerWindowUsecase(
             Settings settings,
             LoadedModel loadedModel,
-            ErrorMessages errorMessages,
-            MakeHandyEditorUsecase makeHandyEditorUsecase
+            ErrorMessages errorMessages
         )
         {
-            _makeHandyEditorUsecase = makeHandyEditorUsecase;
             _errorMessages = errorMessages;
             _loadedModel = loadedModel;
             _settings = settings;
-        }
-
-        private enum EditorType
-        {
-            No,
-            Fk,
-            Ik,
         }
 
         public Action CreateWindowRunnable()
@@ -47,133 +38,91 @@ namespace OpenKh.Tools.Kh2MsetEditorCrazyEdition.Usecases.ImGuiWindows
             var jointAge = _loadedModel.SelectedJointIndexAge.Branch(true);
             var msetAge = _loadedModel.JointDescriptionsAge.Branch(false);
 
-            var editor = EditorType.No;
-
-            Func<Motion.IKHelper>? getIkHelper = null;
-            Motion.IKHelper? ikRef = null;
-            var ikEditors = new List<HandyEditorController>();
-            ikEditors.Add(_makeHandyEditorUsecase.InputInt("joint index", () => ikRef!.Index, it => ikRef!.Index = (short)it));
-            ikEditors.Add(_makeHandyEditorUsecase.InputInt("parent index", () => ikRef!.ParentId, it => ikRef!.ParentId = (short)it));
-            ikEditors.Add(_makeHandyEditorUsecase.InputInt("Unknown", () => ikRef!.Unknown, it => ikRef!.Unknown = it));
-            ikEditors.Add(_makeHandyEditorUsecase.Checkbox("Terminate", () => ikRef!.Terminate, it => ikRef!.Terminate = it));
-            ikEditors.Add(_makeHandyEditorUsecase.Checkbox("Below", () => ikRef!.Below, it => ikRef!.Below = it));
-            ikEditors.Add(_makeHandyEditorUsecase.Checkbox("EnableBias", () => ikRef!.EnableBias, it => ikRef!.EnableBias = it));
-            ikEditors.Add(_makeHandyEditorUsecase.ForEdit3("scale", () => new Vector3(ikRef!.ScaleX, ikRef!.ScaleY, ikRef!.ScaleZ), it =>
-            {
-                ikRef!.ScaleX = it.X;
-                ikRef!.ScaleY = it.Y;
-                ikRef!.ScaleZ = it.Z;
-            }));
-            ikEditors.Add(_makeHandyEditorUsecase.ForEdit3("rotate", () => new Vector3(ikRef!.RotateX, ikRef!.RotateY, ikRef!.RotateZ), it =>
-            {
-                ikRef!.RotateX = it.X;
-                ikRef!.RotateY = it.Y;
-                ikRef!.RotateZ = it.Z;
-            }));
-            ikEditors.Add(_makeHandyEditorUsecase.ForEdit3("translation", () => new Vector3(ikRef!.TranslateX, ikRef!.TranslateY, ikRef!.TranslateZ), it =>
-            {
-                ikRef!.TranslateX = it.X;
-                ikRef!.TranslateY = it.Y;
-                ikRef!.TranslateZ = it.Z;
-            }));
-
-            Func<Mdlx.Bone>? getFkBone = null;
-            Mdlx.Bone? fkBone = null;
-            var fkEditors = new List<HandyEditorController>();
-            fkEditors.Add(_makeHandyEditorUsecase.InputInt("joint index", () => fkBone!.Index, it => fkBone!.Index = it));
-            fkEditors.Add(_makeHandyEditorUsecase.InputInt("parent index", () => fkBone!.Parent, it => fkBone!.Parent = it));
-            fkEditors.Add(_makeHandyEditorUsecase.ForEdit3("scale",
-                () => new Vector3(fkBone!.ScaleX, fkBone!.ScaleY, fkBone!.ScaleZ),
-                it =>
-                {
-                    fkBone!.ScaleX = it.X;
-                    fkBone!.ScaleY = it.Y;
-                    fkBone!.ScaleZ = it.Z;
-                }
-            ));
-            fkEditors.Add(_makeHandyEditorUsecase.ForEdit3("rotate",
-                () => new Vector3(fkBone!.RotationX, fkBone!.RotationY, fkBone!.RotationZ),
-                it =>
-                {
-                    fkBone!.RotationX = it.X;
-                    fkBone!.RotationY = it.Y;
-                    fkBone!.RotationZ = it.Z;
-                }
-            ));
-            fkEditors.Add(_makeHandyEditorUsecase.ForEdit3("translation",
-                () => new Vector3(fkBone!.TranslationX, fkBone!.TranslationY, fkBone!.TranslationZ),
-                it =>
-                {
-                    fkBone!.TranslationX = it.X;
-                    fkBone!.TranslationY = it.Y;
-                    fkBone!.TranslationZ = it.Z;
-                }
-            ));
-
             return () =>
             {
-                if (jointAge.NeedToCatchUpAnyOf(msetAge))
-                {
-                    // load
-
-                    var absIndex = _loadedModel.SelectedJointIndex;
-                    var numFk = _loadedModel.FKJointDescriptions.Count;
-                    if (numFk <= absIndex)
-                    {
-                        // ik
-                        getIkHelper = () => _loadedModel.MotionData!.IKHelpers[absIndex - numFk];
-                        ikRef = getIkHelper();
-                        ikEditors.LoadAll();
-                        editor = EditorType.Ik;
-                    }
-                    else if (0 <= absIndex)
-                    {
-                        // fk
-                        getFkBone = () => _loadedModel.InternalFkBones[absIndex];
-                        fkBone = getFkBone();
-                        fkEditors.LoadAll();
-                        editor = EditorType.Fk;
-                    }
-                    else
-                    {
-                        editor = EditorType.No;
-                    }
-                }
-
                 if (_settings.ViewIKHelper)
                 {
                     ForWindow("IKHelper manager", () =>
                     {
-                        ForMenuBar(() =>
-                        {
-                            ForMenuItem("Apply", () =>
-                            {
-                                if (editor == EditorType.Ik)
-                                {
-                                    ikRef = getIkHelper!();
-                                    ikEditors.SaveAll();
+                        var saved = false;
 
-                                    _loadedModel.SendBackMotionData.TurnOn();
-                                }
-                                if (editor == EditorType.Fk)
-                                {
-                                    _errorMessages.Add(new Exception("fkBone is read only"));
-                                }
-                            });
-                        });
+                        var absIndex = _loadedModel.SelectedJointIndex;
+                        var numFk = _loadedModel.FKJointDescriptions.Count;
 
-                        if (editor == EditorType.Fk)
+                        if (false)
+                        { }
+                        else if (_loadedModel.InternalFkBones?.GetAtOrNull(absIndex) is Mdlx.Bone fk)
                         {
-                            fkEditors.RenderAll();
+                            ForEdit("joint index", () => fk.Index, it => { });
+                            ForEdit("parent index", () => fk.Parent, it => { });
+                            ForEdit3("scale",
+                                () => new Vector3(fk.ScaleX, fk.ScaleY, fk.ScaleZ),
+                                it =>
+                                {
+                                }
+                            );
+                            ForEdit3("rotate",
+                                () => new Vector3(fk.RotationX, fk.RotationY, fk.RotationZ),
+                                it =>
+                                {
+                                }
+                            );
+                            ForEdit3("translation",
+                                () => new Vector3(fk.TranslationX, fk.TranslationY, fk.TranslationZ),
+                                it =>
+                                {
+                                }
+                            );
+                        }
+                        else if (_loadedModel.MotionData?.IKHelpers.GetAtOrNull(absIndex - numFk) is Motion.IKHelper ikRef)
+                        {
+                            ForEdit("joint index", () => ikRef.Index, it => { ikRef.Index = it; saved = true; });
+                            ForEdit("parent index", () => ikRef.ParentId, it => { ikRef.ParentId = it; saved = true; });
+                            ForEdit("Unknown", () => ikRef.Unknown, it => { ikRef.Unknown = it; saved = true; });
+                            ForEdit("Terminate", () => ikRef.Terminate, it => { ikRef.Terminate = it; saved = true; });
+                            ForEdit("Below", () => ikRef.Below, it => { ikRef.Below = it; saved = true; });
+                            ForEdit("EnableBias", () => ikRef.EnableBias, it => { ikRef.EnableBias = it; saved = true; });
+                            ForEdit3("scale",
+                                () => new Vector3(ikRef.ScaleX, ikRef.ScaleY, ikRef.ScaleZ),
+                                it =>
+                                {
+                                    ikRef.ScaleX = it.X;
+                                    ikRef.ScaleY = it.Y;
+                                    ikRef.ScaleZ = it.Z;
+                                    saved = true;
+                                }
+                            );
+                            ForEdit3("rotate",
+                                () => new Vector3(ikRef.RotateX, ikRef.RotateY, ikRef.RotateZ),
+                                it =>
+                                {
+                                    ikRef.RotateX = it.X;
+                                    ikRef.RotateY = it.Y;
+                                    ikRef.RotateZ = it.Z;
+                                    saved = true;
+                                }
+                            );
+                            ForEdit3("translation",
+                                () => new Vector3(ikRef.TranslateX, ikRef.TranslateY, ikRef.TranslateZ),
+                                it =>
+                                {
+                                    ikRef.TranslateX = it.X;
+                                    ikRef.TranslateY = it.Y;
+                                    ikRef.TranslateZ = it.Z;
+                                    saved = true;
+                                }
+                            );
+                        }
+                        else
+                        {
+                            ImGui.Text("(Not available)");
                         }
 
-                        if (editor == EditorType.Ik)
+                        if (saved)
                         {
-                            ikEditors.RenderAll();
+                            _loadedModel.SendBackMotionData.TurnOn();
                         }
-                    },
-                        menuBar: true
-                    );
+                    });
                 }
             };
         }
