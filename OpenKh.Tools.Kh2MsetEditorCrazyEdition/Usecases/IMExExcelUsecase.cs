b52c82ff8eab9608f5ec9f6f7f7030046e56559d
kenjiuno <h8mastre@gmail.com>
2023-07-29 23:58:42 +0900

    WIP

--- OpenKh.Tools.Kh2MsetEditorCrazyEdition/Usecases/IMExExcelUsecase.cs
+++ OpenKh.Tools.Kh2MsetEditorCrazyEdition/Usecases/IMExExcelUsecase.cs
@@ -538,7 +538,7 @@ namespace OpenKh.Tools.Kh2MsetEditorCrazyEdition.Usecases
                         }
                         catch (Exception ex)
                         {
-                            addError(new Exception($"A {SheetName} sheet cell at R{2 + y}C{2 + x} cannot be imported due to error.", ex));
+                            addError(new Exception($"A {SheetName} sheet {headerNames[x]} cell at R{2 + y}C{2 + x} cannot be imported due to error.", ex));
                         }
                     }
                 }
