1028bccd3d4908cbf23bc153079a35e3a63912cb
kenjiuno <h8mastre@gmail.com>
2024-01-25 23:49:49 +0900

    Adding loader menu

--- OpenKh.Tools.KhModels/View/MainWindow.xaml.cs
+++ OpenKh.Tools.KhModels/View/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
 using OpenKh.AssimpUtils;
+using OpenKh.Tools.Common.Wpf;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Xe.Tools.Wpf.Commands;
+using Xe.Tools.Wpf.Dialogs;
 
 namespace OpenKh.Tools.KhModels.View
 {
@@ -16,6 +22,49 @@ namespace OpenKh.Tools.KhModels.View
         {
             InitializeComponent();
             thisVM = new MainWindowVM(viewport);
+
+            {
+                var loaders = new List<LoaderItem>();
+
+                LoaderItem GetLoaderOf(string extensions, string name)
+                {
+                    var command = new RelayCommand(
+                        _ =>
+                        {
+                            FileDialog.OnOpen(
+                                filePath =>
+                                {
+                                    thisVM.LoadFilepath(filePath);
+                                },
+                                FileDialogFilterComposer.Compose()
+                                    .AddExtensions(extensions, extensions.Split(','))
+                            );
+                        }
+                    );
+
+                    return new LoaderItem(extensions, name, command);
+                }
+
+                loaders.Add(GetLoaderOf("fbx,dae", "Assimp"));
+                loaders.Add(GetLoaderOf("mdls", "KH1"));
+                loaders.Add(GetLoaderOf("wpn", "KH1 - weapon"));
+                loaders.Add(GetLoaderOf("mdlx", "KH2"));
+                loaders.Add(GetLoaderOf("map", "KH 2 Map"));
+                loaders.Add(GetLoaderOf("pmo", "KH BBS - DDD"));
+                loaders.Add(GetLoaderOf("arc", "KH BBS Map"));
+                loaders.Add(GetLoaderOf("pmp", "KH DDD Map"));
+
+                _loader.ItemsSource = loaders;
+            }
+        }
+
+        private record LoaderItem(
+            string Extensions,
+            string Name,
+            ICommand Command
+        )
+        {
+            public string Header => $"{Name} ({Extensions})";
         }
 
         private void Grid_Drop(object sender, DragEventArgs e)
@@ -37,7 +86,7 @@ namespace OpenKh.Tools.KhModels.View
         {
             thisVM.ExportMdls();
         }
-        private void Menu_ExportAsFbx(object sender, EventArgs e)
+        private void Menu_ExportAsFbx(object sender, RoutedEventArgs e)
         {
             thisVM.ExportModel();
         }
@@ -53,7 +102,7 @@ namespace OpenKh.Tools.KhModels.View
 
         private void Button_ShowMesh(object sender, RoutedEventArgs e)
         {
-            thisVM.ShowMesh = ! thisVM.ShowMesh;
+            thisVM.ShowMesh = !thisVM.ShowMesh;
             thisVM.SetOptions();
             thisVM.VpService.Render();
         }
@@ -70,6 +119,5 @@ namespace OpenKh.Tools.KhModels.View
             thisVM.SetOptions();
             thisVM.VpService.Render();
         }
-
     }
 }
