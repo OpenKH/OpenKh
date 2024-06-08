f52740d3d78e1c01b4df597ccfb603e0f2c6aa3e
osdanova <osdanovaonline@outlook.com>
2024-06-08 21:20:44 +0200

    ObjectEditor - UI module

--- OpenKh.Tools.Kh2ObjectEditor/ViewModel/ModuleLoader_VM.cs
+++ OpenKh.Tools.Kh2ObjectEditor/ViewModel/ModuleLoader_VM.cs
@@ -8,6 +8,7 @@ namespace OpenKh.Tools.Kh2ObjectEditor.ViewModel
     {
         public Visibility TabModelEnabled { get { return MdlxService.Instance.MdlxBar == null ? Visibility.Collapsed : Visibility.Visible; } }
         public Visibility TabTexturesEnabled { get { return MdlxService.Instance.MdlxBar == null ? Visibility.Collapsed : Visibility.Visible; } }
+        public Visibility TabUIEnabled { get { return (ApdxService.Instance.ImgdFace == null && ApdxService.Instance.ImgdCommand == null) ? Visibility.Collapsed : Visibility.Visible; } }
         public Visibility TabMotionsEnabled { get { return MsetService.Instance.MsetBar == null ? Visibility.Collapsed : Visibility.Visible; } }
         public Visibility TabCollisionsEnabled { get { return MdlxService.Instance.CollisionFile == null ? Visibility.Collapsed : Visibility.Visible; } }
         public Visibility TabParticlesEnabled { get { return ApdxService.Instance.ApdxBar == null ? Visibility.Collapsed : Visibility.Visible; } }
