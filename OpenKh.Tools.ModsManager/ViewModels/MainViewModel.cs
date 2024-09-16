018b990b59d9de2778c18f055cadb004cdff2f92
shananas <s.odea@comcast.net>
2024-09-15 21:45:28 -0400

    Remove old `PcReleaseLanguage` in setup wizard for new `PCReleaseLanguage from my last PR. This also fixes trying to extract from `en` instead of `jp` when Japanese EGS version is selected if user doesnt restart the setup wizard after selecting those options. Also fixes SteamAPITrick setup wizard page showing when steam is not selected.

--- OpenKh.Tools.ModsManager/ViewModels/MainViewModel.cs
+++ OpenKh.Tools.ModsManager/ViewModels/MainViewModel.cs
@@ -478,7 +478,6 @@ namespace OpenKh.Tools.ModsManager.ViewModels
                     ConfigPcsx2Location = ConfigurationService.Pcsx2Location,
                     ConfigPcReleaseLocation = ConfigurationService.PcReleaseLocation,
                     ConfigPcReleaseLocationKH3D = ConfigurationService.PcReleaseLocationKH3D,
-                    ConfigPcReleaseLanguage = ConfigurationService.PcReleaseLanguage,
                     ConfigRegionId = ConfigurationService.RegionId,
                     ConfigPanaceaInstalled = ConfigurationService.PanaceaInstalled,
                 };
