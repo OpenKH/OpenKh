2fcfec4b4c51ca2c2817acd2bb0599dfd22868c7
some1fromthedark <some1fromthedark@gmail.com>
2026-01-30 09:38:17 -0800

    Remove unnecessary imports

--- OpenKh.Command.Arc/Program.cs
+++ OpenKh.Command.Arc/Program.cs
@@ -5,8 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
-using System.ComponentModel;
 
 namespace OpenKh.Command.Arc
 {
